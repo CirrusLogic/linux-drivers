bf73d9a1f18087e7989b78a5204bc5863911c075
Ken Mixter <kmixter@google.com>
2010-03-01 15:58:59 -0800

    CHROMIUM: x86: print out initial usec value calculated from TSC and CPU HZ
    This is to give an idea of how much time was spent in firmware and kernel
    prior to timekeeping initialization in the kernel. Print out the value of
    TSC converted to microseconds.
    
    BUG=chromium-os:4900
    TEST=boot on an x86 platform, verify that there is a line in the
    kernel log saying "Initial usec timer"
    
    Change-Id: I0a90643f383a937821f8a6439e994ffdaf43d454
    Signed-off-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/7584
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    
    [rebase44(groeck): Replaced printk with pr_info; squashed with
    	'CHROMIUM: Write the initial TSC at early kernel init'
    	and dropped printing the raw TSC value.]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    Conflicts:
    	arch/x86/kernel/tsc.c
    
    [rebase49(groeck): Resolved conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    Conflicts:
    	arch/x86/kernel/tsc.c
    
    [rebase419(groeck): rdtscll -> rdtsc]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- arch/x86/kernel/tsc.c
+++ arch/x86/kernel/tsc.c
@@ -1414,6 +1414,8 @@ device_initcall(init_tsc_clocksource);
 
 static bool __init determine_cpu_tsc_frequencies(bool early)
 {
+	u64 initial_tsc;
+
 	/* Make sure that cpu and tsc are not already calibrated */
 	WARN_ON(cpu_khz || tsc_khz);
 
@@ -1429,6 +1431,8 @@ static bool __init determine_cpu_tsc_frequencies(bool early)
 		cpu_khz = pit_hpet_ptimer_calibrate_cpu();
 	}
 
+	initial_tsc = rdtsc();
+
 	/*
 	 * Trust non-zero tsc_khz as authoritative,
 	 * and use it to sanity check cpu_khz,
@@ -1441,6 +1445,10 @@ static bool __init determine_cpu_tsc_frequencies(bool early)
 
 	if (tsc_khz == 0)
 		return false;
+
+	do_div(initial_tsc, cpu_khz / 1000);
+	pr_info("Initial usec timer %llu\n",
+		(unsigned long long)initial_tsc);
 
 	pr_info("Detected %lu.%03lu MHz processor\n",
 		(unsigned long)cpu_khz / KHZ,
