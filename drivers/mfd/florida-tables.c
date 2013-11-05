c083fc024d9f94fa5acae3ed722790f03e5f9f07
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2013-11-05 10:26:45 +0000

    mfd: arizona: Add additional registers as readable for debug
    Change-Id: I687f1f672b904438ae9ee6947dbd93f858b003f6
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- drivers/mfd/florida-tables.c
+++ drivers/mfd/florida-tables.c
@@ -1997,6 +1997,12 @@ static bool florida_readable_register(struct device *dev, unsigned int reg)
 	case ARIZONA_MIC_BIAS_CTRL_1:
 	case ARIZONA_MIC_BIAS_CTRL_2:
 	case ARIZONA_MIC_BIAS_CTRL_3:
+	case ARIZONA_HP_CTRL_1L:
+	case ARIZONA_HP_CTRL_1R:
+	case ARIZONA_HP_CTRL_2L:
+	case ARIZONA_HP_CTRL_2R:
+	case ARIZONA_HP_CTRL_3L:
+	case ARIZONA_HP_CTRL_3R:
 	case ARIZONA_ACCESSORY_DETECT_MODE_1:
 	case ARIZONA_HEADPHONE_DETECT_1:
 	case ARIZONA_HEADPHONE_DETECT_2:
@@ -2043,6 +2049,7 @@ static bool florida_readable_register(struct device *dev, unsigned int reg)
 	case ARIZONA_DMIC4R_CONTROL:
 	case ARIZONA_OUTPUT_ENABLES_1:
 	case ARIZONA_OUTPUT_STATUS_1:
+	case ARIZONA_OUTPUT_STANDBY_1:
 	case ARIZONA_RAW_OUTPUT_STATUS_1:
 	case ARIZONA_OUTPUT_RATE_1:
 	case ARIZONA_OUTPUT_VOLUME_RAMP:
