f4cde5fd44e2a02a450346d27158d40e89fe9441
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2013-11-21 14:21:04 +0000

    mfd: arizona: Add support for WM8280
    Change-Id: I4746c2caacf3a649e61defde572942d687eb63f7
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@opensource.wolfsonmicro.com>

--- drivers/mfd/arizona-i2c.c
+++ drivers/mfd/arizona-i2c.c
@@ -43,6 +43,7 @@ static int arizona_i2c_probe(struct i2c_client *i2c,
 		break;
 #endif
 #ifdef CONFIG_MFD_FLORIDA
+	case WM8280:
 	case WM5110:
 		regmap_config = &florida_i2c_regmap;
 		break;
@@ -86,6 +87,7 @@ static int arizona_i2c_remove(struct i2c_client *i2c)
 
 static const struct i2c_device_id arizona_i2c_id[] = {
 	{ "wm5102", WM5102 },
+	{ "wm8280", WM8280 },
 	{ "wm5110", WM5110 },
 	{ "wm8997", WM8997 },
 	{ }
