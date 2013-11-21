525196defcacc837f5439f9fcadf990229cccdd6
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2013-11-21 15:44:55 +0000

    extcon: arizona: Add support for WM8280
    Change-Id: Ic6ad9144363f018335a2c47f72347c3278c2a99d
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@opensource.wolfsonmicro.com>

--- drivers/extcon/extcon-arizona.c
+++ drivers/extcon/extcon-arizona.c
@@ -161,6 +161,7 @@ static void arizona_extcon_do_magic(struct arizona_extcon_info *info,
 	int ret;
 
 	switch (arizona->type) {
+	case WM8280:
 	case WM5110:
 		mask = 0x0007;
 		if (magic)
@@ -1277,6 +1278,7 @@ static int arizona_extcon_probe(struct platform_device *pdev)
 			break;
 		}
 		break;
+	case WM8280:
 	case WM5110:
 		switch (arizona->rev) {
 		case 0 ... 2:
