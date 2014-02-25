15a199f8be83d4bc744d952894dab80de6faff19
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-02-25 13:45:49 +0000

    regmap: Mark reg_defaults in regmap_multi_reg_write as const
    There should be no need for the writes supplied to this function to be
    edited by it so mark them as const.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry picked from commit f7e2cec02b0e5bfe2180f09de9b0bc724774c51a)
    
    Change-Id: Ie0f109b0278e2c6b86bbbf611aa25799549f8391
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- include/linux/regmap.h
+++ include/linux/regmap.h
@@ -148,7 +148,7 @@ int regmap_raw_write(struct regmap *map, unsigned int reg,
 		     const void *val, size_t val_len);
 int regmap_bulk_write(struct regmap *map, unsigned int reg, const void *val,
 			size_t val_count);
-int regmap_multi_reg_write(struct regmap *map, struct reg_default *regs,
+int regmap_multi_reg_write(struct regmap *map, const struct reg_default *regs,
 			int num_regs);
 int regmap_read(struct regmap *map, unsigned int reg, unsigned int *val);
 int regmap_raw_read(struct regmap *map, unsigned int reg,
