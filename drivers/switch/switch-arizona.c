1c0ff3d61d0ec3798fa6d260fc102d32a3396cf1
bsapthagiri <sapthagiri.baratam@incubesol.com>
2014-06-16 21:47:50 +0100

    switch: arizona: Seperate out mic callback from hpdet callback
    Change-Id: I4086baa492bd0ab21959fbeca5af70b023d9abca
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: bsapthagiri <sapthagiri.baratam@incubesol.com>

--- drivers/switch/switch-arizona.c
+++ drivers/switch/switch-arizona.c
@@ -717,7 +717,7 @@ int arizona_hpdet_start(struct arizona_extcon_info *info)
 		int imp = info->arizona->pdata.fixed_hpdet_imp;
 
 		if (arizona->pdata.hpdet_cb)
-			arizona->pdata.hpdet_cb(imp, info->mic);
+			arizona->pdata.hpdet_cb(imp);
 
 		switch (arizona->type) {
 		case WM5110:
@@ -833,7 +833,7 @@ int arizona_hpdet_reading(struct arizona_extcon_info *info, int val)
 	arizona->hp_impedance = val;
 
 	if (arizona->pdata.hpdet_cb)
-		arizona->pdata.hpdet_cb(arizona->hp_impedance, info->mic);
+		arizona->pdata.hpdet_cb(arizona->hp_impedance);
 
 	switch (arizona->type) {
 	case WM5110:
@@ -1048,6 +1048,9 @@ done:
 		else
 			arizona_extcon_report(info, BIT_HEADSET_NO_MIC);
 	}
+
+	if (arizona->pdata.micd_cb)
+		arizona->pdata.micd_cb(info->mic);
 
 	return 0;
 }
@@ -1495,8 +1498,11 @@ static irqreturn_t arizona_jackdet(int irq, void *data)
 
 		/* Use a sufficiently large number to indicate open circuit */
 		if (arizona->pdata.hpdet_cb) {
-			arizona->pdata.hpdet_cb(ARIZONA_HP_Z_OPEN, false);
+			arizona->pdata.hpdet_cb(ARIZONA_HP_Z_OPEN);
 		}
+
+		if (arizona->pdata.micd_cb)
+			arizona->pdata.micd_cb(false);
 	}
 
 out:
