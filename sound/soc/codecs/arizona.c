5d3fc9b683ecec390bd9601bfdc865f3b781f42b
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-06-25 13:31:07 +0100

    ASoC: arizona: Correct checking of FLL ratio limitations
    The check to ensure the Fref frequency is within the bounds for the
    current ratio, was placed in the wrong loop. The initial configuration
    will always be valid and the loop lowering the ratio will only reinforce
    this validity. The check should be on the loop increasing the ratio.
    This could on occasion cause an invalid ratio/Fref combination to be
    selected.
    
    Change-Id: Ia7103d06c84ddd54c5a405cc6426ddde4629e739
    Reported-by: Ryo Tsutsui <ryo.tsutsui@wolfsonmicro.com>
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- sound/soc/codecs/arizona.c
+++ sound/soc/codecs/arizona.c
@@ -1792,6 +1792,10 @@ static int arizona_calc_fratio(struct arizona_fll *fll,
 	while (div <= ARIZONA_FLL_MAX_REFDIV) {
 		for (ratio = init_ratio; ratio <= ARIZONA_FLL_MAX_FRATIO;
 		     ratio++) {
+			if (ARIZONA_FLL_VCO_CORNER / (fll->vco_mult * ratio) <
+			    Fref)
+				break;
+
 			if (target % (ratio * Fref)) {
 				cfg->refdiv = refdiv;
 				cfg->fratio = ratio - 1;
@@ -1800,10 +1804,6 @@ static int arizona_calc_fratio(struct arizona_fll *fll,
 		}
 
 		for (ratio = init_ratio - 1; ratio > 0; ratio--) {
-			if (ARIZONA_FLL_VCO_CORNER / (fll->vco_mult * ratio) <
-			    Fref)
-				break;
-
 			if (target % (ratio * Fref)) {
 				cfg->refdiv = refdiv;
 				cfg->fratio = ratio - 1;
