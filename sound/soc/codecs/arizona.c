a12334abddea997c21f9fdcad5646def8aa4280e
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2014-10-02 14:16:43 +0100

    ASoC: florida: Add delay after SYSCLK patch
    When updating the write sequencer we must ensure there is a small
    delay before anything uses the write sequencer after the patch has
    been applied.
    
    Change-Id: Ia643033595a07cff3c4a90e526e75180b6e6d67f
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Mayuresh Kulkarni <mkulkarni@opensource.wolfsonmicro.com>

--- sound/soc/codecs/arizona.c
+++ sound/soc/codecs/arizona.c
@@ -1112,6 +1112,7 @@ static int florida_hp_pre_enable(struct snd_soc_dapm_widget *w)
 					       florida_no_dre_left_enable,
 					       ARRAY_SIZE(florida_no_dre_left_enable));
 		}
+		udelay(1000);
 		break;
 	case ARIZONA_OUT1R_ENA_SHIFT:
 		if (val & ARIZONA_DRE1R_ENA_MASK) {
@@ -1123,6 +1124,7 @@ static int florida_hp_pre_enable(struct snd_soc_dapm_widget *w)
 					       florida_no_dre_right_enable,
 					       ARRAY_SIZE(florida_no_dre_right_enable));
 		}
+		udelay(1000);
 		break;
 
 	default:
