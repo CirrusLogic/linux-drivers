8e67aa41c09028c047ab1eac4805130bd5c581c5
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2014-10-23 16:04:27 +0100

    ASoC: arizona: Add initial CS47L24 codec driver
    Change-Id: I9fa717aa274439284f2dc1a037ca443b37a07a95
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Vitaly Rodionov <vitalyr@opensource.wolfsonmicro.com>

--- sound/soc/codecs/arizona.c
+++ sound/soc/codecs/arizona.c
@@ -1,6 +1,7 @@
 /*
  * arizona.c - Wolfson Arizona class device shared support
  *
+ * Copyright 2014 Cirrus Logic
  * Copyright 2012 Wolfson Microelectronics plc
  *
  * Author: Mark Brown <broonie@opensource.wolfsonmicro.com>
@@ -103,6 +104,8 @@ static int arizona_spk_ev(struct snd_soc_dapm_widget *w,
 		switch (arizona->type) {
 		case WM8280:
 		case WM5110:
+		case WM1831:
+		case CS47L24:
 			msleep(10);
 			break;
 		default:
@@ -201,6 +204,8 @@ int arizona_init_spk(struct snd_soc_codec *codec)
 
 	switch (arizona->type) {
 	case WM8997:
+	case WM1831:
+	case CS47L24:
 		spkr = false;
 		break;
 	default:
@@ -300,7 +305,7 @@ int arizona_init_input(struct snd_soc_codec *codec)
 
 	memset(&routes, 0, sizeof(routes));
 
-	for (i = 0; i < ARRAY_SIZE(pdata->dmic_ref); ++i) {
+	for (i = 0; i < priv->num_inputs / 2; ++i) {
 		routes[0].source = arizona_dmic_refs[pdata->dmic_ref[i]];
 		routes[1].source = arizona_dmic_refs[pdata->dmic_ref[i]];
 
@@ -325,6 +330,8 @@ int arizona_init_gpio(struct snd_soc_codec *codec)
 	switch (arizona->type) {
 	case WM8280:
 	case WM5110:
+	case WM1831:
+	case CS47L24:
 		snd_soc_dapm_disable_pin(&codec->dapm, "DRC2 Signal Activity");
 		break;
 	default:
@@ -2315,6 +2322,8 @@ static int arizona_calc_fratio(struct arizona_fll *fll,
 		break;
 	case WM8998:
 	case WM1814:
+	case WM1831:
+	case CS47L24:
 		if (sync)
 			return init_ratio;
 		break;
