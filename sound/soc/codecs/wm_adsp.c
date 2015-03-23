2bffbbd7ed32272eaa84186db1bbe9f45aa02aae
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2015-03-23 14:01:42 +0000

    ASoC: wm_adsp: Fail if there are errors applying the binfile
    The code was logging errors during the binfile load but
    continuing with the load. The result is unlikely to be
    useful if we've failed to load all the data to the DSP so
    instead return an error and fail the load if there are
    any failures during loading the binfile.
    
    Change-Id: I824186d60c45ed3870a8aaa03a4ea50bba8b29b4
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -2052,7 +2052,8 @@ static int wm_adsp_load_coeff(struct wm_adsp *dsp)
 				mem = wm_adsp_find_region(dsp, type);
 				if (!mem) {
 					adsp_err(dsp, "No ZM\n");
-					break;
+					ret = -EINVAL;
+					goto out_fw;
 				}
 				reg = wm_adsp_region_to_reg(mem, 0);
 
@@ -2073,7 +2074,8 @@ static int wm_adsp_load_coeff(struct wm_adsp *dsp)
 			mem = wm_adsp_find_region(dsp, type);
 			if (!mem) {
 				adsp_err(dsp, "No base for region %x\n", type);
-				break;
+				ret = -EINVAL;
+				goto out_fw;
 			}
 
 			reg = 0;
@@ -2089,15 +2091,19 @@ static int wm_adsp_load_coeff(struct wm_adsp *dsp)
 				}
 			}
 
-			if (reg == 0)
+			if (reg == 0) {
 				adsp_err(dsp, "No %x for algorithm %x\n",
 					 type, le32_to_cpu(blk->id));
+				ret = -EINVAL;
+				goto out_fw;
+			}
 			break;
 
 		default:
 			adsp_err(dsp, "%s.%d: Unknown region type %x at %d\n",
 				 file, blocks, type, pos);
-			break;
+			ret = -EINVAL;
+			goto out_fw;
 		}
 
 		if (reg) {
@@ -2109,6 +2115,7 @@ static int wm_adsp_load_coeff(struct wm_adsp *dsp)
 				adsp_err(dsp,
 					"%s.%d: Failed to write to %x in %s: %d\n",
 					file, blocks, reg, region_name, ret);
+				goto out_fw;
 			}
 		}
 
