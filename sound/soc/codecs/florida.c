f1236f78e5b56f15e6eb5bf7436d3303f8279085
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2015-04-20 15:58:58 +0100

    ASoC: wm_adsp: Add basic debugfs entries
    This patch adds some debugfs nodes to get information
    about the currently running firmware.
    
    Change-Id: Idb42b550cdb6c40824fd51d143358ccf7b97bc74
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- sound/soc/codecs/florida.c
+++ sound/soc/codecs/florida.c
@@ -2143,9 +2143,12 @@ static int florida_codec_probe(struct snd_soc_codec *codec)
 {
 	struct florida_priv *priv = snd_soc_codec_get_drvdata(codec);
 	struct arizona *arizona = priv->core.arizona;
-	int ret;
+	int i, ret;
 
 	priv->core.arizona->dapm = &codec->dapm;
+
+	for (i = 0; i < FLORIDA_NUM_ADSP; i++)
+		wm_adsp_init_debugfs(&priv->core.adsp[i], codec);
 
 	arizona_init_spk(codec);
 	arizona_init_gpio(codec);
@@ -2191,6 +2194,10 @@ static int florida_codec_remove(struct snd_soc_codec *codec)
 {
 	struct florida_priv *priv = snd_soc_codec_get_drvdata(codec);
 	struct arizona *arizona = priv->core.arizona;
+	int i;
+
+	for (i = 0; i < FLORIDA_NUM_ADSP; i++)
+		wm_adsp_cleanup_debugfs(&priv->core.adsp[i]);
 
 	irq_set_irq_wake(arizona->irq, 0);
 	arizona_free_irq(arizona, ARIZONA_IRQ_DSP_IRQ1, priv);
