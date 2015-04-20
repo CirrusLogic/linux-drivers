0d503aaff63ebc12509346e1bb20c305908c401d
Nikesh Oswal <nikesh@opensource.wolfsonmicro.com>
2015-04-20 13:54:17 +0100

    mfd: arizona: add moon support
    For Moon
    
     - add spi and i2c support
     - verify device id register
     - add (register) mfd devices
     - register irq's
     - disable ldo1 (no ldo1 on moon)
     - apply rev-0 silicon patch
     - scan settings of children's micbiases
     - scan settings of dmic clock sources
     - scan settings of gnd field in micd_configs
    
    Additional Registers (as compared to clearwater) for
    
     -Micbias Switches
     -IN Rate Control
     -DMIC Clk Source
     -FLL_AO
     -MICDET2 Block
     -Additional Registers for MICDET1 and HPDET
     -DFC
     -DSP Region Locking
    
    DSP Memory regions (as compared to clearwater)
    
     -DSP1/6/7 has more memory
     -DSP4 has less memory
    
    Change-Id: I11dcc1762c3c6c9cd2b3e6ca433eec77c942706f
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- drivers/mfd/arizona-irq.c
+++ drivers/mfd/arizona-irq.c
@@ -282,6 +282,16 @@ int arizona_irq_init(struct arizona *arizona)
 		irq_ctrl_reg = CLEARWATER_IRQ1_CTRL;
 		break;
 #endif
+#ifdef CONFIG_MFD_MOON
+	case CS47L90:
+	case CS47L91:
+		aod = &moon_irq;
+		irq = NULL;
+
+		arizona->ctrlif_error = false;
+		irq_ctrl_reg = CLEARWATER_IRQ1_CTRL;
+		break;
+#endif
 	default:
 		BUG_ON("Unknown Arizona class device" == NULL);
 		return -EINVAL;
--- include/linux/mfd/arizona/pdata.h
+++ include/linux/mfd/arizona/pdata.h
@@ -51,6 +51,10 @@
 #define ARIZONA_MAX_INPUT 12
 
 #define ARIZONA_MAX_MICBIAS 4
+#define ARIZONA_MAX_CHILD_MICBIAS 4
+
+#define MARLEY_NUM_CHILD_MICBIAS 2
+#define MOON_NUM_CHILD_MICBIAS 4
 
 #define ARIZONA_MAX_OUTPUT 6
 
@@ -73,13 +77,15 @@ struct arizona_jd_state;
 struct arizona_micbias {
 	int mV;                    /** Regulated voltage */
 	unsigned int ext_cap:1;    /** External capacitor fitted */
-	unsigned int discharge:1;  /** Actively discharge */
+	/** Actively discharge */
+	unsigned int discharge[ARIZONA_MAX_CHILD_MICBIAS];
 	unsigned int soft_start:1; /** Disable aggressive startup ramp rate */
 	unsigned int bypass:1;     /** Use bypass mode */
 };
 
 struct arizona_micd_config {
 	unsigned int src;
+	unsigned int gnd;
 	unsigned int bias;
 	bool gpio;
 };
@@ -246,6 +252,9 @@ struct arizona_pdata {
 
 	/** Reference voltage for DMIC inputs */
 	int dmic_ref[ARIZONA_MAX_INPUT];
+
+	/** Clock Source for DMIC's */
+	int dmic_clksrc[ARIZONA_MAX_INPUT];
 
 	/** MICBIAS configurations */
 	struct arizona_micbias micbias[ARIZONA_MAX_MICBIAS];
