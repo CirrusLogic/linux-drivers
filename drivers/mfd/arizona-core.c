0d76900e33161f5bc714e8d183fa3bc2641aa8ad
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2015-05-28 10:09:35 +0100

    mfd: arizona: Fix type for clk32k device tree entry
    Corrects this Sparse warning:
    drivers/mfd/arizona-core.c
    warning 1405:34 incorrect type in argument 4 (different signedness)
    expected unsigned int [usertype] *data got int *<noident>
    
    Change-Id: I4d14e65d470650f2ec73171fda86fd741096f331
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- drivers/mfd/arizona-core.c
+++ drivers/mfd/arizona-core.c
@@ -1338,7 +1338,7 @@ static int arizona_of_get_core_pdata(struct arizona *arizona)
 	pdata->reset = arizona_of_get_named_gpio(arizona, "wlf,reset", true);
 	pdata->ldoena = arizona_of_get_named_gpio(arizona, "wlf,ldoena", true);
 
-	arizona_of_read_u32(arizona, "wlf,clk32k-src", false,
+	arizona_of_read_s32(arizona, "wlf,clk32k-src", false,
 				&pdata->clk32k_src);
 
 	arizona_of_get_micd_ranges(arizona, "wlf,micd-ranges");
