0308ba87eef8e683e33fd9f2b1c3d2818f7073b3
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2015-06-02 15:40:43 +0100

    ASoC: clearwater: Fix incorrect DRC2 interrupt masking
    The codec_probe() was masking the DRC2 interrupt where it should
    be unmasking it. The codec_remove was using the old Arizona interrupt
    mask register that doesn't exist on Clearwater and also was unmasking
    instead of masking.
    
    Change-Id: I975453c4724a78a4910b65b30e8462bf42252348
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- sound/soc/codecs/clearwater.c
+++ sound/soc/codecs/clearwater.c
@@ -2947,7 +2947,7 @@ static int clearwater_codec_probe(struct snd_soc_codec *codec)
 
 	ret = regmap_update_bits(arizona->regmap, CLEARWATER_IRQ2_MASK_9,
 				 CLEARWATER_DRC2_SIG_DET_EINT2,
-				 CLEARWATER_DRC2_SIG_DET_EINT2);
+				 0);
 	if (ret != 0) {
 		dev_err(arizona->dev,
 			"Failed to unmask DRC2 IRQ for DSP: %d\n",
@@ -2969,9 +2969,9 @@ static int clearwater_codec_remove(struct snd_soc_codec *codec)
 
 	irq_set_irq_wake(arizona->irq, 0);
 	arizona_free_irq(arizona, ARIZONA_IRQ_DSP_IRQ1, priv);
-	regmap_update_bits(arizona->regmap, ARIZONA_IRQ2_STATUS_3_MASK,
-			   ARIZONA_IM_DRC2_SIG_DET_EINT2,
-			   0);
+	regmap_update_bits(arizona->regmap, CLEARWATER_IRQ2_MASK_9,
+			   CLEARWATER_DRC2_SIG_DET_EINT2,
+			   CLEARWATER_DRC2_SIG_DET_EINT2);
 
 	priv->core.arizona->dapm = NULL;
 
