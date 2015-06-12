000a31d1e4976fa8f383b43b4b6986a92ebe5ad8
Piotr Stankiewicz <piotrs@opensource.wolfsonmicro.com>
2015-06-12 09:11:35 +0100

    ASoC: marley: initialize debugfs entries for DSP cores
    Change-Id: Iaa5fd0d43f0509e0e55946b1fabc3819c0181bd3
    Signed-off-by: Piotr Stankiewicz <piotrs@opensource.wolfsonmicro.com>

--- sound/soc/codecs/marley.c
+++ sound/soc/codecs/marley.c
@@ -2045,7 +2045,10 @@ static int marley_codec_probe(struct snd_soc_codec *codec)
 {
 	struct marley_priv *priv = snd_soc_codec_get_drvdata(codec);
 	struct arizona *arizona = priv->core.arizona;
-	int ret;
+	int i, ret;
+
+	for (i = 0; i < MARLEY_NUM_ADSP; ++i)
+		wm_adsp_init_debugfs(&priv->core.adsp[i], codec);
 
 	codec->control_data = priv->core.arizona->regmap;
 	priv->core.arizona->dapm = &codec->dapm;
@@ -2107,6 +2110,10 @@ static int marley_codec_remove(struct snd_soc_codec *codec)
 {
 	struct marley_priv *priv = snd_soc_codec_get_drvdata(codec);
 	struct arizona *arizona = priv->core.arizona;
+	int i;
+
+	for (i = 0; i < MARLEY_NUM_ADSP; ++i)
+		wm_adsp_cleanup_debugfs(&priv->core.adsp[i]);
 
 	irq_set_irq_wake(arizona->irq, 0);
 	arizona_free_irq(arizona, ARIZONA_IRQ_DSP_IRQ1, priv);
