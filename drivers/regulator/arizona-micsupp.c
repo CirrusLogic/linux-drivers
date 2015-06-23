6e4c1cec92bc60389cc247f5d6bfffdd2cd2941c
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2015-06-23 09:30:35 +0100

    regulator: arizona-micsupp: Always add CHANGE_BYPASS to DT init_data
    When reading the init_data from device tree the 3.10 kernel doesn't
    support REGULATOR_CHANGE_BYPASS. As there are few sensible use-cases for
    not having this simply add the flag to valid_ops_mask after reading the
    init_data from DT.
    
    Change-Id: I367d80739dd01480638e69d214c7a2fc550cb560
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- drivers/regulator/arizona-micsupp.c
+++ drivers/regulator/arizona-micsupp.c
@@ -254,6 +254,9 @@ static int arizona_micsupp_of_get_pdata(struct arizona *arizona,
 			init_data->consumer_supplies = &micsupp->supply;
 			init_data->num_consumer_supplies = 1;
 
+			init_data->constraints.valid_ops_mask |=
+					REGULATOR_CHANGE_BYPASS;
+
 			pdata->micvdd = init_data;
 		}
 	}
