569fdaf3bbfb2cb03e8dfceebb627dcf8063923a
Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
2015-07-01 11:45:59 +0100

    ASoC: moon: don't change osr when input is in analog mode
    In analog mode osr is fixed to the power on reset value of
    3.072MHz and setting it to any lower value significantly
    detoriates the audio quality so this patch blocks any
    changes of osr when input is in analog mode
    
    Change-Id: I874e021d8b04ad15a29cfdc972561d2f5ce8932b
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- sound/soc/codecs/arizona.h
+++ sound/soc/codecs/arizona.h
@@ -322,6 +322,9 @@ extern int moon_in_rate_put(struct snd_kcontrol *kcontrol,
 extern int moon_dfc_put(struct snd_kcontrol *kcontrol,
 	struct snd_ctl_elem_value *ucontrol);
 
+extern int moon_osr_put(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol);
+
 extern int arizona_put_anc_input(struct snd_kcontrol *kcontrol,
 			  struct snd_ctl_elem_value *ucontrol);
 
