a4238429829be9e6d53930be4b47e2fc84640fac
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2015-07-21 14:56:28 +0100

    switch: arizona: Don't treat older codecs as default IP version
    Add specific IP version cases for the WM8997, WM8998 and WM1814
    instead of using the default case. This allows the default to be
    for latest silicon IP.
    
    Change-Id: Ib0d67c071d458f6558ca11db33f4acc00e57e94c
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- drivers/switch/switch-arizona.c
+++ drivers/switch/switch-arizona.c
@@ -3747,6 +3747,10 @@ static int arizona_extcon_probe(struct platform_device *pdev)
 	arizona->extcon_info = info;
 
 	switch (arizona->type) {
+	case WM8997:
+		info->micd_clamp = true;
+		info->hpdet_ip_version = 1;
+		break;
 	case WM5102:
 		switch (arizona->rev) {
 		case 0:
@@ -3757,6 +3761,11 @@ static int arizona_extcon_probe(struct platform_device *pdev)
 			info->hpdet_ip_version = 1;
 			break;
 		}
+		break;
+	case WM8998:
+	case WM1814:
+		info->micd_clamp = true;
+		info->hpdet_ip_version = 2;
 		break;
 	case WM8280:
 	case WM5110:
