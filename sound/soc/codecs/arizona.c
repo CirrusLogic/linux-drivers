boundarydevices/linux

ASoC: arizona: Add function to set input mode
Sometimes Inputs may have both analog and digital microphone connected.
So in this case we need to select the input mode based on which microphone
is enabled for a particular usecase. This change adds the necessary enums
and functions to create an alsa control for input mode selection.

Change-Id: I92e7e21050e6f49eb1e321ca380712a7096ef120
Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

sound/soc/codecs/arizona.c
@@ -1360,6 +1360,57 @@ const struct soc_enum clearwater_output_anc_src_defs[] = {
 };
 EXPORT_SYMBOL_GPL(clearwater_output_anc_src_defs);
 
+const char *arizona_ip_mode_text[2] = {
+	"Analog", "Digital",
+};
+
+const struct soc_enum arizona_ip_mode[] = {
+	SOC_ENUM_SINGLE(ARIZONA_IN1L_CONTROL, ARIZONA_IN1_MODE_SHIFT,
+		ARRAY_SIZE(arizona_ip_mode_text), arizona_ip_mode_text),
+	SOC_ENUM_SINGLE(ARIZONA_IN2L_CONTROL, ARIZONA_IN2_MODE_SHIFT,
+		ARRAY_SIZE(arizona_ip_mode_text), arizona_ip_mode_text),
+	SOC_ENUM_SINGLE(ARIZONA_IN3L_CONTROL, ARIZONA_IN3_MODE_SHIFT,
+		ARRAY_SIZE(arizona_ip_mode_text), arizona_ip_mode_text),
+};
+EXPORT_SYMBOL_GPL(arizona_ip_mode);
+
+int arizona_ip_mode_put(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_codec *codec = snd_kcontrol_chip(kcontrol);
+	struct soc_enum *e = (struct soc_enum *)kcontrol->private_value;
+	unsigned int reg, ret = 0;
+
+	mutex_lock_nested(&codec->card->dapm_mutex, SND_SOC_DAPM_CLASS_RUNTIME);
+
+	/* Cannot change input mode on an active input*/
+	reg = snd_soc_read(codec, ARIZONA_INPUT_ENABLES);
+
+	switch (e->reg) {
+	case ARIZONA_IN1L_CONTROL:
+		if (reg & (ARIZONA_IN1L_ENA_MASK |ARIZONA_IN1R_ENA_MASK))
+			return -EBUSY;
+		break;
+	case ARIZONA_IN2L_CONTROL:
+		if (reg & (ARIZONA_IN2L_ENA_MASK |ARIZONA_IN2R_ENA_MASK))
+			return -EBUSY;
+		break;
+	case ARIZONA_IN3L_CONTROL:
+		if (reg & (ARIZONA_IN3L_ENA_MASK |ARIZONA_IN3R_ENA_MASK))
+			return -EBUSY;
+		break;
+	default:
+		return -EINVAL;
+		break;
+	}
+
+	ret = snd_soc_put_enum_double(kcontrol, ucontrol);
+	mutex_unlock(&codec->card->dapm_mutex);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(arizona_ip_mode_put);
+
 static void arizona_in_set_vu(struct snd_soc_codec *codec, int ena)
 {
 	struct arizona_priv *priv = snd_soc_codec_get_drvdata(codec);

CirrusLogic/linux-drivers

sound/soc/codecs/arizona.c
@@ -1415,6 +1415,57 @@ const struct soc_enum clearwater_output_anc_src_defs[] = {
 };
 EXPORT_SYMBOL_GPL(clearwater_output_anc_src_defs);
 
+const char *arizona_ip_mode_text[2] = {
+	"Analog", "Digital",
+};
+
+const struct soc_enum arizona_ip_mode[] = {
+	SOC_ENUM_SINGLE(ARIZONA_IN1L_CONTROL, ARIZONA_IN1_MODE_SHIFT,
+		ARRAY_SIZE(arizona_ip_mode_text), arizona_ip_mode_text),
+	SOC_ENUM_SINGLE(ARIZONA_IN2L_CONTROL, ARIZONA_IN2_MODE_SHIFT,
+		ARRAY_SIZE(arizona_ip_mode_text), arizona_ip_mode_text),
+	SOC_ENUM_SINGLE(ARIZONA_IN3L_CONTROL, ARIZONA_IN3_MODE_SHIFT,
+		ARRAY_SIZE(arizona_ip_mode_text), arizona_ip_mode_text),
+};
+EXPORT_SYMBOL_GPL(arizona_ip_mode);
+
+int arizona_ip_mode_put(struct snd_kcontrol *kcontrol,
+	struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
+	struct soc_enum *e = (struct soc_enum *)kcontrol->private_value;
+	unsigned int reg, ret = 0;
+
+	mutex_lock_nested(&codec->component.card->dapm_mutex, SND_SOC_DAPM_CLASS_RUNTIME);
+
+	/* Cannot change input mode on an active input*/
+	reg = snd_soc_read(codec, ARIZONA_INPUT_ENABLES);
+
+	switch (e->reg) {
+	case ARIZONA_IN1L_CONTROL:
+		if (reg & (ARIZONA_IN1L_ENA_MASK |ARIZONA_IN1R_ENA_MASK))
+			return -EBUSY;
+		break;
+	case ARIZONA_IN2L_CONTROL:
+		if (reg & (ARIZONA_IN2L_ENA_MASK |ARIZONA_IN2R_ENA_MASK))
+			return -EBUSY;
+		break;
+	case ARIZONA_IN3L_CONTROL:
+		if (reg & (ARIZONA_IN3L_ENA_MASK |ARIZONA_IN3R_ENA_MASK))
+			return -EBUSY;
+		break;
+	default:
+		return -EINVAL;
+		break;
+	}
+
+	ret = snd_soc_put_enum_double(kcontrol, ucontrol);
+	mutex_unlock(&codec->component.card->dapm_mutex);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(arizona_ip_mode_put);
+
 static void arizona_in_set_vu(struct snd_soc_codec *codec, int ena)
 {
 	struct arizona_priv *priv = snd_soc_codec_get_drvdata(codec);