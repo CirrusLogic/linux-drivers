boundarydevices/linux

ASoC: wm5102: Add input and output rate controls
Change-Id: I1f719234a7e9324f0a0eaf4730826f88a8d19dd6
Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

sound/soc/codecs/wm5102.c
@@ -960,6 +960,9 @@ SND_SOC_BYTES_EXT("Output Compensation Coefficient", 2,
 SOC_SINGLE_EXT("Output Compensation Switch", 0, 0, 1, 0,
 	       wm5102_out_comp_switch_get, wm5102_out_comp_switch_put),
 
+SOC_VALUE_ENUM("Output Rate 1", arizona_output_rate),
+SOC_VALUE_ENUM("In Rate", arizona_input_rate),
+
 WM5102_NG_SRC("HPOUT1L", ARIZONA_NOISE_GATE_SELECT_1L),
 WM5102_NG_SRC("HPOUT1R", ARIZONA_NOISE_GATE_SELECT_1R),
 WM5102_NG_SRC("HPOUT2L", ARIZONA_NOISE_GATE_SELECT_2L),

CirrusLogic/linux-drivers

sound/soc/codecs/wm5102.c
@@ -960,6 +960,9 @@ SND_SOC_BYTES_EXT("Output Compensation Coefficient", 2,
 SOC_SINGLE_EXT("Output Compensation Switch", 0, 0, 1, 0,
 	       wm5102_out_comp_switch_get, wm5102_out_comp_switch_put),
 
+SOC_ENUM("Output Rate 1", arizona_output_rate),
+SOC_ENUM("In Rate", arizona_input_rate),
+
 WM5102_NG_SRC("HPOUT1L", ARIZONA_NOISE_GATE_SELECT_1L),
 WM5102_NG_SRC("HPOUT1R", ARIZONA_NOISE_GATE_SELECT_1R),
 WM5102_NG_SRC("HPOUT2L", ARIZONA_NOISE_GATE_SELECT_2L),