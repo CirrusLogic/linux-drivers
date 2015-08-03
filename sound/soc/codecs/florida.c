kitakar5525/linux-kernel

ASoC: florida: Add DAPM/routing hookup for the ANC block
The Florida device contains a hardware ANC block, this patch connects up
controls and routing for this.

Change-Id: I344e3ad76b34620a8834ce1846c17efe66d1072f
Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

sound/soc/codecs/florida.c
@@ -264,6 +264,30 @@ static DECLARE_TLV_DB_SCALE(ng_tlv, -10200, 600, 0);
 	SOC_SINGLE(name " NG SPKDAT2L Switch", base, 10, 1, 0), \
 	SOC_SINGLE(name " NG SPKDAT2R Switch", base, 11, 1, 0)
 
+#define FLORIDA_RXANC_INPUT_ROUTES(widget, name) \
+	{ widget, NULL, name " Input" }, \
+	{ name " Input", "IN1L", "IN1L PGA" }, \
+	{ name " Input", "IN1R", "IN1R PGA" }, \
+	{ name " Input", "IN1L + IN1R", "IN1L PGA" }, \
+	{ name " Input", "IN1L + IN1R", "IN1R PGA" }, \
+	{ name " Input", "IN2L", "IN2L PGA" }, \
+	{ name " Input", "IN2R", "IN2R PGA" }, \
+	{ name " Input", "IN2L + IN2R", "IN2L PGA" }, \
+	{ name " Input", "IN2L + IN2R", "IN2R PGA" }, \
+	{ name " Input", "IN3L", "IN3L PGA" }, \
+	{ name " Input", "IN3R", "IN3R PGA" }, \
+	{ name " Input", "IN3L + IN3R", "IN3L PGA" }, \
+	{ name " Input", "IN3L + IN3R", "IN3R PGA" }, \
+	{ name " Input", "IN4L", "IN4L PGA" }, \
+	{ name " Input", "IN4R", "IN4R PGA" }, \
+	{ name " Input", "IN4L + IN4R", "IN4L PGA" }, \
+	{ name " Input", "IN4L + IN4R", "IN4R PGA" }
+
+#define FLORIDA_RXANC_OUTPUT_ROUTES(widget, name) \
+	{ widget, NULL, name " ANC Source" }, \
+	{ name " ANC Source", "RXANCL", "RXANCL" }, \
+	{ name " ANC Source", "RXANCR", "RXANCR" }
+
 static const struct snd_kcontrol_new florida_snd_controls[] = {
 SOC_ENUM("IN1 OSR", arizona_in_dmic_osr[0]),
 SOC_ENUM("IN2 OSR", arizona_in_dmic_osr[1]),
@@ -321,6 +345,18 @@ SOC_SINGLE_TLV("IN4R Digital Volume", ARIZONA_ADC_DIGITAL_VOLUME_4R,
 
 SOC_ENUM("Input Ramp Up", arizona_in_vi_ramp),
 SOC_ENUM("Input Ramp Down", arizona_in_vd_ramp),
+
+SND_SOC_BYTES_MASK("RXANC Config", ARIZONA_CLOCK_CONTROL, 1,
+		   ARIZONA_CLK_R_ENA_CLR | ARIZONA_CLK_R_ENA_SET |
+		   ARIZONA_CLK_L_ENA_CLR | ARIZONA_CLK_L_ENA_SET),
+SND_SOC_BYTES("RXANC Coefficients", ARIZONA_ANC_COEFF_START,
+	      ARIZONA_ANC_COEFF_END - ARIZONA_ANC_COEFF_START + 1),
+SND_SOC_BYTES("RXANCL Config", ARIZONA_FCL_FILTER_CONTROL, 1),
+SND_SOC_BYTES("RXANCL Coefficients", ARIZONA_FCL_COEFF_START,
+	      ARIZONA_FCL_COEFF_END - ARIZONA_FCL_COEFF_START + 1),
+SND_SOC_BYTES("RXANCR Config", ARIZONA_FCR_FILTER_CONTROL, 1),
+SND_SOC_BYTES("RXANCR Coefficients", ARIZONA_FCR_COEFF_START,
+	      ARIZONA_FCR_COEFF_END - ARIZONA_FCR_COEFF_START + 1),
 
 ARIZONA_MIXER_CONTROLS("EQ1", ARIZONA_EQ1MIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("EQ2", ARIZONA_EQ2MIX_INPUT_1_SOURCE),
@@ -711,6 +747,28 @@ static const struct soc_enum florida_aec_loopback =
 static const struct snd_kcontrol_new florida_aec_loopback_mux =
 	SOC_DAPM_VALUE_ENUM("AEC Loopback", florida_aec_loopback);
 
+static const struct snd_kcontrol_new florida_anc_input_mux[] = {
+	SOC_DAPM_ENUM_EXT("RXANCL Input", arizona_anc_input_src[0],
+			  snd_soc_dapm_get_enum_virt, arizona_put_anc_input),
+	SOC_DAPM_ENUM_EXT("RXANCR Input", arizona_anc_input_src[1],
+			  snd_soc_dapm_get_enum_virt, arizona_put_anc_input),
+};
+
+static const struct snd_kcontrol_new florida_output_anc_src[] = {
+	SOC_DAPM_ENUM("HPOUT1L ANC Source", arizona_output_anc_src[0]),
+	SOC_DAPM_ENUM("HPOUT1R ANC Source", arizona_output_anc_src[1]),
+	SOC_DAPM_ENUM("HPOUT2L ANC Source", arizona_output_anc_src[2]),
+	SOC_DAPM_ENUM("HPOUT2R ANC Source", arizona_output_anc_src[3]),
+	SOC_DAPM_ENUM("HPOUT3L ANC Source", arizona_output_anc_src[4]),
+	SOC_DAPM_ENUM("HPOUT3R ANC Source", arizona_output_anc_src[5]),
+	SOC_DAPM_ENUM("SPKOUTL ANC Source", arizona_output_anc_src[6]),
+	SOC_DAPM_ENUM("SPKOUTR ANC Source", arizona_output_anc_src[7]),
+	SOC_DAPM_ENUM("SPKDAT1L ANC Source", arizona_output_anc_src[8]),
+	SOC_DAPM_ENUM("SPKDAT1R ANC Source", arizona_output_anc_src[9]),
+	SOC_DAPM_ENUM("SPKDAT2L ANC Source", arizona_output_anc_src[10]),
+	SOC_DAPM_ENUM("SPKDAT2R ANC Source", arizona_output_anc_src[11]),
+};
+
 static const struct snd_soc_dapm_widget florida_dapm_widgets[] = {
 SND_SOC_DAPM_SUPPLY("SYSCLK", ARIZONA_SYSTEM_CLOCK_1, ARIZONA_SYSCLK_ENA_SHIFT,
 		    0, florida_sysclk_ev, SND_SOC_DAPM_POST_PMU),
@@ -896,6 +954,41 @@ SND_SOC_DAPM_PGA("ISRC3DEC4", ARIZONA_ISRC_3_CTRL_3,
 SND_SOC_DAPM_VALUE_MUX("AEC Loopback", ARIZONA_DAC_AEC_CONTROL_1,
 		       ARIZONA_AEC_LOOPBACK_ENA_SHIFT, 0,
 		       &florida_aec_loopback_mux),
+
+SND_SOC_DAPM_MUX("RXANCL Input", SND_SOC_NOPM, 0, 0, &florida_anc_input_mux[0]),
+SND_SOC_DAPM_MUX("RXANCR Input", SND_SOC_NOPM, 0, 0, &florida_anc_input_mux[1]),
+
+SND_SOC_DAPM_PGA_E("RXANCL", SND_SOC_NOPM, ARIZONA_CLK_L_ENA_SET_SHIFT,
+		   0, NULL, 0, arizona_anc_ev,
+		   SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_PRE_PMD),
+SND_SOC_DAPM_PGA_E("RXANCR", SND_SOC_NOPM, ARIZONA_CLK_R_ENA_SET_SHIFT,
+		   0, NULL, 0, arizona_anc_ev,
+		   SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_PRE_PMD),
+
+SND_SOC_DAPM_MUX("HPOUT1L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[0]),
+SND_SOC_DAPM_MUX("HPOUT1R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[1]),
+SND_SOC_DAPM_MUX("HPOUT2L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[2]),
+SND_SOC_DAPM_MUX("HPOUT2R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[3]),
+SND_SOC_DAPM_MUX("HPOUT3L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[4]),
+SND_SOC_DAPM_MUX("HPOUT3R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[5]),
+SND_SOC_DAPM_MUX("SPKOUTL ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[6]),
+SND_SOC_DAPM_MUX("SPKOUTR ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[7]),
+SND_SOC_DAPM_MUX("SPKDAT1L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[8]),
+SND_SOC_DAPM_MUX("SPKDAT1R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[9]),
+SND_SOC_DAPM_MUX("SPKDAT2L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[10]),
+SND_SOC_DAPM_MUX("SPKDAT2R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[11]),
 
 SND_SOC_DAPM_AIF_OUT("AIF1TX1", NULL, 0,
 		     ARIZONA_AIF1_TX_ENABLES, ARIZONA_AIF1TX1_ENA_SHIFT, 0),
@@ -1573,6 +1666,22 @@ static const struct snd_soc_dapm_route florida_dapm_routes[] = {
 	{ "AEC Loopback", "SPKDAT2R", "OUT6R" },
 	{ "SPKDAT2L", NULL, "OUT6L" },
 	{ "SPKDAT2R", NULL, "OUT6R" },
+
+	FLORIDA_RXANC_INPUT_ROUTES("RXANCL", "RXANCL"),
+	FLORIDA_RXANC_INPUT_ROUTES("RXANCR", "RXANCR"),
+
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT1L", "HPOUT1L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT1R", "HPOUT1R"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT2L", "HPOUT2L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT2R", "HPOUT2R"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT3L", "HPOUT3L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT3R", "HPOUT3R"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT4L", "SPKOUTL"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT4R", "SPKOUTR"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT5L", "SPKDAT1L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT5R", "SPKDAT1R"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT6L", "SPKDAT2L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT6R", "SPKDAT2R"),
 
 	{ "MICSUPP", NULL, "SYSCLK" },
 

CirrusLogic/linux-drivers

Signed-off-by: Mayuresh Kulkarni <mkulkarni@opensource.wolfsonmicro.com>
Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

sound/soc/codecs/florida.c
@@ -264,6 +264,30 @@ static DECLARE_TLV_DB_SCALE(ng_tlv, -10200, 600, 0);
 	SOC_SINGLE(name " NG SPKDAT2L Switch", base, 10, 1, 0), \
 	SOC_SINGLE(name " NG SPKDAT2R Switch", base, 11, 1, 0)
 
+#define FLORIDA_RXANC_INPUT_ROUTES(widget, name) \
+	{ widget, NULL, name " Input" }, \
+	{ name " Input", "IN1L", "IN1L PGA" }, \
+	{ name " Input", "IN1R", "IN1R PGA" }, \
+	{ name " Input", "IN1L + IN1R", "IN1L PGA" }, \
+	{ name " Input", "IN1L + IN1R", "IN1R PGA" }, \
+	{ name " Input", "IN2L", "IN2L PGA" }, \
+	{ name " Input", "IN2R", "IN2R PGA" }, \
+	{ name " Input", "IN2L + IN2R", "IN2L PGA" }, \
+	{ name " Input", "IN2L + IN2R", "IN2R PGA" }, \
+	{ name " Input", "IN3L", "IN3L PGA" }, \
+	{ name " Input", "IN3R", "IN3R PGA" }, \
+	{ name " Input", "IN3L + IN3R", "IN3L PGA" }, \
+	{ name " Input", "IN3L + IN3R", "IN3R PGA" }, \
+	{ name " Input", "IN4L", "IN4L PGA" }, \
+	{ name " Input", "IN4R", "IN4R PGA" }, \
+	{ name " Input", "IN4L + IN4R", "IN4L PGA" }, \
+	{ name " Input", "IN4L + IN4R", "IN4R PGA" }
+
+#define FLORIDA_RXANC_OUTPUT_ROUTES(widget, name) \
+	{ widget, NULL, name " ANC Source" }, \
+	{ name " ANC Source", "RXANCL", "RXANCL" }, \
+	{ name " ANC Source", "RXANCR", "RXANCR" }
+
 static const struct snd_kcontrol_new florida_snd_controls[] = {
 SOC_ENUM("IN1 OSR", arizona_in_dmic_osr[0]),
 SOC_ENUM("IN2 OSR", arizona_in_dmic_osr[1]),
@@ -321,6 +345,18 @@ SOC_SINGLE_TLV("IN4R Digital Volume", ARIZONA_ADC_DIGITAL_VOLUME_4R,
 
 SOC_ENUM("Input Ramp Up", arizona_in_vi_ramp),
 SOC_ENUM("Input Ramp Down", arizona_in_vd_ramp),
+
+SND_SOC_BYTES_MASK("RXANC Config", ARIZONA_CLOCK_CONTROL, 1,
+		   ARIZONA_CLK_R_ENA_CLR | ARIZONA_CLK_R_ENA_SET |
+		   ARIZONA_CLK_L_ENA_CLR | ARIZONA_CLK_L_ENA_SET),
+SND_SOC_BYTES("RXANC Coefficients", ARIZONA_ANC_COEFF_START,
+	      ARIZONA_ANC_COEFF_END - ARIZONA_ANC_COEFF_START + 1),
+SND_SOC_BYTES("RXANCL Config", ARIZONA_FCL_FILTER_CONTROL, 1),
+SND_SOC_BYTES("RXANCL Coefficients", ARIZONA_FCL_COEFF_START,
+	      ARIZONA_FCL_COEFF_END - ARIZONA_FCL_COEFF_START + 1),
+SND_SOC_BYTES("RXANCR Config", ARIZONA_FCR_FILTER_CONTROL, 1),
+SND_SOC_BYTES("RXANCR Coefficients", ARIZONA_FCR_COEFF_START,
+	      ARIZONA_FCR_COEFF_END - ARIZONA_FCR_COEFF_START + 1),
 
 ARIZONA_MIXER_CONTROLS("EQ1", ARIZONA_EQ1MIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("EQ2", ARIZONA_EQ2MIX_INPUT_1_SOURCE),
@@ -711,6 +747,28 @@ static const struct soc_enum florida_aec_loopback =
 static const struct snd_kcontrol_new florida_aec_loopback_mux =
 	SOC_DAPM_ENUM("AEC Loopback", florida_aec_loopback);
 
+static const struct snd_kcontrol_new florida_anc_input_mux[] = {
+	SOC_DAPM_ENUM_EXT("RXANCL Input", arizona_anc_input_src[0],
+			  arizona_get_anc_input, arizona_put_anc_input),
+	SOC_DAPM_ENUM_EXT("RXANCR Input", arizona_anc_input_src[1],
+			  arizona_get_anc_input, arizona_put_anc_input),
+};
+
+static const struct snd_kcontrol_new florida_output_anc_src[] = {
+	SOC_DAPM_ENUM("HPOUT1L ANC Source", arizona_output_anc_src[0]),
+	SOC_DAPM_ENUM("HPOUT1R ANC Source", arizona_output_anc_src[1]),
+	SOC_DAPM_ENUM("HPOUT2L ANC Source", arizona_output_anc_src[2]),
+	SOC_DAPM_ENUM("HPOUT2R ANC Source", arizona_output_anc_src[3]),
+	SOC_DAPM_ENUM("HPOUT3L ANC Source", arizona_output_anc_src[4]),
+	SOC_DAPM_ENUM("HPOUT3R ANC Source", arizona_output_anc_src[5]),
+	SOC_DAPM_ENUM("SPKOUTL ANC Source", arizona_output_anc_src[6]),
+	SOC_DAPM_ENUM("SPKOUTR ANC Source", arizona_output_anc_src[7]),
+	SOC_DAPM_ENUM("SPKDAT1L ANC Source", arizona_output_anc_src[8]),
+	SOC_DAPM_ENUM("SPKDAT1R ANC Source", arizona_output_anc_src[9]),
+	SOC_DAPM_ENUM("SPKDAT2L ANC Source", arizona_output_anc_src[10]),
+	SOC_DAPM_ENUM("SPKDAT2R ANC Source", arizona_output_anc_src[11]),
+};
+
 static const struct snd_soc_dapm_widget florida_dapm_widgets[] = {
 SND_SOC_DAPM_SUPPLY("SYSCLK", ARIZONA_SYSTEM_CLOCK_1, ARIZONA_SYSCLK_ENA_SHIFT,
 		    0, florida_sysclk_ev, SND_SOC_DAPM_POST_PMU),
@@ -896,6 +954,41 @@ SND_SOC_DAPM_PGA("ISRC3DEC4", ARIZONA_ISRC_3_CTRL_3,
 SND_SOC_DAPM_MUX("AEC Loopback", ARIZONA_DAC_AEC_CONTROL_1,
 		       ARIZONA_AEC_LOOPBACK_ENA_SHIFT, 0,
 		       &florida_aec_loopback_mux),
+
+SND_SOC_DAPM_MUX("RXANCL Input", SND_SOC_NOPM, 0, 0, &florida_anc_input_mux[0]),
+SND_SOC_DAPM_MUX("RXANCR Input", SND_SOC_NOPM, 0, 0, &florida_anc_input_mux[1]),
+
+SND_SOC_DAPM_PGA_E("RXANCL", SND_SOC_NOPM, ARIZONA_CLK_L_ENA_SET_SHIFT,
+		   0, NULL, 0, arizona_anc_ev,
+		   SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_PRE_PMD),
+SND_SOC_DAPM_PGA_E("RXANCR", SND_SOC_NOPM, ARIZONA_CLK_R_ENA_SET_SHIFT,
+		   0, NULL, 0, arizona_anc_ev,
+		   SND_SOC_DAPM_POST_PMU | SND_SOC_DAPM_PRE_PMD),
+
+SND_SOC_DAPM_MUX("HPOUT1L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[0]),
+SND_SOC_DAPM_MUX("HPOUT1R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[1]),
+SND_SOC_DAPM_MUX("HPOUT2L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[2]),
+SND_SOC_DAPM_MUX("HPOUT2R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[3]),
+SND_SOC_DAPM_MUX("HPOUT3L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[4]),
+SND_SOC_DAPM_MUX("HPOUT3R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[5]),
+SND_SOC_DAPM_MUX("SPKOUTL ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[6]),
+SND_SOC_DAPM_MUX("SPKOUTR ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[7]),
+SND_SOC_DAPM_MUX("SPKDAT1L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[8]),
+SND_SOC_DAPM_MUX("SPKDAT1R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[9]),
+SND_SOC_DAPM_MUX("SPKDAT2L ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[10]),
+SND_SOC_DAPM_MUX("SPKDAT2R ANC Source", SND_SOC_NOPM, 0, 0,
+		 &florida_output_anc_src[11]),
 
 SND_SOC_DAPM_AIF_OUT("AIF1TX1", NULL, 0,
 		     ARIZONA_AIF1_TX_ENABLES, ARIZONA_AIF1TX1_ENA_SHIFT, 0),
@@ -1573,6 +1666,22 @@ static const struct snd_soc_dapm_route florida_dapm_routes[] = {
 	{ "AEC Loopback", "SPKDAT2R", "OUT6R" },
 	{ "SPKDAT2L", NULL, "OUT6L" },
 	{ "SPKDAT2R", NULL, "OUT6R" },
+
+	FLORIDA_RXANC_INPUT_ROUTES("RXANCL", "RXANCL"),
+	FLORIDA_RXANC_INPUT_ROUTES("RXANCR", "RXANCR"),
+
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT1L", "HPOUT1L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT1R", "HPOUT1R"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT2L", "HPOUT2L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT2R", "HPOUT2R"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT3L", "HPOUT3L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT3R", "HPOUT3R"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT4L", "SPKOUTL"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT4R", "SPKOUTR"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT5L", "SPKDAT1L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT5R", "SPKDAT1R"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT6L", "SPKDAT2L"),
+	FLORIDA_RXANC_OUTPUT_ROUTES("OUT6R", "SPKDAT2R"),
 
 	{ "MICSUPP", NULL, "SYSCLK" },
 