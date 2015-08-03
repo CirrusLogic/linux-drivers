boundarydevices/linux

ASoC: arizona: Add support for ASRC RATE 2
Change-Id: Iba00848fde7f79ebed103f67e3e1455d9b0fe869
Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

sound/soc/codecs/cs47l24.c
@@ -195,6 +195,7 @@ SOC_VALUE_ENUM("ISRC1 FSH", arizona_isrc_fsh[0]),
 SOC_VALUE_ENUM("ISRC2 FSH", arizona_isrc_fsh[1]),
 SOC_VALUE_ENUM("ISRC3 FSH", arizona_isrc_fsh[2]),
 SOC_VALUE_ENUM("ASRC RATE 1", arizona_asrc_rate1),
+SOC_VALUE_ENUM("ASRC RATE 2", arizona_asrc_rate2),
 
 ARIZONA_MIXER_CONTROLS("DSP2L", ARIZONA_DSP2LMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("DSP2R", ARIZONA_DSP2RMIX_INPUT_1_SOURCE),

sound/soc/codecs/florida.c
@@ -445,6 +445,7 @@ SOC_VALUE_ENUM("ISRC1 FSH", arizona_isrc_fsh[0]),
 SOC_VALUE_ENUM("ISRC2 FSH", arizona_isrc_fsh[1]),
 SOC_VALUE_ENUM("ISRC3 FSH", arizona_isrc_fsh[2]),
 SOC_VALUE_ENUM("ASRC RATE 1", arizona_asrc_rate1),
+SOC_VALUE_ENUM("ASRC RATE 2", arizona_asrc_rate2),
 
 ARIZONA_MIXER_CONTROLS("DSP1L", ARIZONA_DSP1LMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("DSP1R", ARIZONA_DSP1RMIX_INPUT_1_SOURCE),

sound/soc/codecs/wm5102.c
@@ -878,6 +878,7 @@ SOC_VALUE_ENUM("FX Rate", arizona_fx_rate),
 SOC_VALUE_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_VALUE_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
 SOC_VALUE_ENUM("ASRC RATE 1", arizona_asrc_rate1),
+SOC_VALUE_ENUM("ASRC RATE 2", arizona_asrc_rate2),
 
 ARIZONA_MIXER_CONTROLS("Mic", ARIZONA_MICMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("Noise", ARIZONA_NOISEMIX_INPUT_1_SOURCE),

sound/soc/codecs/wm8998.c
@@ -200,6 +200,7 @@ SOC_VALUE_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
 SOC_VALUE_ENUM("ISRC1 FSH", arizona_isrc_fsh[0]),
 SOC_VALUE_ENUM("ISRC2 FSH", arizona_isrc_fsh[1]),
 SOC_VALUE_ENUM("ASRC RATE 1", arizona_asrc_rate1),
+SOC_VALUE_ENUM("ASRC RATE 2", arizona_asrc_rate2),
 
 SOC_SINGLE_TLV("Noise Generator Volume", ARIZONA_COMFORT_NOISE_GENERATOR,
 	       ARIZONA_NOISE_GEN_GAIN_SHIFT, 0x16, 0, noise_tlv),

CirrusLogic/linux-drivers

sound/soc/codecs/cs47l24.c
@@ -195,6 +195,7 @@ SOC_ENUM("ISRC1 FSH", arizona_isrc_fsh[0]),
 SOC_ENUM("ISRC2 FSH", arizona_isrc_fsh[1]),
 SOC_ENUM("ISRC3 FSH", arizona_isrc_fsh[2]),
 SOC_ENUM("ASRC RATE 1", arizona_asrc_rate1),
+SOC_ENUM("ASRC RATE 2", arizona_asrc_rate2),
 
 ARIZONA_MIXER_CONTROLS("DSP2L", ARIZONA_DSP2LMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("DSP2R", ARIZONA_DSP2RMIX_INPUT_1_SOURCE),

sound/soc/codecs/florida.c
@@ -440,6 +440,7 @@ SOC_ENUM("ISRC1 FSH", arizona_isrc_fsh[0]),
 SOC_ENUM("ISRC2 FSH", arizona_isrc_fsh[1]),
 SOC_ENUM("ISRC3 FSH", arizona_isrc_fsh[2]),
 SOC_ENUM("ASRC RATE 1", arizona_asrc_rate1),
+SOC_ENUM("ASRC RATE 2", arizona_asrc_rate2),
 
 ARIZONA_SAMPLE_RATE_CONTROL("Sample Rate 2", 2),
 ARIZONA_SAMPLE_RATE_CONTROL("Sample Rate 3", 3),

sound/soc/codecs/wm5102.c
@@ -876,6 +876,7 @@ ARIZONA_SAMPLE_RATE_CONTROL_DVFS("Sample Rate 3", 3),
 SOC_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
 SOC_ENUM("ASRC RATE 1", arizona_asrc_rate1),
+SOC_ENUM("ASRC RATE 2", arizona_asrc_rate2),
 
 SOC_ENUM("FX Rate", arizona_fx_rate),
 

sound/soc/codecs/wm8998.c
@@ -200,6 +200,7 @@ SOC_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
 SOC_ENUM("ISRC1 FSH", arizona_isrc_fsh[0]),
 SOC_ENUM("ISRC2 FSH", arizona_isrc_fsh[1]),
 SOC_ENUM("ASRC RATE 1", arizona_asrc_rate1),
+SOC_ENUM("ASRC RATE 2", arizona_asrc_rate2),
 
 SOC_SINGLE_TLV("Noise Generator Volume", ARIZONA_COMFORT_NOISE_GENERATOR,
 	       ARIZONA_NOISE_GEN_GAIN_SHIFT, 0x16, 0, noise_tlv),