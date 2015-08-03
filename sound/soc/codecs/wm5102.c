boundarydevices/linux

ASoC: arizona: Add support for ASRC RATE 1
Some CODECs are missing a control for ASRC Rate 1, add this to the
CODECs that are missing it, but support it.

Change-Id: Ifa100ab1f075e326b2e85ff3513d75b15a1f4a44
Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

sound/soc/codecs/wm5102.c
@@ -877,6 +877,7 @@ SOC_VALUE_ENUM("FX Rate", arizona_fx_rate),
 
 SOC_VALUE_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_VALUE_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
+SOC_VALUE_ENUM("ASRC RATE 1", arizona_asrc_rate1),
 
 ARIZONA_MIXER_CONTROLS("Mic", ARIZONA_MICMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("Noise", ARIZONA_NOISEMIX_INPUT_1_SOURCE),

sound/soc/codecs/wm8997.c
@@ -241,6 +241,7 @@ SND_SOC_BYTES("LHPF4 Coefficients", ARIZONA_HPLPF4_2, 1),
 
 SOC_VALUE_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_VALUE_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
+SOC_VALUE_ENUM("ASRC RATE 1", arizona_asrc_rate1),
 
 ARIZONA_MIXER_CONTROLS("Mic", ARIZONA_MICMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("Noise", ARIZONA_NOISEMIX_INPUT_1_SOURCE),

CirrusLogic/linux-drivers

sound/soc/codecs/wm5102.c
@@ -875,6 +875,7 @@ ARIZONA_SAMPLE_RATE_CONTROL_DVFS("Sample Rate 3", 3),
 
 SOC_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
+SOC_ENUM("ASRC RATE 1", arizona_asrc_rate1),
 
 SOC_ENUM("FX Rate", arizona_fx_rate),
 

sound/soc/codecs/wm8997.c
@@ -251,6 +251,7 @@ SND_SOC_BYTES("LHPF4 Coefficients", ARIZONA_HPLPF4_2, 1),
 
 SOC_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
+SOC_ENUM("ASRC RATE 1", arizona_asrc_rate1),
 
 ARIZONA_MIXER_CONTROLS("Mic", ARIZONA_MICMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("Noise", ARIZONA_NOISEMIX_INPUT_1_SOURCE),