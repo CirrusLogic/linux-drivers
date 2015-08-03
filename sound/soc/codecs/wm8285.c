boundarydevices/linux

ASoC: wm8285: Add ASRC{1,2}_RATE2 controls
Change-Id: Id582de6d7698f5588fa31c46d4859d713d62509a
Signed-off-by: Nariman Poushin <nariman@opensource.wolfsonmicro.com>

sound/soc/codecs/wm8285.c
@@ -494,6 +494,7 @@ SOC_ENUM("LHPF4 Mode", arizona_lhpf4_mode),
 
 SOC_VALUE_ENUM("Sample Rate 2", arizona_sample_rate[0]),
 SOC_VALUE_ENUM("Sample Rate 3", arizona_sample_rate[1]),
+SOC_VALUE_ENUM("ASYNC Sample Rate 2", arizona_sample_rate[2]),
 
 SOC_VALUE_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_VALUE_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
@@ -503,8 +504,10 @@ SOC_VALUE_ENUM("ISRC1 FSH", arizona_isrc_fsh[0]),
 SOC_VALUE_ENUM("ISRC2 FSH", arizona_isrc_fsh[1]),
 SOC_VALUE_ENUM("ISRC3 FSH", arizona_isrc_fsh[2]),
 SOC_VALUE_ENUM("ISRC4 FSH", arizona_isrc_fsh[3]),
-SOC_VALUE_ENUM("ASRC1 Rate 1", wm8285_asrc1_rate1),
-SOC_VALUE_ENUM("ASRC2 Rate 1", wm8285_asrc2_rate1),
+SOC_VALUE_ENUM("ASRC1 Rate 1", wm8285_asrc1_rate[0]),
+SOC_VALUE_ENUM("ASRC1 Rate 2", wm8285_asrc1_rate[1]),
+SOC_VALUE_ENUM("ASRC2 Rate 1", wm8285_asrc2_rate[0]),
+SOC_VALUE_ENUM("ASRC2 Rate 2", wm8285_asrc2_rate[1]),
 
 ARIZONA_MIXER_CONTROLS("DSP1L", ARIZONA_DSP1LMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("DSP1R", ARIZONA_DSP1RMIX_INPUT_1_SOURCE),

CirrusLogic/linux-drivers

sound/soc/codecs/wm8285.c
@@ -495,6 +495,7 @@ SOC_ENUM("LHPF4 Mode", arizona_lhpf4_mode),
 
 SOC_ENUM("Sample Rate 2", arizona_sample_rate[0]),
 SOC_ENUM("Sample Rate 3", arizona_sample_rate[1]),
+SOC_ENUM("ASYNC Sample Rate 2", arizona_sample_rate[2]),
 
 SOC_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
@@ -504,8 +505,10 @@ SOC_ENUM("ISRC1 FSH", arizona_isrc_fsh[0]),
 SOC_ENUM("ISRC2 FSH", arizona_isrc_fsh[1]),
 SOC_ENUM("ISRC3 FSH", arizona_isrc_fsh[2]),
 SOC_ENUM("ISRC4 FSH", arizona_isrc_fsh[3]),
-SOC_ENUM("ASRC1 Rate 1", wm8285_asrc1_rate1),
-SOC_ENUM("ASRC2 Rate 1", wm8285_asrc2_rate1),
+SOC_ENUM("ASRC1 Rate 1", wm8285_asrc1_rate[0]),
+SOC_ENUM("ASRC1 Rate 2", wm8285_asrc1_rate[1]),
+SOC_ENUM("ASRC2 Rate 1", wm8285_asrc2_rate[0]),
+SOC_ENUM("ASRC2 Rate 2", wm8285_asrc2_rate[1]),
 
 ARIZONA_MIXER_CONTROLS("DSP1L", ARIZONA_DSP1LMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("DSP1R", ARIZONA_DSP1RMIX_INPUT_1_SOURCE),