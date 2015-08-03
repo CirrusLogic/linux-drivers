kitakar5525/linux-kernel

ASoC: arizona: Add support for FX rate selection
Change-Id: I1095e25a6c4c587920bab298a12108b37d40dbee
Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

sound/soc/codecs/florida.c
@@ -443,6 +443,8 @@ SOC_ENUM("LHPF4 Mode", arizona_lhpf4_mode),
 ARIZONA_SAMPLE_RATE_CONTROL("Sample Rate 2", 2),
 ARIZONA_SAMPLE_RATE_CONTROL("Sample Rate 3", 3),
 
+SOC_VALUE_ENUM("FX Rate", arizona_fx_rate),
+
 SOC_VALUE_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_VALUE_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
 SOC_VALUE_ENUM("ISRC3 FSL", arizona_isrc_fsl[2]),

sound/soc/codecs/wm5102.c
@@ -877,6 +877,8 @@ SOC_ENUM("LHPF4 Mode", arizona_lhpf4_mode),
 ARIZONA_SAMPLE_RATE_CONTROL_DVFS("Sample Rate 2", 2),
 ARIZONA_SAMPLE_RATE_CONTROL_DVFS("Sample Rate 3", 3),
 
+SOC_VALUE_ENUM("FX Rate", arizona_fx_rate),
+
 SOC_VALUE_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_VALUE_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
 

CirrusLogic/linux-drivers

Signed-off-by: Mayuresh Kulkarni <mkulkarni@opensource.wolfsonmicro.com>

sound/soc/codecs/florida.c
@@ -451,6 +451,8 @@ SOC_ENUM("ASRC RATE 1", arizona_asrc_rate1),
 ARIZONA_SAMPLE_RATE_CONTROL("Sample Rate 2", 2),
 ARIZONA_SAMPLE_RATE_CONTROL("Sample Rate 3", 3),
 
+SOC_ENUM("FX Rate", arizona_fx_rate),
+
 ARIZONA_MIXER_CONTROLS("DSP1L", ARIZONA_DSP1LMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("DSP1R", ARIZONA_DSP1RMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("DSP2L", ARIZONA_DSP2LMIX_INPUT_1_SOURCE),

sound/soc/codecs/wm5102.c
@@ -880,6 +880,8 @@ ARIZONA_SAMPLE_RATE_CONTROL_DVFS("Sample Rate 3", 3),
 SOC_ENUM("ISRC1 FSL", arizona_isrc_fsl[0]),
 SOC_ENUM("ISRC2 FSL", arizona_isrc_fsl[1]),
 
+SOC_ENUM("FX Rate", arizona_fx_rate),
+
 ARIZONA_MIXER_CONTROLS("Mic", ARIZONA_MICMIX_INPUT_1_SOURCE),
 ARIZONA_MIXER_CONTROLS("Noise", ARIZONA_NOISEMIX_INPUT_1_SOURCE),
 