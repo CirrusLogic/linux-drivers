cac6d597551063efc4d5afbfb1ae41e5f0897a8d
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2015-08-05 15:41:17 +0100

    mfd: arizona: CS47L24 and WM1831 don't have ISOLATION_CONTROL register
    The runtime_suspend and runtime_resume were attempting to access
    the ISOLATION_CONTROL register on CS47L24 and WM1831, which don't
    have this register and so the code was failing with a regmap error.
    
    Change-Id: I343ad04702fc0c820fc87add5b54391a943f2625
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- drivers/mfd/arizona-core.c
+++ drivers/mfd/arizona-core.c
@@ -625,8 +625,6 @@ static int arizona_runtime_resume(struct device *dev)
 	case WM8997:
 	case WM8998:
 	case WM1814:
-	case WM1831:
-	case CS47L24:
 		ret = arizona_wait_for_boot(arizona);
 		if (ret != 0) {
 			goto err;
@@ -641,6 +639,13 @@ static int arizona_runtime_resume(struct device *dev)
 					"Failed to connect DCVDD: %d\n", ret);
 				goto err;
 			}
+		}
+		break;
+	case WM1831:
+	case CS47L24:
+		ret = arizona_wait_for_boot(arizona);
+		if (ret != 0) {
+			goto err;
 		}
 		break;
 	default:
@@ -707,8 +712,6 @@ static int arizona_runtime_suspend(struct device *dev)
 		case WM8280:
 		case WM8998:
 		case WM1814:
-		case WM1831:
-		case CS47L24:
 			ret = regmap_update_bits(arizona->regmap,
 						 ARIZONA_ISOLATION_CONTROL,
 						 ARIZONA_ISOLATE_DCVDD1,
