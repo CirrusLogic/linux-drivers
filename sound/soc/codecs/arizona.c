eb6f5805a68c25fbd53790b442ec5a39969a3cef
Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
2015-08-06 13:13:12 +0100

    ASoC: arizona: fix high bandwidth setting of fll
    high bandwidth must be based on fsync value, which is the
    input to fll synchroniser loop after applying the input
    divider, also the threshold to switch the bandwidth must
    be 128K and not 100K
    
    Change-Id: I98f8dd5932f38a79d294eee15c8efbeee142f96b
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>

--- sound/soc/codecs/arizona.c
+++ sound/soc/codecs/arizona.c
@@ -3854,6 +3854,7 @@ static int arizona_enable_fll(struct arizona_fll *fll)
 	int already_enabled = arizona_is_enabled_fll(fll);
 	struct arizona_fll_cfg cfg;
 	bool fll_change;
+	unsigned int fsync_freq;
 
 	if (already_enabled < 0)
 		return already_enabled;
@@ -3881,7 +3882,7 @@ static int arizona_enable_fll(struct arizona_fll *fll)
 				  false);
 		if (fll->sync_src >= 0) {
 			arizona_calc_fll(fll, &cfg, fll->sync_freq, true);
-
+			fsync_freq = fll->sync_freq / (1 << cfg.refdiv);
 			fll_change |= arizona_apply_fll(arizona, fll->base + 0x10, &cfg,
 					  fll->sync_src, true);
 			use_sync = true;
@@ -3903,7 +3904,7 @@ static int arizona_enable_fll(struct arizona_fll *fll)
 	 * Increase the bandwidth if we're not using a low frequency
 	 * sync source.
 	 */
-	if (use_sync && fll->sync_freq > 100000)
+	if (use_sync && fsync_freq > 128000)
 		regmap_update_bits(arizona->regmap, fll->base + 0x17,
 				   ARIZONA_FLL1_SYNC_BW, 0);
 	else
