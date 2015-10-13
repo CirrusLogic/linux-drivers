0d4ffd4fdd376445953f33806eecba5ec07afb1d
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2015-10-13 14:15:25 +0100

    Revert "ASoC: moon: call adsp debugfs init and clean-up routines"
    This reverts commit 1bdae7ba877fa495f882d3365bd1c1cd70dfccd9.
    Preparing to switch to upstream implementation of wm_adsp debugfs
    
    Change-Id: Ib44f545613233ede3010a2def60c0502e12d4145
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- sound/soc/codecs/moon.c
+++ sound/soc/codecs/moon.c
@@ -2907,9 +2907,6 @@ static int moon_codec_probe(struct snd_soc_codec *codec)
 	struct arizona *arizona = priv->core.arizona;
 	int ret, i, j;
 
-	for (i = 0; i < MOON_NUM_ADSP; i++)
-		wm_adsp_init_debugfs(&priv->core.adsp[i], codec);
-
 	priv->core.arizona->dapm = &codec->dapm;
 
 	arizona_init_gpio(codec);
@@ -2986,9 +2983,6 @@ static int moon_codec_remove(struct snd_soc_codec *codec)
 	int i;
 	struct moon_priv *priv = snd_soc_codec_get_drvdata(codec);
 	struct arizona *arizona = priv->core.arizona;
-
-	for (i = 0; i < MOON_NUM_ADSP; i++)
-		wm_adsp_cleanup_debugfs(&priv->core.adsp[i]);
 
 	irq_set_irq_wake(arizona->irq, 0);
 	arizona_free_irq(arizona, ARIZONA_IRQ_DSP_IRQ1, priv);
