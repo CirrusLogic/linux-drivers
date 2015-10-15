b6996b3f43f437c92927c8a7a8fbef0141a02167
Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
2015-10-15 12:45:53 +0100

    ASoC: arizona: Handle lrclk_adv in DSP-A mode
    When lrclk_advance is set in DSP_A mode then fsync is
    shifted left by half bitclk this was added in moon for
    compatibility with tbone amplifier in tdm mode.
    
    Change-Id: Ifb223d82eba43bf2e60d1ec7ab227112d8ff6c2a
    Signed-off-by: Nikesh Oswal <Nikesh.Oswal@wolfsonmicro.com>
    Signed-off-by: V V N Praveen Kumar <praveenkumar.vegivada@incubesol.com>

--- sound/soc/codecs/arizona.c
+++ sound/soc/codecs/arizona.c
@@ -3443,7 +3443,10 @@ EXPORT_SYMBOL_GPL(arizona_set_sysclk);
 static int arizona_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
 {
 	struct snd_soc_codec *codec = dai->codec;
+	struct arizona_priv *priv = snd_soc_codec_get_drvdata(codec);
+	struct arizona *arizona = priv->arizona;
 	int lrclk, bclk, mode, base;
+	unsigned int mask;
 
 	base = dai->driver->base;
 
@@ -3521,9 +3524,22 @@ static int arizona_set_fmt(struct snd_soc_dai *dai, unsigned int fmt)
 	snd_soc_update_bits(codec, base + ARIZONA_AIF_TX_PIN_CTRL,
 			    ARIZONA_AIF1TX_LRCLK_INV |
 			    ARIZONA_AIF1TX_LRCLK_MSTR, lrclk);
+
+	mask = ARIZONA_AIF1RX_LRCLK_INV | ARIZONA_AIF1RX_LRCLK_MSTR;
+	switch (arizona->type) {
+	case CS47L90:
+	case CS47L91:
+		mask |= ARIZONA_AIF1RX_LRCLK_ADV;
+		if (arizona->pdata.lrclk_adv[dai->id - 1] &&
+			mode == SND_SOC_DAIFMT_DSP_A)
+			lrclk |= ARIZONA_AIF1RX_LRCLK_ADV;
+		break;
+	default:
+		break;
+	}
+
 	snd_soc_update_bits(codec, base + ARIZONA_AIF_RX_PIN_CTRL,
-			    ARIZONA_AIF1RX_LRCLK_INV |
-			    ARIZONA_AIF1RX_LRCLK_MSTR, lrclk);
+			    mask, lrclk);
 	snd_soc_update_bits(codec, base + ARIZONA_AIF_FORMAT,
 			    ARIZONA_AIF1_FMT_MASK, mode);
 
