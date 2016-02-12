9fa408a6bb6d9d93e1bd2b63d6744b5f60831b83
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2016-02-12 15:07:02 +0000

    ASoC: madera: Fixes to DSP clock setup
    - cs47l90 clocking must still set the DSP rate in DSP_CONFIG_1
    
    - remove call of madera_spin_sysclk from madera_write_adsp_clk_setting,
      it's already called around the function from madera_set_adsp_clk
    
    - there's no need to clear and restore the mixer inputs if the DSP
      sample rate has not changed
    
    Change-Id: I1259c6f97383c7b369fc97681fa98696eeec5b68
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- sound/soc/codecs/madera.c
+++ sound/soc/codecs/madera.c
@@ -788,39 +788,37 @@ static int madera_write_adsp_clk_setting(struct wm_adsp *dsp, unsigned int freq)
 	unsigned int mask, val;
 	int ret;
 
-	madera_spin_sysclk(priv);
+	mask = MADERA_DSP_RATE_MASK;
+	val = priv->adsp_rate_cache[dsp->num - 1] << MADERA_DSP_RATE_SHIFT;
 
 	switch (priv->madera->type) {
 	case CS47L35:
 	case CS47L85:
 	case WM1840:
 		/* use legacy frequency registers */
-		mask = MADERA_DSP_RATE_MASK | MADERA_DSP_CLK_SEL_MASK;
-		val = priv->adsp_rate_cache[dsp->num - 1] <<
-			MADERA_DSP_RATE_SHIFT;
+		mask |= MADERA_DSP_CLK_SEL_MASK;
 		val |= (freq << MADERA_DSP_CLK_SEL_SHIFT);
-
-		dev_dbg(priv->madera->dev,
-			"Set legacy DSP clocking to 0x%x\n", val);
-
-		ret = regmap_update_bits(dsp->regmap,
-					 dsp->base + MADERA_DSP_CONFIG_1_OFFS,
-					 mask, val);
 		break;
 	default:
 		/* Configure exact dsp frequency */
-		dev_dbg(priv->madera->dev, "Set DSP frequency to %u\n", freq);
+		dev_dbg(priv->madera->dev, "Set DSP frequency to 0x%x\n", freq);
 
 		ret = regmap_write(dsp->regmap,
 				   dsp->base + MADERA_DSP_CONFIG_2_OFFS, freq);
+		if (ret)
+			goto err;
 		break;
 	}
 
-	madera_spin_sysclk(priv);
+	ret = regmap_update_bits(dsp->regmap,
+				 dsp->base + MADERA_DSP_CONFIG_1_OFFS,
+				 mask, val);
+	dev_dbg(priv->madera->dev, "Set DSP clocking to 0x%x\n", val);
 
-	if (ret)
-		dev_err(dsp->dev, "Failed to set DSP%d clock: %d\n",
-			dsp->num, ret);
+	return 0;
+
+err:
+	dev_err(dsp->dev, "Failed to set DSP%d clock: %d\n", dsp->num, ret);
 
 	return ret;
 }
@@ -840,13 +838,12 @@ int madera_set_adsp_clk(struct wm_adsp *dsp, unsigned int freq)
 		return ret;
 	}
 
-	cur &= MADERA_DSP_RATE_MASK | MADERA_DSP_CLK_SEL_MASK;
+	cur &= MADERA_DSP_RATE_MASK;
 	new = priv->adsp_rate_cache[dsp->num - 1] << MADERA_DSP_RATE_SHIFT;
-	new |= (freq << MADERA_DSP_CLK_SEL_SHIFT);
 
 	if (new == cur) {
 		dev_dbg(madera->dev, "DSP rate not changed\n");
-		return 0;
+		return madera_write_adsp_clk_setting(dsp, freq);
 	}
 
 	ret = priv->get_sources(dsp->base, &cur_sources, &lim);
