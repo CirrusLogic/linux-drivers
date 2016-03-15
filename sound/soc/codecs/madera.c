75af176427be09ab13fbdcbe9396fa5905c710b3
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2016-03-15 11:06:57 +0000

    ASoC: madera: Reduce cs47l90/91 headphone power up delay
    The headphone output enable sequence is much faster on cs47l90 and
    cs47l91 only requiring 6ms. This is applied only for those two codecs
    rather than for all newer codecs in case the delay increases on future
    codcs. It is safer to use the larger delay by default and reduce on a
    chip-by-chip basis.
    
    Change-Id: Ifd582bf432de8bd0da98545a75bd207688d15dea
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- sound/soc/codecs/madera.c
+++ sound/soc/codecs/madera.c
@@ -2724,6 +2724,18 @@ int madera_out_ev(struct snd_soc_dapm_widget *w,
 {
 	struct snd_soc_codec *codec = snd_soc_dapm_to_codec(w->dapm);
 	struct madera_priv *priv = snd_soc_codec_get_drvdata(codec);
+	struct madera *madera = priv->madera;
+	int out_up_delay;
+
+	switch (madera->type) {
+	case CS47L90:
+	case CS47L91:
+		out_up_delay = 6;
+		break;
+	default:
+		out_up_delay = 17;
+		break;
+	}
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
@@ -2735,7 +2747,7 @@ int madera_out_ev(struct snd_soc_dapm_widget *w,
 		case MADERA_OUT3L_ENA_SHIFT:
 		case MADERA_OUT3R_ENA_SHIFT:
 			priv->out_up_pending++;
-			priv->out_up_delay += 17;
+			priv->out_up_delay += out_up_delay;
 			break;
 		default:
 			break;
