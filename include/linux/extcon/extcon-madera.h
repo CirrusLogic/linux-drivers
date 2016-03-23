7428787b428678de74f01ca64b51f097935fa930
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2016-03-23 14:45:40 +0000

    switch: madera: Update copyrights
    Change-Id: Ia13474fd68e910b6855707ef10f1bd6e8fcf34d7
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- include/linux/extcon/extcon-madera.h
+++ include/linux/extcon/extcon-madera.h
@@ -1,7 +1,7 @@
 /*
  * extcon-madera.h - public extcon driver API for Cirrus Logic Madera codecs
  *
- * Copyright 2015 Cirrus Logic
+ * Copyright 2015-2016 Cirrus Logic
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
