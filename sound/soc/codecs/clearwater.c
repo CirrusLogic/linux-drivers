20509ba201dacab7b43172b83a5ae263229d1930
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2016-04-28 09:52:07 +0100

    ASoC: clearwater: Add missing route from OUT3R to SYSCLK
    Change-Id: I5ad6993064d492c1e94f2122ed0065b164f9f515
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- sound/soc/codecs/clearwater.c
+++ sound/soc/codecs/clearwater.c
@@ -2147,6 +2147,7 @@ static const struct snd_soc_dapm_route clearwater_dapm_routes[] = {
 	{ "OUT2L", NULL, "SYSCLK" },
 	{ "OUT2R", NULL, "SYSCLK" },
 	{ "OUT3L", NULL, "SYSCLK" },
+	{ "OUT3R", NULL, "SYSCLK" },
 	{ "OUT4L", NULL, "SYSCLK" },
 	{ "OUT4R", NULL, "SYSCLK" },
 	{ "OUT5L", NULL, "SYSCLK" },
