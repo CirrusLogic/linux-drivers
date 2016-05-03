83a428bfcf08b363e5fa1882018ceb86411963cb
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2016-05-03 15:04:39 +0100

    ASoC: cs47l90: Add missing route from OUT3R to SYSCLK
    Change-Id: I58e572cda3d9600bd2a73e75fea3ce254da5f6f6
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- sound/soc/codecs/cs47l90.c
+++ sound/soc/codecs/cs47l90.c
@@ -1812,6 +1812,7 @@ static const struct snd_soc_dapm_route cs47l90_dapm_routes[] = {
 	{ "OUT2L", NULL, "SYSCLK" },
 	{ "OUT2R", NULL, "SYSCLK" },
 	{ "OUT3L", NULL, "SYSCLK" },
+	{ "OUT3R", NULL, "SYSCLK" },
 	{ "OUT5L", NULL, "SYSCLK" },
 	{ "OUT5R", NULL, "SYSCLK" },
 
