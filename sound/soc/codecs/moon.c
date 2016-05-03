ea31c59523abbc8df74ea8b81e6e5bd7deb548d3
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2016-05-03 14:41:43 +0100

    ASoC: moon: Add missing route from OUT3R to SYSCLK
    Change-Id: I3fabb4f2e149bbd8654e104c0f47defcaa6d858d
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- sound/soc/codecs/moon.c
+++ sound/soc/codecs/moon.c
@@ -2108,6 +2108,7 @@ static const struct snd_soc_dapm_route moon_dapm_routes[] = {
 	{ "OUT2L", NULL, "SYSCLK" },
 	{ "OUT2R", NULL, "SYSCLK" },
 	{ "OUT3L", NULL, "SYSCLK" },
+	{ "OUT3R", NULL, "SYSCLK" },
 	{ "OUT5L", NULL, "SYSCLK" },
 	{ "OUT5R", NULL, "SYSCLK" },
 
