674173f4f9f9b2efa1715c14d75207cd991f03d0
jjassal <jjassal@opensource.wolfsonmicro.com>
2016-07-15 11:11:09 +0100

    ASoC: moon: Make use of probe stage in wm_adsp driver
    This adds code from a patch that should have been added
    during moon development. The original patch was:
    ASoC: wm_adsp: Add codec_probe and codec_remove stubs
    
    Change-Id: I360b9adb2147d9ce5c74c33ea9dae1f65a417e32
    Signed-off-by: jjassal <jjassal@opensource.wolfsonmicro.com>

--- sound/soc/codecs/moon.c
+++ sound/soc/codecs/moon.c
@@ -2802,6 +2802,12 @@ static int moon_codec_probe(struct snd_soc_codec *codec)
 	regmap_update_bits(arizona->regmap, ARIZONA_SAMPLE_RATE_1,
 			   ARIZONA_SAMPLE_RATE_1_MASK, 0x03);
 
+	for (i = 0; i < MOON_NUM_ADSP; ++i) {
+		ret = wm_adsp2_codec_probe(&priv->core.adsp[i], codec);
+		if (ret)
+			return ret;
+	}
+
 	ret = snd_soc_add_codec_controls(codec, wm_adsp2v2_fw_controls, 14);
 	if (ret != 0)
 		return ret;
@@ -2881,6 +2887,10 @@ static int moon_codec_remove(struct snd_soc_codec *codec)
 	regmap_update_bits(arizona->regmap, CLEARWATER_IRQ2_MASK_9,
 			   CLEARWATER_DRC2_SIG_DET_EINT2,
 			   CLEARWATER_DRC2_SIG_DET_EINT2);
+
+	for (i = 0; i < MOON_NUM_ADSP; ++i)
+		wm_adsp2_codec_remove(&priv->core.adsp[i], codec);
+
 	priv->core.arizona->dapm = NULL;
 
 	return 0;
