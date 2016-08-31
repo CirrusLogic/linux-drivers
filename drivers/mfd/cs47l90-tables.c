49d27b2e058880f36f995460c893252dc336dcdd
Stuart Henderson <stuarth@opensource.wolfsonmicro.com>
2016-08-31 15:49:16 +0100

    mfd: madera: Make write sequencer control registers volatile
    These registers contain volatile fields and also should never
    be written back to hardware as this could trigger a write sequence.
    
    Change-Id: Ib40cad212f6359f95f41b6dba780079377fcad61
    Signed-off-by: Stuart Henderson <stuarth@opensource.wolfsonmicro.com>

--- drivers/mfd/cs47l90-tables.c
+++ drivers/mfd/cs47l90-tables.c
@@ -76,9 +76,6 @@ static const struct reg_default cs47l90_reg_default[] = {
 	{ 0x00000008, 0x0308 }, /* R8     - Ctrl IF CFG 1 */
 	{ 0x00000009, 0x0200 }, /* R9     - Ctrl IF CFG 2 */
 	{ 0x0000000A, 0x0308 }, /* R10    - Ctrl IF CFG 3 */
-	{ 0x00000016, 0x0000 }, /* R22 (0x16) - Write Sequencer Ctrl 0 */
-	{ 0x00000017, 0x0000 }, /* R23 (0x17) - Write Sequencer Ctrl 1 */
-	{ 0x00000018, 0x0000 }, /* R24 (0x18) - Write Sequencer Ctrl 2 */
 	{ 0x00000020, 0x0000 }, /* R32 (0x20) - Tone Generator 1 */
 	{ 0x00000021, 0x1000 }, /* R33 (0x21) - Tone Generator 2 */
 	{ 0x00000022, 0x0000 }, /* R34 (0x22) - Tone Generator 3 */
@@ -2702,6 +2699,9 @@ static bool cs47l90_16bit_volatile_register(struct device *dev,
 	switch (reg) {
 	case MADERA_SOFTWARE_RESET:
 	case MADERA_HARDWARE_REVISION:
+	case MADERA_WRITE_SEQUENCER_CTRL_0:
+	case MADERA_WRITE_SEQUENCER_CTRL_1:
+	case MADERA_WRITE_SEQUENCER_CTRL_2:
 	case MADERA_HAPTICS_STATUS:
 	case MADERA_SAMPLE_RATE_1_STATUS:
 	case MADERA_SAMPLE_RATE_2_STATUS:
