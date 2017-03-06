8856760bf4f69e2c0be6076b618fbc6d6a2c905a
Furquan Shaikh <furquan@chromium.org>
2017-03-06 13:58:05 -0800

    CHROMIUM: drivers: input: keyboard: Enable cros_ec_keyb driver for x86
    1. Add a new ACPI ID GOOG0007 in order to enable cros_ec_keyb device
    for x86.
    2. Return -EPROBE_DEFER if parent ec device is not probed before the
    cros_ec_keyb device probe.
    
    BUG=b:35774934
    TEST=Verified that cros_ec_keyb driver is able to detect button events
    on poppy.
    
    Signed-off-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/450838
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    (cherry picked from commit 774b17bc78df2d4960fcd1dc6923186ec40e242d)
    
    Change-Id: Ifc8d8e54b851679807e6fca3e172b78613a0e68b

--- drivers/input/keyboard/cros_ec_keyb.c
+++ drivers/input/keyboard/cros_ec_keyb.c
@@ -12,6 +12,7 @@
 // expensive.
 
 #include <linux/module.h>
+#include <linux/acpi.h>
 #include <linux/bitops.h>
 #include <linux/i2c.h>
 #include <linux/input.h>
@@ -615,6 +616,13 @@ static int cros_ec_keyb_probe(struct platform_device *pdev)
 	struct cros_ec_keyb *ckdev;
 	int err;
 
+	/*
+	 * If the parent ec device has not been probed yet, defer the probe of
+	 * this keyboard/button driver until later.
+	 */
+	if (ec == NULL)
+		return -EPROBE_DEFER;
+
 	ckdev = devm_kzalloc(dev, sizeof(*ckdev), GFP_KERNEL);
 	if (!ckdev)
 		return -ENOMEM;
@@ -659,6 +667,14 @@ static int cros_ec_keyb_remove(struct platform_device *pdev)
 	return 0;
 }
 
+#ifdef CONFIG_ACPI
+static const struct acpi_device_id cros_ec_keyb_acpi_match[] = {
+	{ "GOOG0007", 0 },
+	{ }
+};
+MODULE_DEVICE_TABLE(acpi, cros_ec_keyb_acpi_match);
+#endif
+
 #ifdef CONFIG_OF
 static const struct of_device_id cros_ec_keyb_of_match[] = {
 	{ .compatible = "google,cros-ec-keyb" },
@@ -675,6 +691,7 @@ static struct platform_driver cros_ec_keyb_driver = {
 	.driver = {
 		.name = "cros-ec-keyb",
 		.of_match_table = of_match_ptr(cros_ec_keyb_of_match),
+		.acpi_match_table = ACPI_PTR(cros_ec_keyb_acpi_match),
 		.pm = &cros_ec_keyb_pm_ops,
 	},
 };
