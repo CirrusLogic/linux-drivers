95a19a5599dfab775b2b6cc6b162333039c70b26
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2017-05-31 13:00:29 +0100

    ASoC: wm_adsp: Remove HALO clock frequency register
    This register isn't present on all codecs with a HALO core so
    must be handled by the parent codec driver.
    
    Change-Id: I30e81c77af19661af837aadf92cc378c72f5f4c5
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -232,8 +232,6 @@
 /*
  * HALO core
  */
-#define HALO_CLOCK_FREQ                      0x00000
-#define HALO_CLOCK_STATUS                    0x00008
 #define HALO_SAMPLE_RATE_RX1                 0x00080
 #define HALO_SAMPLE_RATE_TX1                 0x00280
 #define HALO_SCRATCH1                        0x005c0
@@ -3401,8 +3399,7 @@ int wm_adsp2_early_event(struct snd_soc_dapm_widget *w,
 EXPORT_SYMBOL_GPL(wm_adsp2_early_event);
 
 int wm_halo_early_event(struct snd_soc_dapm_widget *w,
-			struct snd_kcontrol *kcontrol, int event,
-			unsigned int freq)
+			struct snd_kcontrol *kcontrol, int event)
 {
 	struct snd_soc_codec *codec = snd_soc_dapm_to_codec(w->dapm);
 	struct wm_adsp *dsps = snd_soc_codec_get_drvdata(codec);
@@ -3417,15 +3414,6 @@ int wm_halo_early_event(struct snd_soc_dapm_widget *w,
 					 HALO_CORE_RESET, HALO_CORE_RESET);
 		if (ret != 0) {
 			adsp_err(dsp, "Error while resetting core: %d\n", ret);
-			return ret;
-		}
-
-		adsp_dbg(dsp, "Set clock frequency to 0x%x\n", freq);
-
-		ret = regmap_write(dsp->regmap,
-				   dsp->base + HALO_CLOCK_FREQ, freq);
-		if (ret) {
-			adsp_err(dsp, "Failed to set clock: %d\n", ret);
 			return ret;
 		}
 
