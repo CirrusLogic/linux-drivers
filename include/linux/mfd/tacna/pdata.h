e7521bb93ac4b730d79d82224aab02438c3ec602
Richard Fitzgerald <rf@opensource.wolfosnmicro.com>
2017-07-23 15:54:44 +0100

    include: tacna: Convert comments to kerneldoc format
    Convert the comments on pdata structures and public structures
    to kerneldoc format.
    
    Change-Id: I9fd47b9526d56a2473891aae00b964b182dba073
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfosnmicro.com>

--- include/linux/mfd/tacna/pdata.h
+++ include/linux/mfd/tacna/pdata.h
@@ -53,42 +53,42 @@ struct tacna_micbias_pdata {
 	struct tacna_micbias_pin_pdata pin[TACNA_MAX_CHILD_MICBIAS];
 };
 
+/**
+ * struct tacna_pdata - Configuration for Tacna MFD driver
+ *
+ * @reset:	    GPIO controlling RESET_B, if any
+ * @micvdd:	    Substruct of pdata for the MICSUPP regulator
+ *		    (see include/linux/regulator/arizonamicsupp.h)
+ * @irqchip:	    Substruct of pdata for the irqchip driver
+ * 		    (see include/linux/irqchip/irq-tacna-pdata.h)
+ * @gpio_base:	    Base GPIO
+ * @gpio_configs:   Array of GPIO configurations for pinctrl
+ * @n_gpio_configs: Number of configs in gpio_configs
+ * @micbias:	    MICBIAS configurations
+ * @codec:	    Substructure of pdata for the ASoC codec driver
+ * 		    (see include/sound/tacna-pdata.h)
+ * @gpsw:	    General purpose switch mode setting. See the SW1_MODE field
+ *		    in the datasheet for for information on the GPSW
+ * @accdet:	    Accessory detection configurations
+ */
 struct tacna_pdata {
-	/** GPIO controlling RESET_B, if any */
 	int reset;
 
-	/** Substruct of pdata for the MICSUPP regulator */
 	struct arizona_micsupp_pdata micvdd;
 
-	/** Substruct of pdata for the irqchip driver */
 	struct tacna_irqchip_pdata irqchip;
 
-	/** Base GPIO */
 	int gpio_base;
 
-	/**
-	 * Array of GPIO configurations
-	 * See Documentation/pinctrl.txt
-	 */
 	const struct pinctrl_map *gpio_configs;
 	int n_gpio_configs;
 
-	/** MICBIAS configurations */
 	struct tacna_micbias_pdata micbias[TACNA_MAX_MICBIAS];
 
-	/**
-	 * Substructure of pdata for the ASoC codec driver
-	 * See include/sound/tacna-pdata.h
-	 */
 	struct tacna_codec_pdata codec;
 
-	/**
-	 * General purpose switch mode setting
-	 * See the SW1_MODE field in the datasheet for the available values
-	 */
 	u32 gpsw[TACNA_MAX_GPSW];
 
-	/** Accessory detection configurations */
 	struct tacna_accdet_pdata accdet[TACNA_MAX_ACCESSORY];
 };
 
