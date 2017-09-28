2915bd4fc6a7210173e1c03e710a26ce5193c686
Richard Fitzgerald <rf@opensource.wolfsonmicro.com>
2017-09-28 10:26:25 +0100

    extcon: tacna: Fix incorrect validity check of micd_pol_gpio
    Code was checking >0 as if the value was an integer but it is a
    pointer so it should be a check for NULL.
    
    The patch also changes an assigned of 0 in an error case to an
    assignment of NULL.
    
    Change-Id: I09ae187cc126e6554fca2c24387ffd997bbdfb6e
    Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

--- drivers/extcon/extcon-tacna.c
+++ drivers/extcon/extcon-tacna.c
@@ -678,7 +678,7 @@ static void tacna_extcon_set_mode(struct tacna_extcon *info, int mode)
 		info->micd_modes[mode].bias, info->micd_modes[mode].gpio,
 		info->micd_modes[mode].hp_gnd);
 
-	if (info->micd_pol_gpio > 0)
+	if (info->micd_pol_gpio)
 		gpiod_set_value_cansleep(info->micd_pol_gpio,
 					 info->micd_modes[mode].gpio);
 
@@ -1983,7 +1983,7 @@ static void tacna_extcon_process_accdet_node(struct tacna_extcon *info,
 							gpio_status,
 							"cirrus,micd-pol-gpio");
 	if (IS_ERR(info->micd_pol_gpio))
-		info->micd_pol_gpio = 0;
+		info->micd_pol_gpio = NULL;
 }
 
 static int tacna_extcon_get_device_pdata(struct tacna_extcon *info)
