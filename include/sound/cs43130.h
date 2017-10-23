766a02471e09cdd8efd0a0ecec6727a287c7c782
Adam Brickman <Adam.Brickman@cirrus.com>
2017-10-23 14:06:27 -0500

    ASoC: cs43130: Add driver for Cirrus Logic CS43130
    Change-Id: Iadecd40650d52e0e430f7ab982f94f2c323da36f
    Signed-off-by: Adam Brickman <Adam.Brickman@cirrus.com>

---
+++ include/sound/cs43130.h
@@ -0,0 +1,18 @@
+/*
+ * linux/sound/cs43130.h -- Platform data for CS43130
+ *
+ * Copyright (c) 2016 Cirrus Logic Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef __CS43130_H
+#define __CS43130_H
+
+struct cs43130_platform_data {
+
+};
+
+#endif /* __CS43130_H */
