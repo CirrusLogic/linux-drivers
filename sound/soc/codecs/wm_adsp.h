1245d6e6b6af904649728e317671fc06f4f3a900
Charles Keepax <ckeepax@opensource.cirrus.com>
2018-01-15 11:24:33 +0000

    ASoC: wm_adsp: Add a delay after setting Halo rate
    Change-Id: Ic75fefa191d003b566983c007c4a83221084f34b
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>
    Signed-off-by: Qi Zhou <qi.zhou@cirrus.com>

--- sound/soc/codecs/wm_adsp.h
+++ sound/soc/codecs/wm_adsp.h
@@ -99,6 +99,7 @@ struct wm_adsp {
 	unsigned int n_rx_channels;
 	unsigned int n_tx_channels;
 
+	struct mutex *rate_lock;
 	u8 *rx_rate_cache;
 	u8 *tx_rate_cache;
 
@@ -147,7 +148,8 @@ int wm_adsp2_init(struct wm_adsp *dsp, struct mutex *fw_lock);
 void wm_adsp2_remove(struct wm_adsp *dsp);
 int wm_adsp2_codec_probe(struct wm_adsp *dsp, struct snd_soc_codec *codec);
 int wm_adsp2_codec_remove(struct wm_adsp *dsp, struct snd_soc_codec *codec);
-int wm_halo_init(struct wm_adsp *dsp, struct mutex *fw_lock);
+int wm_halo_init(struct wm_adsp *dsp,
+		 struct mutex *fw_lock, struct mutex *rate_lock);
 int wm_adsp1_event(struct snd_soc_dapm_widget *w,
 		   struct snd_kcontrol *kcontrol, int event);
 
