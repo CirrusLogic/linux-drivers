6a59c7b9d528fbcf95a41d5a1fe8ec7a15a9d36a
Stuart Henderson <stuarth@opensource.cirrus.com>
2018-02-01 15:30:06 +0000

    ASoC: cs48l32: Add codec driver for CS48L32
    Change-Id: I893c8b96fdec8b0beff25bec6a3e91ffd5394ea9
    Signed-off-by: Stuart Henderson <stuarth@opensource.cirrus.com>

--- sound/soc/codecs/cs47l96.c
+++ sound/soc/codecs/cs47l96.c
@@ -767,8 +767,8 @@ SND_SOC_BYTES("ANCL Coefficients", TACNA_ANC_L_CTRL_3,
 
 SOC_ENUM("Sample Rate 2", tacna_sample_rate[0]),
 SOC_ENUM("Sample Rate 3", tacna_sample_rate[1]),
-SOC_ENUM("Async Sample Rate 1", tacna_sample_rate[2]),
-SOC_ENUM("Async Sample Rate 2", tacna_sample_rate[3]),
+SOC_ENUM("Async Sample Rate 1", tacna_sample_rate_async[0]),
+SOC_ENUM("Async Sample Rate 2", tacna_sample_rate_async[1]),
 
 TACNA_RATE_ENUM("FX Rate", tacna_fx_rate),
 
