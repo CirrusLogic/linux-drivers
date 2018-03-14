e9ff4c985b630b1070ad9b0aa5cb53c2cfe68e7b
Richard Fitzgerald <rf@opensource.cirrus.com>
2018-03-14 12:47:01 +0000

    ASoC: cs47l96: Clocks must only be set through the correct codec driver
    SYSCLKAO must only be set through the cs47l96-ao driver and all other
    clocks through the main cs47l96 driver. Enforce this to prevent the
    drivers getting the wrong clock setups.
    
    Change-Id: I3d6ea2408a58bb3e593a1e0ff31a717afec37d45
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

--- sound/soc/codecs/cs47l96.c
+++ sound/soc/codecs/cs47l96.c
@@ -2664,6 +2664,21 @@ static int cs47l96_set_fll(struct snd_soc_codec *codec, int fll_id, int source,
 	return tacna_fllhj_set_refclk(&cs47l96->fll[idx], source, fref, fout);
 }
 
+static int cs47l96_set_sysclk(struct snd_soc_codec *codec, int clk_id,
+			      int source, unsigned int freq, int dir)
+{
+	struct cs47l96 *cs47l96 = snd_soc_codec_get_drvdata(codec);
+
+	switch (clk_id) {
+	case TACNA_CLK_SYSCLKAO:
+		dev_err(cs47l96->core.dev,
+			"SYSCLKAO must be set through cs47l96-ao codec\n");
+		return -EINVAL;
+	default:
+		return tacna_set_sysclk(codec, clk_id, source, freq, dir);
+	}
+}
+
 static struct regmap *cs47l96_get_regmap(struct device *dev)
 {
 	struct cs47l96 *cs47l96 = dev_get_drvdata(dev);
@@ -2678,7 +2693,7 @@ static struct snd_soc_codec_driver soc_codec_dev_cs47l96 = {
 
 	.idle_bias_off = true,
 
-	.set_sysclk = tacna_set_sysclk,
+	.set_sysclk = cs47l96_set_sysclk,
 	.set_pll = cs47l96_set_fll,
 
 	.component_driver = {
