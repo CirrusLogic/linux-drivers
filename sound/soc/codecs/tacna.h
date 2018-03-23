cf7631f63c2af490e6fb607557716a82ecb0e00f
Richard Fitzgerald <rf@opensource.cirrus.com>
2018-03-23 17:33:33 +0000

    ASoC: cs47l96: Add controls for input channel swap
    IN1/2 have left/right analogue inputs so need DAPM routing.
    IN3/4 are digital only, no left/right inputs to PGA widget, so
    only need an ALSA control for the channel swap register field.
    
    Change-Id: Iae3c237393f11489a42b11e83af9c7bf32bc97a7
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

--- sound/soc/codecs/tacna.h
+++ sound/soc/codecs/tacna.h
@@ -416,7 +416,7 @@ extern const struct snd_kcontrol_new tacna_inmux[];
 extern const struct snd_kcontrol_new tacna_inmode_switch[];
 extern const struct snd_kcontrol_new tacna_dmode_mux[];
 extern const struct snd_kcontrol_new tacna_in_swap_chan[];
-extern const struct soc_enum tacna_in2_swap_chan;
+extern const struct soc_enum tacna_in_swap_chan_ctrl[];
 extern const char * const
 	tacna_in_swap_chan_texts[TACNA_IN_SWAP_CHAN_ENUM_SIZE];
 
