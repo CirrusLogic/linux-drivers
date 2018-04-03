629572893fd9ffce799441af7bf94feb94bb8449
Jeff LaBundy <jeff.labundy@cirrus.com>
2018-04-03 13:59:25 -0500

    misc: cs40l2x: Extend support for CS40L2X family of devices
    * Renamed all files and namespaces from cs40l20 to cs40l2x
    * Added platform driver entries for CS40L25, CS40L25A and CS40L25B
    * Updated copyright year to 2018
    
    Change-Id: I2b2266a9870e713d454bee3cfc3e01e5e53bc191
    Signed-off-by: Jeff LaBundy <jeff.labundy@cirrus.com>

--- include/linux/platform_data/cs40l2x.h
+++ include/linux/platform_data/cs40l2x.h
@@ -1,21 +1,22 @@
 /*
- * linux/platform_data/cs40l20.h -- Platform data for CS40L20
+ * linux/platform_data/cs40l2x.h -- Platform data for
+ * CS40L20/CS40L25/CS40L25A/CS40L25B
  *
- * Copyright (c) 2017 Cirrus Logic Inc.
+ * Copyright 2018 Cirrus Logic, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
 
-#ifndef __CS40L20_H
-#define __CS40L20_H
+#ifndef __CS40L2X_H
+#define __CS40L2X_H
 
-struct cs40l20_platform_data {
+struct cs40l2x_platform_data {
 	unsigned int boost_ind;
 	unsigned int boost_cap;
 	unsigned int boost_ipk;
 	bool refclk_gpio2;
 };
 
-#endif /* __CS40L20_H */
+#endif /* __CS40L2X_H */
