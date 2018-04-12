6c6636b9f9393d1ec3beb7e819da3272f1a4a775
Richard Fitzgerald <rf@opensource.cirrus.com>
2018-04-12 17:18:14 +0100

    ASoC: cs47l96_ao: Check that SYSCLKAO and FLL1AO <=49.152MHz
    Block attempts to configure frequencies that are out of range.
    
    Change-Id: Ic811439cb7815c78aa6afa15d3a14cc7c455e6e9
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

--- sound/soc/codecs/cs47l96-ao.c
+++ sound/soc/codecs/cs47l96-ao.c
@@ -869,6 +869,11 @@ static int cs47l96_ao_set_fll(struct snd_soc_codec *codec, int fll_id, int sourc
 		return -EINVAL;
 	}
 
+	if (fout > 49152000) {
+		dev_err(codec->dev, "%u not supported for FLL1AO\n", fout);
+		return -EINVAL;
+	}
+
 	return tacna_fllhj_set_refclk(&cs47l96_ao->fll, source, fref, fout);
 }
 
@@ -879,6 +884,11 @@ static int cs47l96_ao_set_sysclk(struct snd_soc_codec *codec, int clk_id,
 
 	switch (clk_id) {
 	case TACNA_CLK_SYSCLKAO:
+		if (freq > 49152000) {
+			dev_err(codec->dev, "%u not supported for SYSCLKAO\n",
+				freq);
+			return -EINVAL;
+		}
 		return tacna_set_sysclk(codec, clk_id, source, freq, dir);
 	default:
 		dev_err(cs47l96_ao->core.dev, "Unknown clock id %u\n", clk_id);
