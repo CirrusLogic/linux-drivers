09734c11865d311099415c18271ff9194a52163a
Li Xu <li.xu@cirrus.com>
2018-04-16 11:01:53 -0500

    ASoC: wm_adsp: Limit bulk I2C read size for Xilinx
    Due to Xilinx I2C controller's limitation,
    limit bulk I2C read size.
    
    SQA-952
    
    Change-Id: Ied35c6a07e8cd4dd551e29cb1a178e8550fea5d5
    Signed-off-by: Li Xu <li.xu@cirrus.com>
    Signed-off-by: David Rhodes <david.rhodes@cirrus.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -34,7 +34,7 @@
 
 #include "wm_adsp.h"
 
-#define MAX_BULKSIZE			1028
+#define MAX_BULKSIZE			1024
 #define REG_BYTESIZE			4
 
  #define adsp_crit(_dsp, fmt, ...) \
@@ -1387,6 +1387,8 @@ static int wm_coeff_read_control(struct wm_coeff_ctl *ctl,
 	void *scratch;
 	int ret;
 	unsigned int reg;
+	int read_len = 0;
+	int toread_len;
 
 	ret = wm_coeff_base_reg(ctl, &reg);
 	if (ret)
@@ -1396,14 +1398,22 @@ static int wm_coeff_read_control(struct wm_coeff_ctl *ctl,
 	if (!scratch)
 		return -ENOMEM;
 
-	ret = wm_adsp2_raw_read(MAXBULK, dsp->regmap, reg, scratch, len);
-	if (ret) {
-		adsp_err(dsp, "Failed to read %zu bytes from %x: %d\n",
-			 len, reg, ret);
-		kfree(scratch);
-		return ret;
+	while ((len - read_len) > 0) {
+		toread_len = (len - read_len) > MAX_BULKSIZE ?
+			MAX_BULKSIZE : (len - read_len);
+		regmap_raw_read(dsp->regmap, reg + read_len / REG_BYTESIZE,
+				scratch + read_len, toread_len);
+		if (ret) {
+			adsp_err(dsp, "Failed to read %zu bytes from %x: %d\n",
+				 toread_len,
+				 reg + read_len / REG_BYTESIZE, ret);
+			kfree(scratch);
+			return ret;
+		}
+		adsp_dbg(dsp, "Read %zu bytes from %x\n", toread_len,
+			 reg + read_len / REG_BYTESIZE);
+		read_len += toread_len;
 	}
-	adsp_dbg(dsp, "Read %zu bytes from %x\n", len, reg);
 
 	memcpy(buf, scratch, len);
 	kfree(scratch);
