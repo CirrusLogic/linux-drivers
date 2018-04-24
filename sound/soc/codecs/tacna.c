f92f0d93f2c9729a63786dc81c3182ea92def5ce
Simon Trimmer <simont@opensource.cirrus.com>
2018-04-24 13:37:06 +0100

    ASoC: tacna: export a couple of ultrasonic handler symbols
    For build confgurations using modules
    
    Change-Id: I746b876f47908c0d3fb7856bb94d0172cebfb963
    Signed-off-by: Simon Trimmer <simont@opensource.cirrus.com>

--- sound/soc/codecs/tacna.c
+++ sound/soc/codecs/tacna.c
@@ -936,6 +936,7 @@ irqreturn_t tacna_us1_activity(int irq, void *data)
 
 	return IRQ_HANDLED;
 }
+EXPORT_SYMBOL_GPL(tacna_us1_activity);
 
 irqreturn_t tacna_us2_activity(int irq, void *data)
 {
@@ -947,6 +948,7 @@ irqreturn_t tacna_us2_activity(int irq, void *data)
 
 	return IRQ_HANDLED;
 }
+EXPORT_SYMBOL_GPL(tacna_us2_activity);
 
 const char * const tacna_vol_ramp_text[TACNA_VOL_RAMP_ENUM_SIZE] = {
 	"0ms/6dB", "0.5ms/6dB", "1ms/6dB", "2ms/6dB", "4ms/6dB", "8ms/6dB",
