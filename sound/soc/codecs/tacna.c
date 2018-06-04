87060d26a9d16e1495330a04a37b2a2dc9e38c66
Richard Fitzgerald <rf@opensource.cirrus.com>
2018-06-04 14:53:49 +0100

    ASoC: cs47l94: Remove CS47L94
    Change-Id: I4bbfb9e89148a0daf3460e41f88fa4304312096b
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

--- sound/soc/codecs/tacna.c
+++ sound/soc/codecs/tacna.c
@@ -2714,8 +2714,6 @@ static int tacna_set_dacclk(struct snd_soc_codec *codec, int source,
 	}
 
 	switch (priv->tacna->type) {
-	case CS47L94:
-	case CS47L95:
 	case CS47L96:
 	case CS47L97:
 		if (freq % 4000)
