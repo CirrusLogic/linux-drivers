37528897954fd77778721317e3a0dcc3910574e1
Richard Fitzgerald <rf@opensource.cirrus.com>
2018-06-06 14:09:28 +0100

    irqchip: tacna: Update copyrights
    Change-Id: I33def6d771ac49a2386035cb725f3f35408e4045
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

--- include/linux/irqchip/irq-tacna-pdata.h
+++ include/linux/irqchip/irq-tacna-pdata.h
@@ -1,7 +1,7 @@
 /*
  * Platform data for Cirrus Logic Tacna codecs irqchip driver
  *
- * Copyright 2017 Cirrus Logic
+ * Copyright 2017 Cirrus Logic, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
