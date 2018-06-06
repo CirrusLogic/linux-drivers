251ce3f17c7e8d2e93aae6371d72f7259de314e7
Richard Fitzgerald <rf@opensource.cirrus.com>
2018-06-06 14:02:43 +0100

    mfd: tacna: Update copyrights
    Change-Id: Ieb713aa1a6f3105c9e565aa1dec23cbddd7595df
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

--- drivers/mfd/cs48l32-tables.c
+++ drivers/mfd/cs48l32-tables.c
@@ -1,7 +1,7 @@
 /*
  * Regmap configuration for CS48L32
  *
- * Copyright 2018 Cirrus Logic
+ * Copyright 2018 Cirrus Logic, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
--- include/linux/mfd/tacna/pdata.h
+++ include/linux/mfd/tacna/pdata.h
@@ -1,7 +1,7 @@
 /*
  * Platform data for Cirrus Logic Tacna codecs
  *
- * Copyright 2016-2017 Cirrus Logic
+ * Copyright 2016-2018 Cirrus Logic, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
