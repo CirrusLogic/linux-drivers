9f3fb25a2d6c675f81d5782c4e50843909fbc746
Stuart Henderson <stuarth@opensource.cirrus.com>
2018-06-15 15:13:02 +0100

    ASoC: wm_adsp: Add WDT expiry IRQ handling support
    Change-Id: Ie0f369a5c7888b86b484acf29e49659be15084d0
    Signed-off-by: Stuart Henderson <stuarth@opensource.cirrus.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -242,6 +242,7 @@
 #define HALO_SCRATCH1                        0x005c0
 #define HALO_CCM_CORE_CONTROL                0x41000
 #define HALO_CORE_SOFT_RESET                 0x00010
+#define HALO_WDT_CONTROL                     0x47000
 
 /*
  * HALO Lock support
@@ -394,6 +395,13 @@
  */
 #define HALO_CORE_SOFT_RESET_SHIFT          0
 #define HALO_CORE_SOFT_RESET_MASK           0x00000001
+
+/*
+ * HALO_WDT_CONTROL
+ */
+#define HALO_WDT_EN                         0x00000001
+#define HALO_WDT_EN_MASK                    0x00000001
+#define HALO_WDT_EN_SHIFT                   0
 
 /*
  * HALO_MPU_?M_VIO_STATUS
@@ -3598,6 +3606,12 @@ static void wm_adsp_stop_watchdog(struct wm_adsp *dsp)
 	}
 }
 
+static void wm_halo_stop_watchdog(struct wm_adsp *dsp)
+{
+	regmap_update_bits(dsp->regmap, dsp->base + HALO_WDT_CONTROL,
+			   HALO_WDT_EN_MASK, 0);
+}
+
 int wm_adsp2_early_event(struct snd_soc_dapm_widget *w,
 			 struct snd_kcontrol *kcontrol, int event,
 			 unsigned int freq)
@@ -5037,5 +5051,20 @@ exit_unlock:
 	return IRQ_HANDLED;
 }
 EXPORT_SYMBOL_GPL(wm_halo_bus_error);
+
+irqreturn_t wm_halo_wdt_expire(int irq, void *data)
+{
+	struct wm_adsp *dsp = data;
+
+	mutex_lock(&dsp->pwr_lock);
+
+	adsp_warn(dsp, "WDT Expiry Fault\n");
+	wm_halo_stop_watchdog(dsp);
+
+	mutex_unlock(&dsp->pwr_lock);
+
+	return IRQ_HANDLED;
+}
+EXPORT_SYMBOL_GPL(wm_halo_wdt_expire);
 
 MODULE_LICENSE("GPL v2");
