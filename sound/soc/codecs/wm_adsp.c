8cc79212686f96cb239c1e1ace6620e8eff88145
Stuart Henderson <stuarth@opensource.cirrus.com>
2019-02-06 13:28:19 +0000

    ASoC: wm_adsp: Update cached error state on trigger
    If a compressed stream is restarted after getting an error, the cached
    error value will still be used on the next pointer request, preventing
    the stream from starting.  Resolve this by ensuring the error status is
    updated on trigger start.
    
    Change-Id: I49efe9ffed013d3f1756496605596141d1661c5e
    Signed-off-by: Stuart Henderson <stuarth@opensource.cirrus.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -4617,6 +4617,23 @@ static int wm_adsp_buffer_free(struct wm_adsp *dsp)
 	return 0;
 }
 
+static int wm_adsp_buffer_get_error(struct wm_adsp_compr_buf *buf)
+{
+	int ret;
+
+	ret = wm_adsp_buffer_read(buf, HOST_BUFFER_FIELD(error), &buf->error);
+	if (ret < 0) {
+		adsp_err(buf->dsp, "Failed to check buffer error: %d\n", ret);
+		return ret;
+	}
+	if (buf->error != 0) {
+		adsp_err(buf->dsp, "Buffer error occurred: %d\n", buf->error);
+		return -EIO;
+	}
+
+	return 0;
+}
+
 int wm_adsp_compr_trigger(struct snd_compr_stream *stream, int cmd)
 {
 	struct wm_adsp_compr *compr = stream->runtime->private_data;
@@ -4637,6 +4654,10 @@ int wm_adsp_compr_trigger(struct snd_compr_stream *stream, int cmd)
 				break;
 			}
 		}
+
+		ret = wm_adsp_buffer_get_error(compr->buf);
+		if (ret < 0)
+			break;
 
 		wm_adsp_buffer_clear(compr->buf);
 
@@ -4709,23 +4730,6 @@ static int wm_adsp_buffer_update_avail(struct wm_adsp_compr_buf *buf)
 		 buf->read_index, write_index, avail * buf->dsp->data_word_size);
 
 	buf->avail = avail;
-
-	return 0;
-}
-
-static int wm_adsp_buffer_get_error(struct wm_adsp_compr_buf *buf)
-{
-	int ret;
-
-	ret = wm_adsp_buffer_read(buf, HOST_BUFFER_FIELD(error), &buf->error);
-	if (ret < 0) {
-		adsp_err(buf->dsp, "Failed to check buffer error: %d\n", ret);
-		return ret;
-	}
-	if (buf->error != 0) {
-		adsp_err(buf->dsp, "Buffer error occurred: %d\n", buf->error);
-		return -EIO;
-	}
 
 	return 0;
 }
