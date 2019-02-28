3d4bacc27ab8739fb63fc8b49d8af5b3d4c833f9
Charles Keepax <ckeepax@opensource.cirrus.com>
2019-02-28 14:55:13 +0000

    ASoC: wm_adsp: Don't export wm_adsp2_lock
    This function is not presently called from outside the adsp code and nor
    should it be, as such stop exporting it.
    
    Change-Id: Ibba57b79d0bd454473d62f4c723d338f60020745
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>

--- sound/soc/codecs/wm_adsp.h
+++ sound/soc/codecs/wm_adsp.h
@@ -169,7 +169,6 @@ int wm_adsp2_early_event(struct snd_soc_dapm_widget *w,
 			 struct snd_kcontrol *kcontrol, int event,
 			 unsigned int freq);
 
-int wm_adsp2_lock(struct wm_adsp *adsp, unsigned int regions);
 irqreturn_t wm_adsp2_bus_error(struct wm_adsp *adsp);
 irqreturn_t wm_halo_bus_error(struct wm_adsp *dsp);
 irqreturn_t wm_halo_wdt_expire(int irq, void *data);
