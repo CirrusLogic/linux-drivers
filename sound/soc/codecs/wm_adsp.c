f7e4b8e0f3586f44c49feb8f666d33dbdc6e6fd8
Charles Keepax <ckeepax@opensource.cirrus.com>
2019-03-06 13:23:29 +0000

    ASoC: wm_adsp: Correct handling on booted/running on the VPU
    Make the handling of booted and running closer to the expected
    behaviour.
    
    Change-Id: I41a836e6bc62bde8458b31d76374d2b360178a2a
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>

--- sound/soc/codecs/wm_adsp.c
+++ sound/soc/codecs/wm_adsp.c
@@ -3228,6 +3228,8 @@ static void wm_vpu_boot_work(struct work_struct *work)
 	if (ret != 0)
 		goto err;
 
+	vpu->booted = true;
+
 	/* Sync set controls */
 	ret = wm_coeff_sync_controls(vpu);
 	if (ret != 0)
@@ -3239,7 +3241,7 @@ static void wm_vpu_boot_work(struct work_struct *work)
 			goto err;
 	}
 
-	vpu->booted = true;
+	vpu->running = true;
 
 err:
 	mutex_unlock(&vpu->pwr_lock);
