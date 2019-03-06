156cfbacc2771ec2677c128a88af652a969ff676
Charles Keepax <ckeepax@opensource.cirrus.com>
2019-03-06 13:03:12 +0000

    ASoC: wm_adsp: Tidy up some VPU stuff that is no longer needed
    Remove the now unused wm_adsp_queue_boot_work and don't export
    wm_vpu_setup_algs since it should only be used inside the DSP
    code.
    
    Change-Id: I681f1b8b4b659b3645bc5129d497ead7f541538e
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>

--- sound/soc/codecs/wm_adsp.h
+++ sound/soc/codecs/wm_adsp.h
@@ -161,7 +161,6 @@ int wm_adsp2_codec_remove(struct wm_adsp *dsp, struct snd_soc_codec *codec);
 int wm_halo_init(struct wm_adsp *dsp,
 		 struct mutex *fw_lock, struct mutex *rate_lock);
 void wm_adsp_queue_boot_work(struct wm_adsp *dsp);
-int wm_vpu_setup_algs(struct wm_adsp *vpu);
 int wm_vpu_init(struct wm_adsp *vpu, struct mutex *fw_lock);
 int wm_adsp1_event(struct snd_soc_dapm_widget *w,
 		   struct snd_kcontrol *kcontrol, int event);
