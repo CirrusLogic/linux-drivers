b1e37f4a4a25c959c0753103c8744d7f5f82f669
Stuart Henderson <stuarth@opensource.cirrus.com>
2019-03-25 12:54:07 +0000

    ASoC: cs48l32: Add voicectrl3 dais
    Firmware exists with three voicectrl buffers, so add dais to support this.
    
    Change-Id: I6497da0448a04d4c6320177f636167f776c1fe8d
    Signed-off-by: Stuart Henderson <stuarth@opensource.cirrus.com>

--- sound/soc/codecs/cs48l32.c
+++ sound/soc/codecs/cs48l32.c
@@ -1081,6 +1081,7 @@ static const struct snd_soc_dapm_route cs48l32_dapm_routes[] = {
 	{ "Audio Trace DSP", NULL, "DSP1" },
 	{ "Voice Ctrl DSP", NULL, "DSP1" },
 	{ "Voice Ctrl 2 DSP", NULL, "DSP1" },
+	{ "Voice Ctrl 3 DSP", NULL, "DSP1" },
 
 	{ "MICBIAS1", NULL, "VOUT_MIC" },
 
@@ -1381,6 +1382,27 @@ static struct snd_soc_dai_driver cs48l32_dai[] = {
 			.formats = TACNA_FORMATS,
 		},
 	},
+	{
+		.name = "cs48l32-cpu-voicectrl3",
+		.capture = {
+			.stream_name = "Voice Ctrl 3 CPU",
+			.channels_min = 1,
+			.channels_max = 8,
+			.rates = TACNA_RATES,
+			.formats = TACNA_FORMATS,
+		},
+		.compress_new = &snd_soc_new_compress,
+	},
+	{
+		.name = "cs48l32-dsp-voicectrl3",
+		.capture = {
+			.stream_name = "Voice Ctrl 3 DSP",
+			.channels_min = 1,
+			.channels_max = 8,
+			.rates = TACNA_RATES,
+			.formats = TACNA_FORMATS,
+		},
+	},
 };
 
 static int cs48l32_compr_open(struct snd_compr_stream *stream)
@@ -1391,7 +1413,8 @@ static int cs48l32_compr_open(struct snd_compr_stream *stream)
 
 	if (strcmp(rtd->codec_dai->name, "cs48l32-dsp-trace") &&
 	    strcmp(rtd->codec_dai->name, "cs48l32-dsp-voicectrl") &&
-	    strcmp(rtd->codec_dai->name, "cs48l32-dsp-voicectrl2")) {
+	    strcmp(rtd->codec_dai->name, "cs48l32-dsp-voicectrl2") &&
+	    strcmp(rtd->codec_dai->name, "cs48l32-dsp-voicectrl3")) {
 		dev_err(priv->dev,
 			"No suitable compressed stream for DAI '%s'\n",
 			rtd->codec_dai->name);
