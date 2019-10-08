f1698d5471a24f7bb27b382f7623205babcfe875
James Schulman <james.schulman@cirrus.com>
2019-10-07 19:33:26 -0500

    ASoC: cs35l45: Support DT GPIO config
    Set up basic devicetree configuration for CS35L45.
    For now, only supports chip GPIO configuration.
    
    Change-Id: If0f53524bf491c714dcee2c9739766f0be31dde2
    Signed-off-by: James Schulman <james.schulman@cirrus.com>

--- include/sound/cs35l45.h
+++ include/sound/cs35l45.h
@@ -13,12 +13,29 @@
 
 #define CS35L45_NUM_SUPPLIES 2
 
+struct gpio_ctrl {
+	bool is_present;
+	unsigned int dir;
+	unsigned int lvl;
+	unsigned int op_cfg;
+	unsigned int pol;
+	unsigned int ctrl;
+	unsigned int invert;
+};
+
+struct cs35l45_platform_data {
+	struct gpio_ctrl gpio_ctrl1;
+	struct gpio_ctrl gpio_ctrl2;
+	struct gpio_ctrl gpio_ctrl3;
+};
+
 struct cs35l45_private {
 	struct wm_adsp dsp; /* needs to be first member */
 	struct device *dev;
 	struct regmap *regmap;
 	struct gpio_desc *reset_gpio;
 	struct regulator_bulk_data supplies[CS35L45_NUM_SUPPLIES];
+	struct cs35l45_platform_data pdata;
 	bool initialized;
 	bool halo_booted;
 	struct mutex rate_lock;
