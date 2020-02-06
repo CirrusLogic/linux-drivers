5536ba58727748a40b85fb0dfa3dda598db0d9bc
Charles Keepax <ckeepax@opensource.cirrus.com>
2020-02-06 16:09:22 +0000

    ASoC: wm_adsp: Add Voice Processor Unit support
    Change-Id: I109f3dbd599a3e3258cb9470f5a36d0f0da89ec0
    Signed-off-by: Lucas Tanure <tanureal@opensource.cirrus.com>
    Signed-off-by: Andrew Ford <andrew.ford@cirrus.com>

--- sound/soc/codecs/wmfw.h
+++ sound/soc/codecs/wmfw.h
@@ -55,6 +55,10 @@ struct wmfw_adsp2_sizes {
 	__le32 zm;
 } __packed;
 
+struct wmfw_vpu_sizes {
+	__le32 dm;
+} __packed;
+
 struct wmfw_region {
 	union {
 		__be32 type;
@@ -103,6 +107,13 @@ struct wmfw_halo_id_hdr {
 	__be32 n_algs;
 } __packed;
 
+struct wmfw_vpu_id_hdr {
+	struct wmfw_v3_id_hdr fw;
+	__be32 dm_base;
+	__be32 dm_size;
+	__be32 n_algs;
+} __packed;
+
 struct wmfw_alg_hdr {
 	__be32 id;
 	__be32 ver;
@@ -119,6 +130,12 @@ struct wmfw_adsp2_alg_hdr {
 	__be32 zm;
 	__be32 xm;
 	__be32 ym;
+} __packed;
+
+struct wmfw_vpu_alg_hdr {
+	struct wmfw_alg_hdr alg;
+	__be32 dm_base;
+	__be32 dm_size;
 } __packed;
 
 struct wmfw_halo_alg_hdr {
@@ -178,6 +195,7 @@ struct wmfw_coeff_item {
 #define WMFW_ADSP1 1
 #define WMFW_ADSP2 2
 #define WMFW_HALO 4
+#define WMFW_VPU 0x45
 
 #define WMFW_ABSOLUTE         0xf0
 #define WMFW_ALGORITHM_DATA   0xf2
@@ -197,5 +215,7 @@ struct wmfw_coeff_item {
 #define WMFW_HALO_PM_PACKED 0x10
 #define WMFW_HALO_XM_PACKED 0x11
 #define WMFW_HALO_YM_PACKED 0x12
+
+#define WMFW_VPU_DM 0x30
 
 #endif
