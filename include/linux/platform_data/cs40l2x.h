b3eb45e707da0567971db2ae716c40ab18e36370
Paul Handrigan <Paul.Handrigan@cirrus.com>
2020-02-19 17:16:35 -0600

    misc: cs40l2x: Add conditional classH to specific use cases.
    Add the ability to disable classH when playing clab or dynamic f0
    waveforms and re-enable classH once it is done.  Also, enable classh
    if I2S data is active and the waveform is complete.
    
    Change-Id: Ibe2b0e9ed2c4d7f56380c8bb6e75c50394e6c7a6
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>

--- include/linux/platform_data/cs40l2x.h
+++ include/linux/platform_data/cs40l2x.h
@@ -30,6 +30,7 @@ struct cs40l2x_platform_data {
 	unsigned int boost_ipk;
 	unsigned int boost_ctl;
 	unsigned int boost_ovp;
+	unsigned int boost_clab;
 	bool refclk_gpio2;
 	unsigned int f0_default;
 	unsigned int f0_min;
