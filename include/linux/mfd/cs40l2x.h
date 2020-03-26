28ef277be38dc96cb4d6221a9b41b67eaef52b55
Paul Handrigan <Paul.Handrigan@cirrus.com>
2020-03-26 17:53:27 -0500

    misc: cs40l2x: Add check for wavetable in Y memory
    Add a check for wavetable data for Y memory as well as
    X memory.
    
    Change-Id: I3b3099b687569df3227eef0ed61d21d07448d060
    Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>

--- include/linux/mfd/cs40l2x.h
+++ include/linux/mfd/cs40l2x.h
@@ -828,6 +828,7 @@
 #define CS40L2X_WT_DESC_BYTE_OFFSET	12
 
 #define CS40L2X_SETTLE_DELAY_MS		50
+#define CS40L2X_MAX_WAVEFORMS		128
 
 #define CS40L2X_XM_FW_ID		(CS40L2X_DSP1_XMEM_UNPACK24_0 + 12)
 #define CS40L2X_XM_NUM_ALGOS		(CS40L2X_DSP1_XMEM_UNPACK24_0 + 36)
@@ -1284,8 +1285,8 @@ struct cs40l2x_private {
 	bool comp_enable_redc;
 	bool comp_enable_f0;
 	bool amp_gnd_stby;
-	unsigned int clab_wt_en;
-	unsigned int f0_wt_en;
+	bool clab_wt_en[CS40L2X_MAX_WAVEFORMS];
+	bool f0_wt_en[CS40L2X_MAX_WAVEFORMS];
 	struct cs40l2x_wseq_pair dsp_cache[CS40L2X_DSP_CACHE_MAX];
 	unsigned int dsp_cache_depth;
 
