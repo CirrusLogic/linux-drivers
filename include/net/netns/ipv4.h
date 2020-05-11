5886769153480dd6dc42803b5717275daf0d5b03
Jie Jiang <jiejiang@chromium.org>
2020-05-11 15:30:47 +0900

    CHROMIUM: net: Add per-netns dummy tcp_default_init_rwnd
    Android P CTS test checks if it can read this tcp_default_init_rwnd,
    and this change adds it to make that test able to pass. See the TEST
    line below for the affected test.
    
    tcp_default_init_rwnd is introduced into Android's kernel in
    https://r.android.com/79299 and is discarded from 4.19 (see
    https://r.android.com/804152). But we still need to support Android P
    with kernel 5.4 in CrOS. Since the kernel no longer relies on this
    value to set initial congestion window, a simple dummy handler is enough
    to pass CTS test.
    
    This change replaces the following two changes in our previous kernel:
    - I386b2a9d62de0ebe05c1ebe1b4bd91b314af5c54: the ANDROID change that
    adds tcp_default_init_rwnd in sysctl;
    - I24482a4d4bfd6229293f4ddf457fa924cf0b01a3: the CHROMIUM change that
    makes tcp_default_init_rwnd network namespace aware.
    
    This change can be reverted when all CrOS devices running 5.4 have
    updated to ARCVM.
    
    BUG=b:154051922
    TEST=cts-tradefed run commandAndExit cts -m CtsPermissionTestCases -t
      android.permission.cts.FileSystemPermissionTest#testTcpDefaultRwndSane
      on a careena with grunt-kernelnext passed.
    
    Signed-off-by: Jie Jiang <jiejiang@chromium.org>
    Change-Id: Ifbc056aba0f60845c192d360308f43776261f3f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2192644
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    [rebase510(groeck):
        Squashed:
    	CHROMIUM: net: Fix hang caused by dummy tcp_default_init_rwnd
    ]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- include/net/netns/ipv4.h
+++ include/net/netns/ipv4.h
@@ -118,6 +118,7 @@ struct netns_ipv4 {
 #ifdef CONFIG_NET_L3_MASTER_DEV
 	int sysctl_tcp_l3mdev_accept;
 #endif
+	int sysctl_tcp_default_init_rwnd;
 	int sysctl_tcp_mtu_probing;
 	int sysctl_tcp_mtu_probe_floor;
 	int sysctl_tcp_base_mss;
