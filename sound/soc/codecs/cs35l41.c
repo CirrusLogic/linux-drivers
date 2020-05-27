MotorolaMobilityLLC/motorola-kernel-modules

ASoC: cs35l41: factor out common part of driver remove
Move out redundant part of spi and i2c drivers remove
function to a separate function.

CRs-fixed:(CR)

Change-Id: I4a03b7116b53d82ab9fa7165ed75e1f2cbeea4e9
Signed-off-by: David Rhodes <david.rhodes@cirrus.com>
Reviewed-on: https://gerrit.mot.com/1476648
Tested-by: Jira Key
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver
Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
Submit-Approved: Jira Key

sound/soc/codecs/cs35l41.c
@@ -2497,6 +2497,15 @@ err:
 	return ret;
 }
 
+int cs35l41_remove(struct cs35l41_private *cs35l41)
+{
+	regmap_write(cs35l41->regmap, CS35L41_IRQ1_MASK1, 0xFFFFFFFF);
+	wm_adsp2_remove(&cs35l41->dsp);
+	regulator_bulk_disable(cs35l41->num_supplies, cs35l41->supplies);
+	snd_soc_unregister_codec(cs35l41->dev);
+	return 0;
+}
+
 MODULE_DESCRIPTION("ASoC CS35L41 driver");
 MODULE_AUTHOR("David Rhodes, Cirrus Logic Inc, <david.rhodes@cirrus.com>");
 MODULE_LICENSE("GPL");

CirrusLogic/linux-drivers

sound/soc/codecs/cs35l41.c
@@ -2314,6 +2314,15 @@ err:
 	return ret;
 }
 
+int cs35l41_remove(struct cs35l41_private *cs35l41)
+{
+	regmap_write(cs35l41->regmap, CS35L41_IRQ1_MASK1, 0xFFFFFFFF);
+	wm_adsp2_remove(&cs35l41->dsp);
+	regulator_bulk_disable(cs35l41->num_supplies, cs35l41->supplies);
+	snd_soc_unregister_component(cs35l41->dev);
+	return 0;
+}
+
 MODULE_DESCRIPTION("ASoC CS35L41 driver");
 MODULE_AUTHOR("David Rhodes, Cirrus Logic Inc, <david.rhodes@cirrus.com>");
 MODULE_LICENSE("GPL");