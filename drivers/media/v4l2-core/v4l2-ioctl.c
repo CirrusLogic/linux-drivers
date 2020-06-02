9086ea5b00a4caa88895905117eb8c396dd96888
Fritz Koenig <frkoenig@chromium.org>
2020-06-02 15:04:52 -0700

    HACK: CHROMIUM: v4l2: Add V4L2_PIX_FMT_NV12_UBWC description
    V4L2_PIX_FMT_NV12_UBWC is a temporary format that will be
    used until v4l2 supports modifiers to pass buffer layout
    information.
    
    BUG=b:157938887
    TEST=dmesg no longer has splats when playing video
    
    Signed-off-by: Fritz Koenig <frkoenig@chromium.org>
    Change-Id: I958fb5144653c9d3ddaa27556161f4dbdf03919e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2225935
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: Douglas Anderson <dianders@chromium.org>
    (cherry picked from commit 4ace7f6b8ff57951a70f8af266c226c2ae21827e)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2720786
    Tested-by: Stephen Boyd <swboyd@chromium.org>
    Auto-Submit: Stephen Boyd <swboyd@chromium.org>
    Commit-Queue: Stephen Boyd <swboyd@chromium.org>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1319,6 +1319,7 @@ static void v4l_fill_fmtdesc(struct v4l2_fmtdesc *fmt)
 	case V4L2_PIX_FMT_NV61:		descr = "Y/CrCb 4:2:2"; break;
 	case V4L2_PIX_FMT_NV24:		descr = "Y/CbCr 4:4:4"; break;
 	case V4L2_PIX_FMT_NV42:		descr = "Y/CrCb 4:4:4"; break;
+	case V4L2_PIX_FMT_NV12_UBWC:	descr = "Y/CbCr 4:2:0 UBWC"; break;
 	case V4L2_PIX_FMT_NV12M:	descr = "Y/CbCr 4:2:0 (N-C)"; break;
 	case V4L2_PIX_FMT_NV21M:	descr = "Y/CrCb 4:2:0 (N-C)"; break;
 	case V4L2_PIX_FMT_NV16M:	descr = "Y/CbCr 4:2:2 (N-C)"; break;
