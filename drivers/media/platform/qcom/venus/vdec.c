a1852ad094202ec3a18f05a57368bb28b2b8b13a
Alexandre Courbot <acourbot@chromium.org>
2020-10-09 17:45:33 +0900

    FROMLIST: venus: vdec: return parsed crop information from stream
    Per the stateful codec specification, VIDIOC_G_SELECTION with a target
    of V4L2_SEL_TGT_COMPOSE is supposed to return the crop area of capture
    buffers containing the decoded frame. Until now the driver did not get
    that information from the firmware and just returned the dimensions of
    CAPTURE buffers.
    
    Signed-off-by: Alexandre Courbot <acourbot@chromium.org>
    (am from https://lore.kernel.org/patchwork/patch/1318903/)
    (also found at https://lore.kernel.org/r/20201009084533.2405320-1-acourbot@chromium.org)
    
    BUG=b:168448867
    BUG=b:170187774
    TEST=video.DecodeAccelVD.h264_profile_change passes on trogdor.
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Change-Id: If8c4502757e799c2fa40338faab7a2745a93c7c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2470357
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Auto-Submit: Alexandre Courbot <acourbot@chromium.org>

--- drivers/media/platform/qcom/venus/vdec.c
+++ drivers/media/platform/qcom/venus/vdec.c
@@ -329,6 +329,10 @@ static int vdec_s_fmt(struct file *file, void *fh, struct v4l2_format *f)
 
 	inst->width = format.fmt.pix_mp.width;
 	inst->height = format.fmt.pix_mp.height;
+	inst->crop.top = 0;
+	inst->crop.left = 0;
+	inst->crop.width = inst->width;
+	inst->crop.height = inst->height;
 
 	if (f->type == V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE)
 		inst->fmt_out = fmt;
@@ -346,6 +350,9 @@ vdec_g_selection(struct file *file, void *fh, struct v4l2_selection *s)
 	if (s->type != V4L2_BUF_TYPE_VIDEO_CAPTURE &&
 	    s->type != V4L2_BUF_TYPE_VIDEO_OUTPUT)
 		return -EINVAL;
+
+	s->r.top = 0;
+	s->r.left = 0;
 
 	switch (s->target) {
 	case V4L2_SEL_TGT_CROP_BOUNDS:
@@ -367,15 +374,11 @@ vdec_g_selection(struct file *file, void *fh, struct v4l2_selection *s)
 	case V4L2_SEL_TGT_COMPOSE:
 		if (s->type != V4L2_BUF_TYPE_VIDEO_CAPTURE)
 			return -EINVAL;
-		s->r.width = inst->out_width;
-		s->r.height = inst->out_height;
+		s->r = inst->crop;
 		break;
 	default:
 		return -EINVAL;
 	}
-
-	s->r.top = 0;
-	s->r.left = 0;
 
 	return 0;
 }
@@ -1342,6 +1345,10 @@ static void vdec_event_change(struct venus_inst *inst,
 
 	inst->width = format.fmt.pix_mp.width;
 	inst->height = format.fmt.pix_mp.height;
+	inst->crop.left = ev_data->input_crop.left;
+	inst->crop.top = ev_data->input_crop.top;
+	inst->crop.width = ev_data->input_crop.width;
+	inst->crop.height = ev_data->input_crop.height;
 
 	inst->out_width = ev_data->width;
 	inst->out_height = ev_data->height;
@@ -1441,6 +1448,10 @@ static void vdec_inst_init(struct venus_inst *inst)
 	inst->fmt_cap = &vdec_formats[0];
 	inst->width = frame_width_min(inst);
 	inst->height = ALIGN(frame_height_min(inst), 32);
+	inst->crop.left = 0;
+	inst->crop.top = 0;
+	inst->crop.width = inst->width;
+	inst->crop.height = inst->height;
 	inst->out_width = frame_width_min(inst);
 	inst->out_height = frame_height_min(inst);
 	inst->fps = 30;
