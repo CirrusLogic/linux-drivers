f7eb466235d6313557a9db075aef76e3b5b5c866
CK Hu <ck.hu@mediatek.com>
2020-10-12 11:50:47 +0800

    FROMGIT: drm/mediatek: DRM driver directly refer to sub driver's function
    Some ddp component exist in both display path and other path, so
    sub driver should not directly call DRM driver's function. Let
    DRM driver directly refer to sub driver's function so that sub
    driver need not register these function to DRM driver.
    
    Signed-off-by: CK Hu <ck.hu@mediatek.com>
    Signed-off-by: Chun-Kuang Hu <chunkuang.hu@kernel.org>
    
    (cherry picked from commit 1d33f13a1be32915f25c63f13100938cb9de4c63
    https://git.kernel.org/pub/scm/linux/kernel/git/chunkuang.hu/linux.git mediatek-drm-next)
    
    BUG=b:178064928
    TEST=emerge-kukui chromeos-kernel-5_10
    
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Change-Id: Ibe906dfb3102cad9a69457dce8f4f8fc49f2adc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2641943
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/gpu/drm/mediatek/mtk_disp_color.c
+++ drivers/gpu/drm/mediatek/mtk_disp_color.c
@@ -11,6 +11,7 @@
 #include <linux/platform_device.h>
 #include <linux/soc/mediatek/mtk-cmdq.h>
 
+#include "mtk_disp_drv.h"
 #include "mtk_drm_crtc.h"
 #include "mtk_drm_ddp_comp.h"
 
@@ -42,23 +43,23 @@ struct mtk_disp_color {
 	const struct mtk_disp_color_data	*data;
 };
 
-static int mtk_color_clk_enable(struct device *dev)
+int mtk_color_clk_enable(struct device *dev)
 {
 	struct mtk_disp_color *color = dev_get_drvdata(dev);
 
 	return clk_prepare_enable(color->clk);
 }
 
-static void mtk_color_clk_disable(struct device *dev)
+void mtk_color_clk_disable(struct device *dev)
 {
 	struct mtk_disp_color *color = dev_get_drvdata(dev);
 
 	clk_disable_unprepare(color->clk);
 }
 
-static void mtk_color_config(struct device *dev, unsigned int w,
-			     unsigned int h, unsigned int vrefresh,
-			     unsigned int bpc, struct cmdq_pkt *cmdq_pkt)
+void mtk_color_config(struct device *dev, unsigned int w,
+		      unsigned int h, unsigned int vrefresh,
+		      unsigned int bpc, struct cmdq_pkt *cmdq_pkt)
 {
 	struct mtk_disp_color *color = dev_get_drvdata(dev);
 
@@ -66,7 +67,7 @@ static void mtk_color_config(struct device *dev, unsigned int w,
 	mtk_ddp_write(cmdq_pkt, h, &color->cmdq_reg, color->regs, DISP_COLOR_HEIGHT(color));
 }
 
-static void mtk_color_start(struct device *dev)
+void mtk_color_start(struct device *dev)
 {
 	struct mtk_disp_color *color = dev_get_drvdata(dev);
 
@@ -74,13 +75,6 @@ static void mtk_color_start(struct device *dev)
 	       color->regs + DISP_COLOR_CFG_MAIN);
 	writel(0x1, color->regs + DISP_COLOR_START(color));
 }
-
-static const struct mtk_ddp_comp_funcs mtk_disp_color_funcs = {
-	.clk_enable = mtk_color_clk_enable,
-	.clk_disable = mtk_color_clk_disable,
-	.config = mtk_color_config,
-	.start = mtk_color_start,
-};
 
 static int mtk_disp_color_bind(struct device *dev, struct device *master,
 			       void *data)
@@ -149,8 +143,7 @@ static int mtk_disp_color_probe(struct platform_device *pdev)
 		return comp_id;
 	}
 
-	ret = mtk_ddp_comp_init(dev->of_node, &priv->ddp_comp, comp_id,
-				&mtk_disp_color_funcs);
+	ret = mtk_ddp_comp_init(dev->of_node, &priv->ddp_comp, comp_id);
 	if (ret) {
 		if (ret != -EPROBE_DEFER)
 			dev_err(dev, "Failed to initialize component: %d\n",
