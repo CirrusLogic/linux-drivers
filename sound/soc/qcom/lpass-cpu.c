64f0b74a268a31964200ca0d681c773b1afff262
V Sujith Kumar Reddy <vsujithk@codeaurora.org>
2020-10-13 19:15:28 +0530

    FROMLIST: Asoc: qcom: lpass-cpu: Fix dp audio failure on monitors
    Make LPASS_HDMI_TX_PARITY_ADDR reg as volatile to fix
    dp audio failure with external monitors.
    This patch is upgrade to below patch series.
    https://lore.kernel.org/patchwork/project/lkml/list/?series=466460
    
    Fixes: commit 7cb37b7bd0d3 ("ASoC: qcom: Add support for lpass hdmi driver")
    Signed-off-by: V Sujith Kumar Reddy <vsujithk@codeaurora.org>
    Signed-off-by: Srinivasa Rao Mandadapu <srivasam@codeaurora.org>
    (am from https://patchwork.kernel.org/patch/11835593/)
    (also found at https://lore.kernel.org/r/1602596728-11783-1-git-send-email-srivasam@codeaurora.org)
    
    BUG=b:147399356
    TEST=check DP audio on lazor with patch series
    
    Signed-off-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Change-Id: I5b833c47bdee486f3dd5b9c519268dda016d7db7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2470112
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- sound/soc/qcom/lpass-cpu.c
+++ sound/soc/qcom/lpass-cpu.c
@@ -665,6 +665,8 @@ static bool lpass_hdmi_regmap_volatile(struct device *dev, unsigned int reg)
 		return true;
 	if (reg == LPASS_HDMI_TX_LEGACY_ADDR(v))
 		return true;
+	if (reg == LPASS_HDMI_TX_PARITY_ADDR(v))
+		return true;
 
 	for (i = 0; i < v->rdma_channels; ++i) {
 		if (reg == LPAIF_HDMI_RDMACURR_REG(v, i))
