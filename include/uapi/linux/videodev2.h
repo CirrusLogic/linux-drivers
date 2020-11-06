9b037e32ff78af244aae13d580f112e92825075c
Shunqian Zheng <zhengsq@rock-chips.com>
2020-11-06 13:19:36 +0100

    BACKPORT: media: videodev2.h, v4l2-ioctl: add rkisp1 meta buffer format
    Add the Rockchip ISP1 specific processing parameter format
    V4L2_META_FMT_RK_ISP1_PARAMS and metadata format
    V4L2_META_FMT_RK_ISP1_STAT_3A for 3A.
    
    Signed-off-by: Shunqian Zheng <zhengsq@rock-chips.com>
    Signed-off-by: Jacob Chen <jacob2.chen@rock-chips.com>
    Signed-off-by: Helen Koike <helen.koike@collabora.com>
    Reviewed-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    (cherry picked from commit df22026aebd863745efd753371f46f6ab28a2617)
    
    Conflicts:
       drivers/media/v4l2-core/v4l2-ioctl.c
    
    Due to "BACKPORT: FROMLIST: media: platform: mtk-isp: Add Mediatek CAM driver"
    introducing new formats.
    
    BUG=b:197575417
    TEST=Camera works on Scarlet
    
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    Change-Id: Ibba0ffe35319951981d6a3d420fc60c88e33aaa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3168473
    Reviewed-by: Brian Norris <briannorris@chromium.org>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -818,6 +818,10 @@ struct v4l2_pix_format {
 /* Vendor specific - Mediatek Face Detection meta buffer format for firmware */
 #define V4L2_META_FMT_MTFD_RESULT  v4l2_fourcc('M', 'T', 'f', 'd') /* FD meta capture buffer */
 
+/* Vendor specific - used for RK_ISP1 camera sub-system */
+#define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 3A Parameters */
+#define V4L2_META_FMT_RK_ISP1_STAT_3A	v4l2_fourcc('R', 'K', '1', 'S') /* Rockchip ISP1 3A Statistics */
+
 /* priv field value to indicates that subsequent fields are valid. */
 #define V4L2_PIX_FMT_PRIV_MAGIC		0xfeedcafe
 
