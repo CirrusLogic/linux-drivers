9f641fb86808740603f86b3c89f994d9e06887e8
Douglas Anderson <dianders@chromium.org>
2020-12-01 12:56:11 -0800

    FROMGIT: drm: panel: add flags to BOE NV110WTM-N61
    I forgot to add these when posting up the support for BOE
    NV110WTM-N61.  Add them now.
    
    Fixes: a96ee0f6b58d ("drm: panel: simple: Add BOE NV110WTM-N61")
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Cc: Douglas Anderson <dianders@chromium.org>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Thierry Reding <thierry.reding@gmail.com>
    Cc: dri-devel@lists.freedesktop.org
    Signed-off-by: Sam Ravnborg <sam@ravnborg.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201201125554.v2.1.I8a7bfc0966e803ab91001c9e6d01a736950c4981@changeid
    (cherry picked from commit 9dbf1a4516cff8617fde63d265ccbdd6a0f56854
     https://github.com/freedesktop/drm-misc.git drm-misc-next)
    
    BUG=b:169288627
    TEST=Panel still works
    
    Change-Id: I8a7bfc0966e803ab91001c9e6d01a736950c4981
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2577668
    Tested-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Rob Clark <robdclark@chromium.org>
    Commit-Queue: Douglas Anderson <dianders@chromium.org>

--- drivers/gpu/drm/panel/panel-simple.c
+++ drivers/gpu/drm/panel/panel-simple.c
@@ -1405,6 +1405,7 @@ static const struct drm_display_mode boe_nv110wtm_n61_modes[] = {
 		.vsync_start = 1440 + 3,
 		.vsync_end = 1440 + 3 + 6,
 		.vtotal = 1440 + 3 + 6 + 31,
+		.flags = DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_NVSYNC,
 	},
 	{
 		.clock = 138500,
@@ -1416,6 +1417,7 @@ static const struct drm_display_mode boe_nv110wtm_n61_modes[] = {
 		.vsync_start = 1440 + 3,
 		.vsync_end = 1440 + 3 + 6,
 		.vtotal = 1440 + 3 + 6 + 31,
+		.flags = DRM_MODE_FLAG_PHSYNC | DRM_MODE_FLAG_NVSYNC,
 	},
 };
 
