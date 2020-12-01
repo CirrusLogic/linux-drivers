171ac3bf54a7047a22d44d17bb9149208fcc5294
Tvrtko Ursulin <tvrtko.ursulin@intel.com>
2020-12-01 13:17:57 +0000

    BACKPORT: drm/i915/pmu: Deprecate I915_PMU_LAST and optimize state tracking
    Adding any kinds of "last" abi markers is usually a mistake which I
    repeated when implementing the PMU because it felt convenient at the time.
    
    This patch marks I915_PMU_LAST as deprecated and stops the internal
    implementation using it for sizing the event status bitmask and array.
    
    New way of sizing the fields is a bit less elegant, but it omits reserving
    slots for tracking events we are not interested in, and as such saves some
    runtime space. Adding sampling events is likely to be a special event and
    the new plumbing needed will be easily detected in testing. Existing
    asserts against the bitfield and array sizes are keeping the code safe.
    
    First event which gets the new treatment in this new scheme are the
    interrupts - which neither needs any tracking in i915 pmu nor needs
    waking up the GPU to read it.
    
    v2:
     * Streamline helper names. (Chris)
    
    v3:
     * Comment which events need tracking. (Chris)
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201201131757.206367-1-tvrtko.ursulin@linux.intel.com
    (cherry picked from commit 348fb0cb0a79bce03f402d689bbe0bf666577531)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    Downstream changes:
    -Conflicts with already backported 37ef64511f313 ("drm/i915/pmu: Don't
     grab wakeref when enabling events"). I removed all the wakeref stuff
     from the backport.
    
    BUG=b:187093397
    TEST=Tested with single/multiple displays, suspend/resume, hotplugs on volteer/trogdor/dedede/zork, build tested on amd64/arm64-generic
    
    Change-Id: If07acd7a5d709517edf607b1b9571fad19f0d9c7

--- include/uapi/drm/i915_drm.h
+++ include/uapi/drm/i915_drm.h
@@ -179,7 +179,7 @@ enum drm_i915_pmu_engine_sample {
 #define I915_PMU_RC6_RESIDENCY		__I915_PMU_OTHER(3)
 #define I915_PMU_SOFTWARE_GT_AWAKE_TIME	__I915_PMU_OTHER(4)
 
-#define I915_PMU_LAST I915_PMU_RC6_RESIDENCY
+#define I915_PMU_LAST /* Deprecated - do not use */ I915_PMU_RC6_RESIDENCY
 
 /* Each region is a minimum of 16k, and there are at most 255 of them.
  */
