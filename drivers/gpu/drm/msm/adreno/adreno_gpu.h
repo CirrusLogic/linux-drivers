dea3f0804c1c502afce4452e3c5547798d396584
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-01-04 20:30:41 +0100

    UPSTREAM: drm/msm: Only enable A6xx LLCC code on A6xx
    Using this code on A5xx (and probably older too) causes a
    smmu bug.
    
    Fixes: 474dadb8b0d5 ("drm/msm/a6xx: Add support for using system cache(LLC)")
    Signed-off-by: Konrad Dybcio <konrad.dybcio@somainline.org>
    Tested-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@somainline.org>
    Reviewed-by: Jordan Crouse <jcrouse@codeaurora.org>
    Reviewed-by: Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    (cherry picked from commit 00fd44a1a4700718d5d962432b55c09820f7e709)
    
    BUG=b:170301044
    TEST=Use GPU
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Change-Id: I7f5b3e38114015dc2caabb8f1d286da0a2783a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2640649
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Rob Clark <robdclark@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>

--- drivers/gpu/drm/msm/adreno/adreno_gpu.h
+++ drivers/gpu/drm/msm/adreno/adreno_gpu.h
@@ -211,6 +211,11 @@ static inline int adreno_is_a540(struct adreno_gpu *gpu)
 	return gpu->revn == 540;
 }
 
+static inline bool adreno_is_a6xx(struct adreno_gpu *gpu)
+{
+	return ((gpu->revn < 700 && gpu->revn > 599));
+}
+
 static inline int adreno_is_a618(struct adreno_gpu *gpu)
 {
        return gpu->revn == 618;
