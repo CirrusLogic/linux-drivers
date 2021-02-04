2ce7c74dceb52cdd84e77f4ffb4c23e6244f8e8a
Saravana Kannan <saravanak@google.com>
2021-02-04 16:14:22 +0800

    FROMGIT: OPP: Add function to look up required OPP's for a given OPP
    Add a function that allows looking up required OPPs given a source OPP
    table, destination OPP table and the source OPP.
    
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    [ Viresh: Rearranged code, fixed return errors ]
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    
    (cherry picked from commit 7d8658ef65a4f891d0cff6340fa717b378384642
    https://git.kernel.org/pub/scm/linux/kernel/git/vireshk/pm.git opp/linux-next)
    
    BUG=b:129039370
    TEST=boot kukui and check cci devfreq governor
    
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Change-Id: Id562036a2734915dc2f7519492a28f9927ed8d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2635411
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- include/linux/pm_opp.h
+++ include/linux/pm_opp.h
@@ -156,6 +156,7 @@ void dev_pm_opp_unregister_set_opp_helper(struct opp_table *opp_table);
 struct opp_table *dev_pm_opp_attach_genpd(struct device *dev, const char **names, struct device ***virt_devs);
 void dev_pm_opp_detach_genpd(struct opp_table *opp_table);
 struct opp_table *devm_pm_opp_attach_genpd(struct device *dev, const char **names, struct device ***virt_devs);
+struct dev_pm_opp *dev_pm_opp_xlate_required_opp(struct opp_table *src_table, struct opp_table *dst_table, struct dev_pm_opp *src_opp);
 int dev_pm_opp_xlate_performance_state(struct opp_table *src_table, struct opp_table *dst_table, unsigned int pstate);
 int dev_pm_opp_set_rate(struct device *dev, unsigned long target_freq);
 int dev_pm_opp_set_bw(struct device *dev, struct dev_pm_opp *opp);
@@ -351,6 +352,12 @@ static inline struct opp_table *devm_pm_opp_attach_genpd(struct device *dev,
 				const char **names, struct device ***virt_devs)
 {
 	return ERR_PTR(-ENOTSUPP);
+}
+
+static inline struct dev_pm_opp *dev_pm_opp_xlate_required_opp(struct opp_table *src_table,
+				struct opp_table *dst_table, struct dev_pm_opp *src_opp)
+{
+	return ERR_PTR(-EOPNOTSUPP);
 }
 
 static inline int dev_pm_opp_xlate_performance_state(struct opp_table *src_table, struct opp_table *dst_table, unsigned int pstate)
