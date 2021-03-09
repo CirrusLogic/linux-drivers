e8c93284b8f2ed2f3c6877491bb688f23d567685
Johannes Berg <johannes.berg@intel.com>
2021-03-09 10:48:42 +0100

    CHROMIUM: iwl7000: mac80211: always include HE 6GHz capability in assoc request
    If HE/6GHz is available (thus we consider dot11HE6GOptionImplemented
    to be true), then always include the corresponding capability in the
    association request as required by the spec.
    
    Change-Id: I83ff7eb2ae8ebdf2e30c4fa2461344d9e569f599
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 413d5e6dea459687352d3b1879f866718f8eaa83

--- drivers/net/wireless/iwl7000/mac80211/util.c
+++ drivers/net/wireless/iwl7000/mac80211/util.c
@@ -3003,15 +3003,19 @@ void ieee80211_ie_build_he_6ghz_cap(struct ieee80211_sub_if_data *sdata,
 	u8 *pos;
 	u16 cap;
 
-	sband = ieee80211_get_sband(sdata);
-	if (!sband)
+#if CFG80211_VERSION >= KERNEL_VERSION(5,4,0)
+	if (!cfg80211_any_usable_channels(sdata->local->hw.wiphy,
+					  BIT(NL80211_BAND_6GHZ),
+					  IEEE80211_CHAN_NO_HE))
 		return;
 
-	if (!nl80211_is_6ghz(sband->band))
-		return;
+	sband = sdata->local->hw.wiphy->bands[NL80211_BAND_6GHZ];
+#else
+	return;
+#endif
 
 	iftd = ieee80211_get_sband_iftype_data(sband, iftype);
-	if (WARN_ON(!iftd))
+	if (!iftd)
 		return;
 
 	/* Check for device HE 6 GHz capability before adding element */
