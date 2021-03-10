3cc8490b6e10dbed902d399593e7028bc80fd31b
Brian Geffon <bgeffon@google.com>
2021-03-10 13:50:23 -0800

    FROMLIST: eventfd: Introduce EFD_ZERO_ON_WAKE
    This patch introduces a new flag to eventfd, called EFD_ZERO_ON_WAKE.
    This change is primarily introduced for use cases which do not care about
    the value stored in the eventfd itself. Such existing use cases require an
    additional read syscall to clear the count.
    
    This flag provides the following guarantees:
    
    (1) Writes can never block or return EAGAIN.
        The reason this is true is because we don't actually need to store the
        value and as a result the internal value is only changed between 0 and
        1 and back to 0. Therefore POLLERR and POLLOUT are never possible
        outcomes. A poll with POLLOUT or a write will always immediately
        return regardless of EFD_NONBLOCK.
    
    (2) Read / POLLIN result in the internal value being reset to 0.
        When EFD_NONBLOCK is set reads when the internal value is 0 will
        immediately return with EAGAIN, as it always has. Similiarly, when
        a read is performed without EFD_NONBLOCK it will block until a write
        occurs. In both cases after the read completes successfully the
        internal value is reset to 0. When polling with POLLIN, upon return
        of a POLLIN event the internal value will be reset to 0.
    
    Signed-off-by: Brian Geffon <bgeffon@google.com>
    (am from https://lore.kernel.org/patchwork/patch/1393221/)
    (also found at https://lore.kernel.org/r/20210310215023.4129753-1-bgeffon@google.com)
    
    BUG=b:173022729
    TEST=manually tested with chrome
    
    Signed-off-by: Brian Geffon <bgeffon@chromium.org>
    Change-Id: I81e6daeb52bb8f38f712735c1a3050970addb52c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2782351
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>
    Reviewed-by: Joel Fernandes <joelaf@google.com>

--- include/linux/eventfd.h
+++ include/linux/eventfd.h
@@ -26,8 +26,14 @@
 #define EFD_CLOEXEC O_CLOEXEC
 #define EFD_NONBLOCK O_NONBLOCK
 
+/*
+ * We intentionally use the value of O_NOFOLLOW for EFD_ZERO_ON_WAKE
+ * because O_NOFOLLOW would have no meaning with an eventfd.
+ */
+#define EFD_ZERO_ON_WAKE O_NOFOLLOW
+
 #define EFD_SHARED_FCNTL_FLAGS (O_CLOEXEC | O_NONBLOCK)
-#define EFD_FLAGS_SET (EFD_SHARED_FCNTL_FLAGS | EFD_SEMAPHORE)
+#define EFD_FLAGS_SET (EFD_SHARED_FCNTL_FLAGS | EFD_SEMAPHORE | EFD_ZERO_ON_WAKE)
 
 struct eventfd_ctx;
 struct file;
