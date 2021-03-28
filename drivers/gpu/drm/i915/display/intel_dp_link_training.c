78082f59a155adb5b505556745b108fec8fc7718
Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
2021-03-28 00:57:56 +0000

    UPSTREAM: drm/i915/ilk-glk: Fix link training on links with LTTPRs
    The spec requires to use at least 3.2ms for the AUX timeout period if
    there are LT-tunable PHY Repeaters on the link (2.11.2). An upcoming
    spec update makes this more specific, by requiring a 3.2ms minimum
    timeout period for the LTTPR detection reading the 0xF0000-0xF0007
    range (3.6.5.1).
    
    Accordingly disable LTTPR detection until GLK, where the maximum timeout
    we can set is only 1.6ms.
    
    Link training in the non-transparent mode is known to fail at least on
    some SKL systems with a WD19 dock on the link, which exposes an LTTPR
    (see the References below). While this could have different reasons
    besides the too short AUX timeout used, not detecting LTTPRs (and so not
    using the non-transparent LT mode) fixes link training on these systems.
    
    While at it add a code comment about the platform specific maximum
    timeout values.
    
    v2: Add a comment about the g4x maximum timeout as well. (Ville)
    
    Reported-by: Takashi Iwai <tiwai@suse.de>
    Reported-and-tested-by: Santiago Zarate <santiago.zarate@suse.com>
    Reported-and-tested-by: Bodo Graumann <mail@bodograumann.de>
    References: https://gitlab.freedesktop.org/drm/intel/-/issues/3166
    Fixes: b30edfd8d0b4 ("drm/i915: Switch to LTTPR non-transparent mode link training")
    Cc: <stable@vger.kernel.org> # v5.11
    Cc: Takashi Iwai <tiwai@suse.de>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210317184901.4029798-2-imre.deak@intel.com
    (cherry picked from commit 984982f3ef7b240cd24c2feb2762d81d9d8da3c2)
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    (cherry picked from commit 3f3a9bc1f6df5a1a9ec39e3a5bee420328811be4)
    
    BUG=b:179453336
    TEST=eDP comes up on brya when built with this commit https://crrev.com/i/3584703
    
    Signed-off-by: Linux Patches Robot <linux-patches-robot@chromeos-missing-patches.google.com.iam.gserviceaccount.com>
    Change-Id: Id485399b213151aa61a8828a89ab0e3992e57688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2789046
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Tested-by: Guenter Roeck <groeck@chromium.org>

--- drivers/gpu/drm/i915/display/intel_dp_link_training.c
+++ drivers/gpu/drm/i915/display/intel_dp_link_training.c
@@ -86,6 +86,18 @@ static void intel_dp_read_lttpr_phy_caps(struct intel_dp *intel_dp,
 
 static bool intel_dp_read_lttpr_common_caps(struct intel_dp *intel_dp)
 {
+	struct drm_i915_private *i915 = dp_to_i915(intel_dp);
+
+	if (intel_dp_is_edp(intel_dp))
+		return false;
+
+	/*
+	 * Detecting LTTPRs must be avoided on platforms with an AUX timeout
+	 * period < 3.2ms. (see DP Standard v2.0, 2.11.2, 3.6.6.1).
+	 */
+	if (INTEL_GEN(i915) < 10)
+		return false;
+
 	if (drm_dp_read_lttpr_common_caps(&intel_dp->aux,
 					  intel_dp->lttpr_common_caps) < 0) {
 		intel_dp_reset_lttpr_common_caps(intel_dp);
@@ -132,9 +144,6 @@ int intel_dp_init_lttpr_and_dprx_caps(struct intel_dp *intel_dp)
 	int lttpr_count;
 	bool ret;
 	int i;
-
-	if (intel_dp_is_edp(intel_dp))
-		return 0;
 
 	ret = intel_dp_read_lttpr_common_caps(intel_dp);
 
