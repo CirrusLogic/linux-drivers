71769fcee7126c1d09550f2856ed16cd3df8e689
Peter Zijlstra <peterz@infradead.org>
2021-03-29 15:18:35 +0200

    FROMLIST: sched: Inherit task cookie on fork()
    Note that sched_core_fork() is called from under tasklist_lock, and
    not from sched_fork() earlier. This avoids a few races later.
    
    (am from
    https://lore.kernel.org/lkml/20210422123308.980003687@infradead.org/)
    
    BUG=b:152605392
    TEST=run power_VideoCall test
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Don Hiatt <dhiatt@digitalocean.com>
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: I70fbbbc0a2192f55cee10166992139c82533fc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2880792
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2239,6 +2239,8 @@ static __latent_entropy struct task_struct *copy_process(
 
 	klp_copy_process(p);
 
+	sched_core_fork(p);
+
 	spin_lock(&current->sighand->siglock);
 
 	/*
@@ -2326,6 +2328,7 @@ static __latent_entropy struct task_struct *copy_process(
 	return p;
 
 bad_fork_cancel_cgroup:
+	sched_core_free(p);
 	spin_unlock(&current->sighand->siglock);
 	write_unlock_irq(&tasklist_lock);
 	cgroup_cancel_fork(p, args);
