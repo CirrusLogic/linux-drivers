81fdcafc5058e3efa882517a2876f56401b4da0c
Ben Ami, Golan <golan.ben.ami@intel.com>
2021-04-19 00:28:47 -0700

    CHROMIUM: iwl7000: mac80211: Revert "mac80211: don't apply flow control on management frames"
    We don't really want this fix in core61.
    
    This reverts commit b520b022d267de111a8b18e3020f162096224f77.
    
    Change-Id: Ibd1cb0cc64311ad87b38865217318458bdf31b8d
    iwl7000-tree: 2a409a002fb5f4f44be2d054a87d68c585055269

--- drivers/net/wireless/iwl7000/mac80211/tx.c
+++ drivers/net/wireless/iwl7000/mac80211/tx.c
@@ -1407,18 +1407,9 @@ static void ieee80211_txq_enqueue(struct ieee80211_local *local,
 	ieee80211_set_skb_enqueue_time(skb);
 
 	spin_lock_bh(&fq->lock);
-	/*
-	 * For management frames, don't really apply codel etc.,
-	 * we don't want to apply any shaping or anything we just
-	 * want to simplify the driver API by having them on the
-	 * txqi.
-	 */
-	if (unlikely(txqi->txq.tid == IEEE80211_NUM_TIDS))
-		__skb_queue_tail(&txqi->frags, skb);
-	else
-		fq_tin_enqueue(fq, tin, flow_idx, skb,
-			       fq_skb_free_func,
-			       fq_flow_get_default_func);
+	fq_tin_enqueue(fq, tin, flow_idx, skb,
+		       fq_skb_free_func,
+		       fq_flow_get_default_func);
 	spin_unlock_bh(&fq->lock);
 }
 
@@ -3861,9 +3852,6 @@ bool ieee80211_txq_airtime_check(struct ieee80211_hw *hw,
 		return true;
 
 	if (!txq->sta)
-		return true;
-
-	if (unlikely(txq->tid == IEEE80211_NUM_TIDS))
 		return true;
 
 	sta = container_of(txq->sta, struct sta_info, sta);
