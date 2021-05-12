98632a98634b29489e6a7514da21a0f1e8765ae6
Guenter Roeck <groeck@chromium.org>
2021-05-12 08:10:05 -0700

    CHROMIUM: Revert "ANDROID: mmc: core: Add "ignore mmc pm notify" functionality"
    This reverts commit 358f890e7e71886a0e649de072488f95f6767737.
    
    This does not do any good (it is not used) and only results in merge
    conflicts.
    
    BUG=b:187863626
    TEST=Merge v5.10.36
    
    Change-Id: I84fa45d098f98d7e8cd4124a912e86a61118b586
    Disallow-Recycled-Builds: test-failures
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2891421
    Reviewed-by: Marty Faltesek <mfaltesek@google.com>
    Reviewed-by: William K Lin <wklin@google.com>
    Commit-Queue: William K Lin <wklin@google.com>

--- drivers/mmc/core/host.c
+++ drivers/mmc/core/host.c
@@ -493,8 +493,7 @@ int mmc_add_host(struct mmc_host *host)
 #endif
 
 	mmc_start_host(host);
-	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
-		mmc_register_pm_notifier(host);
+	mmc_register_pm_notifier(host);
 
 	return 0;
 }
@@ -511,8 +510,7 @@ EXPORT_SYMBOL(mmc_add_host);
  */
 void mmc_remove_host(struct mmc_host *host)
 {
-	if (!(host->pm_flags & MMC_PM_IGNORE_PM_NOTIFY))
-		mmc_unregister_pm_notifier(host);
+	mmc_unregister_pm_notifier(host);
 	mmc_stop_host(host);
 
 #ifdef CONFIG_DEBUG_FS
