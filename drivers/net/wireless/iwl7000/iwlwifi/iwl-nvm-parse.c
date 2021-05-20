e740893cfd97d125c9899d4e7f3365435c779ac0
Johannes Berg <johannes.berg@intel.com>
2021-05-20 10:39:47 +0200

    CHROMIUM: iwl7000: add vendor specific capabilities for some RFs
    On some RFs we want to advertise vendor specific capabilities to
    indicate support for improved beamforming rates and/or on 160 MHz,
    some APs where Intel has collaborated with the vendor will use it
    to improve behaviour. Add these elements where relevant, i.e. for
    HR and GF RFs.
    
    Change-Id: If99bdb9009583ac7cc6cbb708e871a67df836dbe
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 383a1f0ee569baf9f55f24d28dd5f9a8b94a59a3

--- drivers/net/wireless/iwl7000/iwlwifi/iwl-nvm-parse.c
+++ drivers/net/wireless/iwl7000/iwlwifi/iwl-nvm-parse.c
@@ -544,6 +544,21 @@ static void iwl_init_vht_hw_capab(struct iwl_trans *trans,
 		cpu_to_le16(IEEE80211_VHT_EXT_NSS_BW_CAPABLE);
 }
 
+#if CFG80211_VERSION >= KERNEL_VERSION(5,14,0)
+static const u8 iwl_vendor_caps[] = {
+	0xdd,			/* vendor element */
+	0x06,			/* length */
+	0x00, 0x17, 0x35,	/* Intel OUI */
+	0x08,			/* type (Intel Capabilities) */
+	/* followed by 16 bits of capabilities */
+#define IWL_VENDOR_CAP_IMPROVED_BF_FDBK_160MHZ	BIT(0)
+#define IWL_VENDOR_CAP_IMPROVED_BF_FDBK_RATE	BIT(1)
+	IWL_VENDOR_CAP_IMPROVED_BF_FDBK_160MHZ |
+	IWL_VENDOR_CAP_IMPROVED_BF_FDBK_RATE,
+	0x00
+};
+#endif
+
 static const struct ieee80211_sband_iftype_data iwl_he_capa[] = {
 	{
 		.types_mask = BIT(NL80211_IFTYPE_STATION),
@@ -802,6 +817,21 @@ iwl_nvm_fixup_sband_iftd(struct iwl_trans *trans,
 	if (fw_has_capa(&fw->ucode_capa, IWL_UCODE_TLV_CAPA_BROADCAST_TWT))
 		iftype_data->he_cap.he_cap_elem.mac_cap_info[2] |=
 			IEEE80211_HE_MAC_CAP2_BCAST_TWT;
+
+#if CFG80211_VERSION >= KERNEL_VERSION(5,14,0)
+	switch (CSR_HW_RFID_TYPE(trans->hw_rf_id)) {
+	case IWL_CFG_RF_TYPE_HR2:
+	case IWL_CFG_RF_TYPE_HR1:
+	case IWL_CFG_RF_TYPE_GF:
+		if (!is_ap) {
+			iftype_data->vendor_elems.data =
+				iwl_vendor_caps;
+			iftype_data->vendor_elems.len =
+				ARRAY_SIZE(iwl_vendor_caps);
+		}
+		break;
+	}
+#endif
 }
 
 static void iwl_init_he_hw_capab(struct iwl_trans *trans,
