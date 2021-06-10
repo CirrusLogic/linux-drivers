4e042af711bc1fbd15e24f6c5d8c45cb6691a705
Sergey Senozhatsky <senozhatsky@chromium.org>
2021-06-10 10:58:52 +0900

    BACKPORT: FROMLIST: kvm: add PM-notifier
    Add KVM PM-notifier so that architectures can have arch-specific
    VM suspend/resume routines. Such architectures need to select
    CONFIG_HAVE_KVM_PM_NOTIFIER and implement kvm_arch_pm_notifier().
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Acked-by: Marc Zyngier <maz@kernel.org>
    (am from https://lore.kernel.org/patchwork/patch/1441655/)
    (also found at https://lore.kernel.org/r/20210606021045.14159-1-senozhatsky@chromium.org)
    
    BUG=b:187377603
    TEST=did rounds of suspend-resume on hatch-arc-r and checked that
         PVCLOCK_GUEST_STOPPED is set
    
    Change-Id: Ia2d44977b59c1cb08638cb996c1a4696221b3aed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2951944
    Tested-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Commit-Queue: Sergey Senozhatsky <senozhatsky@chromium.org>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -26,6 +26,7 @@
 #include <linux/rcuwait.h>
 #include <linux/refcount.h>
 #include <linux/nospec.h>
+#include <linux/notifier.h>
 #include <asm/signal.h>
 
 #include <linux/kvm.h>
@@ -505,6 +506,10 @@ struct kvm {
 	struct srcu_struct irq_srcu;
 	pid_t userspace_pid;
 	unsigned int max_halt_poll_ns;
+
+#ifdef CONFIG_HAVE_KVM_PM_NOTIFIER
+	struct notifier_block pm_notifier;
+#endif
 };
 
 #define kvm_err(fmt, ...) \
@@ -917,6 +922,10 @@ int kvm_arch_vcpu_precreate(struct kvm *kvm, unsigned int id);
 int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu);
 void kvm_arch_vcpu_postcreate(struct kvm_vcpu *vcpu);
 void kvm_arch_vcpu_destroy(struct kvm_vcpu *vcpu);
+
+#ifdef CONFIG_HAVE_KVM_PM_NOTIFIER
+int kvm_arch_pm_notifier(struct kvm *kvm, unsigned long state);
+#endif
 
 #ifdef __KVM_HAVE_ARCH_VCPU_DEBUGFS
 void kvm_arch_create_vcpu_debugfs(struct kvm_vcpu *vcpu, struct dentry *debugfs_dentry);
