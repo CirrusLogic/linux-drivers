a3df3ae7d9c6687c803b9f6290689e797fe1f7e9
YN Chen <YN.Chen@mediatek.com>
2021-06-29 07:05:53 +0800

    BACKPORT: FROMGIT: mt76: mt7921: add .set_sar_specs support
    add .set_sar_specs to allow configuring SAR power limitations on the
    frequency ranges from the userland.
    
    Co-developed-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    Signed-off-by: YN Chen <YN.Chen@mediatek.com>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    
    (cherry picked from commit 2f283b6e139763cc6fc4dafa68dc987acbb0046b
     https://github.com/nbd168/wireless.git mt76)
    
    Conflicts:
       drivers/net/wireless/mediatek/mt76/mt76.h
       drivers/net/wireless/mediatek/mt76/mt7921/main.c
    
    Backport Notes:
       Those conflicts come from currrent mac80211 doesn't support hw rx-amsdu
       de-aggregation, so we only merge those changes to the current context.
    
    BUG=b:178754244, b:188614537
    TEST=build on cherry
    
    Signed-off-by: Sean Wang <objelf@gmail.com>
    Change-Id: I32b5da42a4036e81d71e477c13a572caa0275ac0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3151438
    Reviewed-by: Joshua Emele <jemele@chromium.org>
    Tested-by: Joshua Emele <jemele@chromium.org>
    Commit-Queue: Joshua Emele <jemele@chromium.org>

--- drivers/net/wireless/mediatek/mt76/mt76.h
+++ drivers/net/wireless/mediatek/mt76/mt76.h
@@ -543,6 +543,11 @@ struct mt76_rx_status {
 	s8 chain_signal[IEEE80211_MAX_CHAINS];
 };
 
+struct mt76_freq_range_power {
+	const struct cfg80211_sar_freq_ranges *range;
+	s8 power;
+};
+
 struct mt76_testmode_ops {
 	int (*set_state)(struct mt76_phy *phy, enum mt76_testmode_state state);
 	int (*set_params)(struct mt76_phy *phy, struct nlattr **tb,
@@ -628,6 +633,8 @@ struct mt76_phy {
 
 	struct delayed_work mac_work;
 	u8 mac_work_count;
+
+	struct mt76_freq_range_power *frp;
 };
 
 struct mt76_dev {
@@ -760,6 +767,7 @@ enum mt76_phy_type {
 }
 
 extern struct ieee80211_rate mt76_rates[12];
+extern const struct cfg80211_sar_capa mt76_sar_capa;
 
 #define __mt76_rr(dev, ...)	(dev)->bus->rr((dev), __VA_ARGS__)
 #define __mt76_wr(dev, ...)	(dev)->bus->wr((dev), __VA_ARGS__)
