34cbd936324399f6e2ff667527dc9623ea129e2f
Sean Paul <seanpaul@chromium.org>
2021-07-21 13:12:57 +0000

    Revert "FROMLIST: Fix cursor plane didn't update"
    This reverts commit 3ed84163d82b26a68c7bd37f5889123853e7cd9f.
    
    Reason for revert: Compilation error for unused var plane_helper_funcs. Please land this upstream properly and retry.
    
    Original change's description:
    > FROMLIST: Fix cursor plane didn't update
    >
    > The cursor plane should use the current plane state in atomic_async_update
    > because it would not be the new plane state in the global atomic state
    > since _swap_state happened when those hook are run.
    >
    > Fix cursor plane issue by below modification:
    > 1. Remove plane_helper_funcs->atomic_update(plane, state) in
    >    mtk_drm_crtc_async_update.
    > 2. Add mtk_drm_update_new_state in to mtk_plane_atomic_async_update to
    >    update the cursor plane by current plan hook and update the normal
    >    plane by the new_state.
    >
    > Fixes: 37418bf14c13 ("drm: Use state helper instead of the plane state pointer")
    >
    > Signed-off-by: jason-jh.lin <jason-jh.lin@mediatek.com>
    > Tested-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    >
    > (am from https://patchwork.kernel.org/project/linux-mediatek/patch/20210719082238.26210-2-jason-jh.lin@mediatek.com/)
    >
    > BUG=b:189348863
    > TEST=Mouse can move on the screen.
    >
    > Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    > Change-Id: I5dd427f2d0d5dac548f7be12585451a6cc85e85f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3035404
    > Reviewed-by: Sean Paul <seanpaul@chromium.org>
    > Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    
    Bug: b:189348863
    
    Change-Id: I2e3ebfd9fac6be11d6b72025e526c4ff60a54206
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3043858
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- drivers/gpu/drm/mediatek/mtk_drm_plane.c
+++ drivers/gpu/drm/mediatek/mtk_drm_plane.c
@@ -110,35 +110,6 @@ static int mtk_plane_atomic_async_check(struct drm_plane *plane,
 						   true, true);
 }
 
-static void mtk_plane_update_new_state(struct drm_plane_state *new_state,
-				       struct mtk_plane_state *mtk_plane_state)
-{
-	struct drm_framebuffer *fb = new_state->fb;
-	struct drm_gem_object *gem;
-	struct mtk_drm_gem_obj *mtk_gem;
-	unsigned int pitch, format;
-	dma_addr_t addr;
-
-	gem = fb->obj[0];
-	mtk_gem = to_mtk_gem_obj(gem);
-	addr = mtk_gem->dma_addr;
-	pitch = fb->pitches[0];
-	format = fb->format->format;
-
-	addr += (new_state->src.x1 >> 16) * fb->format->cpp[0];
-	addr += (new_state->src.y1 >> 16) * pitch;
-
-	mtk_plane_state->pending.enable = true;
-	mtk_plane_state->pending.pitch = pitch;
-	mtk_plane_state->pending.format = format;
-	mtk_plane_state->pending.addr = addr;
-	mtk_plane_state->pending.x = new_state->dst.x1;
-	mtk_plane_state->pending.y = new_state->dst.y1;
-	mtk_plane_state->pending.width = drm_rect_width(&new_state->dst);
-	mtk_plane_state->pending.height = drm_rect_height(&new_state->dst);
-	mtk_plane_state->pending.rotation = new_state->rotation;
-}
-
 static void mtk_plane_atomic_async_update(struct drm_plane *plane,
 					  struct drm_atomic_state *state)
 {
@@ -155,10 +126,8 @@ static void mtk_plane_atomic_async_update(struct drm_plane *plane,
 	plane->state->src_h = new_state->src_h;
 	plane->state->src_w = new_state->src_w;
 	swap(plane->state->fb, new_state->fb);
-
-	mtk_plane_update_new_state(new_state, new_plane_state);
-	wmb(); /* Make sure the above parameters are set before update */
 	new_plane_state->pending.async_dirty = true;
+
 	mtk_drm_crtc_async_update(new_state->crtc, plane, state);
 }
 
@@ -220,8 +189,14 @@ static void mtk_plane_atomic_update(struct drm_plane *plane,
 	struct drm_plane_state *new_state = drm_atomic_get_new_plane_state(state,
 									   plane);
 	struct mtk_plane_state *mtk_plane_state = to_mtk_plane_state(new_state);
+	struct drm_crtc *crtc = new_state->crtc;
+	struct drm_framebuffer *fb = new_state->fb;
+	struct drm_gem_object *gem;
+	struct mtk_drm_gem_obj *mtk_gem;
+	unsigned int pitch, format;
+	dma_addr_t addr;
 
-	if (!new_state->crtc || WARN_ON(!new_state->fb))
+	if (!crtc || WARN_ON(!fb))
 		return;
 
 	if (!new_state->visible) {
@@ -229,7 +204,24 @@ static void mtk_plane_atomic_update(struct drm_plane *plane,
 		return;
 	}
 
-	mtk_plane_update_new_state(new_state, mtk_plane_state);
+	gem = fb->obj[0];
+	mtk_gem = to_mtk_gem_obj(gem);
+	addr = mtk_gem->dma_addr;
+	pitch = fb->pitches[0];
+	format = fb->format->format;
+
+	addr += (new_state->src.x1 >> 16) * fb->format->cpp[0];
+	addr += (new_state->src.y1 >> 16) * pitch;
+
+	mtk_plane_state->pending.enable = true;
+	mtk_plane_state->pending.pitch = pitch;
+	mtk_plane_state->pending.format = format;
+	mtk_plane_state->pending.addr = addr;
+	mtk_plane_state->pending.x = new_state->dst.x1;
+	mtk_plane_state->pending.y = new_state->dst.y1;
+	mtk_plane_state->pending.width = drm_rect_width(&new_state->dst);
+	mtk_plane_state->pending.height = drm_rect_height(&new_state->dst);
+	mtk_plane_state->pending.rotation = new_state->rotation;
 	wmb(); /* Make sure the above parameters are set before update */
 	mtk_plane_state->pending.dirty = true;
 }
