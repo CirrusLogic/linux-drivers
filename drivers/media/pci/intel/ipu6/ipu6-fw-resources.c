73fe294387821367a8c8cb95fe12198c437f4ba6
Sergey Senozhatsky <senozhatsky@chromium.org>
2021-07-28 23:48:27 +0900

    CHROMIUM: media/ipu6: make ipu6 fw cells static
    IPU6 cell-id mappings can be static.
    
    BUG=b:195997235
    TEST=run camera app on dedede
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Change-Id: I4be9fe030908e4adbb39af2756f1468ec1800d7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3083788
    Reviewed-by: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/pci/intel/ipu6/ipu6-fw-resources.c
+++ drivers/media/pci/intel/ipu6/ipu6-fw-resources.c
@@ -13,7 +13,7 @@
 /*
  * Cell types by cell IDs
  */
-const u8 ipu6_fw_psys_cell_types[IPU6_FW_PSYS_N_CELL_ID] = {
+static const u8 ipu6_fw_psys_cell_types[IPU6_FW_PSYS_N_CELL_ID] = {
 	IPU6_FW_PSYS_SP_CTRL_TYPE_ID,
 	IPU6_FW_PSYS_VP_TYPE_ID,
 	IPU6_FW_PSYS_ACC_PSA_TYPE_ID,
@@ -48,7 +48,7 @@ const u8 ipu6_fw_psys_cell_types[IPU6_FW_PSYS_N_CELL_ID] = {
 	IPU6_FW_PSYS_TNR_TYPE_ID,
 };
 
-const u16 ipu6_fw_num_dev_channels[IPU6_FW_PSYS_N_DEV_CHN_ID] = {
+static const u16 ipu6_fw_num_dev_channels[IPU6_FW_PSYS_N_DEV_CHN_ID] = {
 	IPU6_FW_PSYS_DEV_CHN_DMA_EXT0_MAX_SIZE,
 	IPU6_FW_PSYS_DEV_CHN_DMA_EXT1_READ_MAX_SIZE,
 	IPU6_FW_PSYS_DEV_CHN_DMA_EXT1_WRITE_MAX_SIZE,
@@ -56,7 +56,7 @@ const u16 ipu6_fw_num_dev_channels[IPU6_FW_PSYS_N_DEV_CHN_ID] = {
 	IPU6_FW_PSYS_DEV_CHN_DMA_ISA_MAX_SIZE,
 };
 
-const u16 ipu6_fw_psys_mem_size[IPU6_FW_PSYS_N_MEM_ID] = {
+static const u16 ipu6_fw_psys_mem_size[IPU6_FW_PSYS_N_MEM_ID] = {
 	IPU6_FW_PSYS_VMEM0_MAX_SIZE,
 	IPU6_FW_PSYS_TRANSFER_VMEM0_MAX_SIZE,
 	IPU6_FW_PSYS_TRANSFER_VMEM1_MAX_SIZE,
@@ -69,7 +69,7 @@ const u16 ipu6_fw_psys_mem_size[IPU6_FW_PSYS_N_MEM_ID] = {
 	IPU6_FW_PSYS_PMEM0_MAX_SIZE
 };
 
-const u16 ipu6_fw_psys_dfms[IPU6_FW_PSYS_N_DEV_DFM_ID] = {
+static const u16 ipu6_fw_psys_dfms[IPU6_FW_PSYS_N_DEV_DFM_ID] = {
 	IPU6_FW_PSYS_DEV_DFM_BB_FULL_PORT_ID_MAX_SIZE,
 	IPU6_FW_PSYS_DEV_DFM_BB_EMPTY_PORT_ID_MAX_SIZE,
 	IPU6_FW_PSYS_DEV_DFM_ISL_FULL_PORT_ID_MAX_SIZE,
@@ -78,7 +78,7 @@ const u16 ipu6_fw_psys_dfms[IPU6_FW_PSYS_N_DEV_DFM_ID] = {
 	IPU6_FW_PSYS_DEV_DFM_LB_EMPTY_PORT_ID_MAX_SIZE,
 };
 
-const u8
+static const u8
 ipu6_fw_psys_c_mem[IPU6_FW_PSYS_N_CELL_ID][IPU6_FW_PSYS_N_MEM_TYPE_ID] = {
 	{
 		/* IPU6_FW_PSYS_SP0_ID */
