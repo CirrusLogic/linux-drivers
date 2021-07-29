440fd941d85f43cf00e5c8cd0fabf98c37d2f592
Sergey Senozhatsky <senozhatsky@chromium.org>
2021-07-29 11:16:00 +0900

    CHROMIUM: media/ipu6: ipu_fw_psys_pg_dump() can be a no-op
    Every kcmd_start attempts to ipu_fw_psys_pg_dump(), which does
    anything valuable only if dev_dbg() is enabled, which depends
    on CONFIG_DEBUG || CONFIG_DYNAMIC_DEBUG:
    
     0)               |      ipu_psys_kcmd_start [intel_ipu6_psys]() {
     0)   0.186 us    |        ipu_fw_psys_pg_start [intel_ipu6_psys]();
     0)               |        ipu_fw_psys_pg_dump [intel_ipu6_psys]() {
     0)   0.199 us    |          ipu6_fw_psys_pg_dump [intel_ipu6_psys]();
     0)   0.573 us    |        }
    
    BUG=b:195997235
    TEST=camera app on dedede
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Change-Id: I2632703216c39ba2163bafac57057a762a994f33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3083789
    Reviewed-by: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/pci/intel/ipu6/ipu6-fw-resources.c
+++ drivers/media/pci/intel/ipu6/ipu6-fw-resources.c
@@ -539,6 +539,8 @@ int ipu6_fw_psys_get_program_manifest_by_process(
 	return 0;
 }
 
+#if defined(DEBUG) || defined(CONFIG_DYNAMIC_DEBUG) || \
+	(defined(CONFIG_DYNAMIC_DEBUG_CORE) && defined(DYNAMIC_DEBUG_MODULE))
 void ipu6_fw_psys_pg_dump(struct ipu_psys *psys,
 			  struct ipu_psys_kcmd *kcmd, const char *note)
 {
@@ -593,3 +595,14 @@ void ipu6_fw_psys_pg_dump(struct ipu_psys *psys,
 		}
 	}
 }
+#else
+void ipu6_fw_psys_pg_dump(struct ipu_psys *psys,
+			  struct ipu_psys_kcmd *kcmd, const char *note)
+{
+	if (ipu_ver == IPU_VER_6SE || ipu_ver == IPU_VER_6 ||
+	    ipu_ver == IPU_VER_6EP)
+		return;
+
+	WARN(1, "%s ipu_ver:[%u] is unsupported!\n", __func__, ipu_ver);
+}
+#endif
