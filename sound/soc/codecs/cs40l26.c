b9e1b2460f736b5a811670a614160308714ea732
James Ogletree <james.ogletree@cirrus.com>
2021-10-27 15:14:56 +0000

    ASoC: cs40l26: Expose delay controls
    Allow for user to adjust the delay that is applied
    to the output, from 0 to 400 (ms).
    
    Change-Id: I5c3be8a44f96fb981d5db5b15a22adde21b5ae96
    Signed-off-by: James Ogletree <james.ogletree@cirrus.com>

--- sound/soc/codecs/cs40l26.c
+++ sound/soc/codecs/cs40l26.c
@@ -537,6 +537,74 @@ static int cs40l26_a2h_volume_put(struct snd_kcontrol *kcontrol,
 	return ret;
 }
 
+static int cs40l26_a2h_delay_get(struct snd_kcontrol *kcontrol,
+		struct snd_ctl_elem_value *ucontrol)
+{
+	struct cs40l26_codec *codec =
+	snd_soc_component_get_drvdata(snd_soc_kcontrol_component(kcontrol));
+	struct cs40l26_private *cs40l26 = codec->core;
+	struct regmap *regmap = cs40l26->regmap;
+	struct device *dev = cs40l26->dev;
+	unsigned int val = 0, reg;
+	int ret;
+
+	ret = cl_dsp_get_reg(cs40l26->dsp, "LRADELAYSAMPS",
+			CL_DSP_XM_UNPACKED_TYPE, CS40L26_A2H_ALGO_ID, &reg);
+	if (ret)
+		return ret;
+
+	pm_runtime_get_sync(dev);
+
+	ret = regmap_read(regmap, reg, &val);
+	if (ret) {
+		dev_err(dev, "Failed to get LRADELAYSAMPS\n");
+		goto err;
+	}
+
+	ucontrol->value.integer.value[0] = val;
+
+err:
+	pm_runtime_mark_last_busy(dev);
+	pm_runtime_put_autosuspend(dev);
+
+	return ret;
+}
+
+static int cs40l26_a2h_delay_put(struct snd_kcontrol *kcontrol,
+		struct snd_ctl_elem_value *ucontrol)
+{
+	struct cs40l26_codec *codec =
+	snd_soc_component_get_drvdata(snd_soc_kcontrol_component(kcontrol));
+	struct cs40l26_private *cs40l26 = codec->core;
+	struct regmap *regmap = cs40l26->regmap;
+	struct device *dev = cs40l26->dev;
+	unsigned int val = 0, reg;
+	int ret;
+
+	ret = cl_dsp_get_reg(cs40l26->dsp, "LRADELAYSAMPS",
+			CL_DSP_XM_UNPACKED_TYPE, CS40L26_A2H_ALGO_ID, &reg);
+	if (ret)
+		return ret;
+
+	if (ucontrol->value.integer.value[0] > CS40L26_A2H_DELAY_MAX)
+		val = CS40L26_A2H_DELAY_MAX;
+	else if (ucontrol->value.integer.value[0] < 0)
+		val = 0;
+	else
+		val = ucontrol->value.integer.value[0];
+
+	pm_runtime_get_sync(dev);
+
+	ret = regmap_write(regmap, reg, val);
+	if (ret)
+		dev_err(dev, "Failed to set LRADELAYSAMPS\n");
+
+	pm_runtime_mark_last_busy(dev);
+	pm_runtime_put_autosuspend(dev);
+
+	return ret;
+}
+
 static const struct snd_kcontrol_new cs40l26_controls[] = {
 	SOC_SINGLE_EXT("A2H Tuning", 0, 0, CS40L26_A2H_MAX_TUNINGS, 0,
 			cs40l26_tuning_get, cs40l26_tuning_put),
@@ -552,6 +620,8 @@ static const struct snd_kcontrol_new cs40l26_controls[] = {
 			cs40l26_i2s_vmon_get, NULL),
 	SOC_SINGLE_EXT("DSP Bypass", 0, 0, 1, 0, cs40l26_bypass_get,
 			cs40l26_bypass_put),
+	SOC_SINGLE_EXT("A2H Delay", 0, 0, CS40L26_A2H_DELAY_MAX, 0,
+			cs40l26_a2h_delay_get, cs40l26_a2h_delay_put),
 };
 
 static const char * const cs40l26_out_mux_texts[] = { "Off", "PCM", "A2H" };
