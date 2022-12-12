59e8af8e645f964be58fad4b91965ac3ce9eda30
David Rhodes <david.rhodes@cirrus.com>
2022-12-12 20:53:24 +0000

    drivers: firmware: cirrus: Export cs_dsp_load_coeff
    The function is needed to load the delta tunning files
    from external drivers.
    
    Change-Id: I94fd541782b21e0ad954e855fa5f3d8a5ef288cf
    Signed-off-by: David Rhodes <david.rhodes@cirrus.com>

--- drivers/firmware/cirrus/cs_dsp.c
+++ drivers/firmware/cirrus/cs_dsp.c
@@ -2161,7 +2161,7 @@ out:
 	return ret;
 }
 
-static int cs_dsp_load_coeff(struct cs_dsp *dsp, const struct firmware *firmware,
+int cs_dsp_load_coeff(struct cs_dsp *dsp, const struct firmware *firmware,
 			     const char *file)
 {
 	LIST_HEAD(buf_list);
@@ -2358,6 +2358,7 @@ out_fw:
 
 	return ret;
 }
+EXPORT_SYMBOL_GPL(cs_dsp_load_coeff);
 
 static int cs_dsp_create_name(struct cs_dsp *dsp)
 {
