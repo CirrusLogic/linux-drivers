f28fc4af90796428f61c10a62be60573e9149c68
Vlad Karpovich <Vlad.Karpovich@cirrus.com>
2023-02-15 10:28:00 -0600

    ASoC: cs35l45: Defer probe on ENOTCONN error
    Defer the driver if it failed to read OTP after reboot.
    This is a work around for an intermittent I2C bus issue during boot up
    on some Qcom platforms.
    
    FJ-805,SOMC-599
    
    Change-Id: I9a68870313ad73c0edada146624a253b31b0f59c
    Signed-off-by: Vlad Karpovich <Vlad.Karpovich@cirrus.com>

--- sound/soc/codecs/cs35l45-i2c.c
+++ sound/soc/codecs/cs35l45-i2c.c
@@ -53,6 +53,10 @@ static int cs35l45_i2c_probe(struct i2c_client *client,
 	ret = cs35l45_initialize(cs35l45);
 	if (ret < 0) {
 		dev_err(dev, "Failed device initialization: %d\n", ret);
+		if (ret == -ENOTCONN) {
+			ret = -EPROBE_DEFER;
+			dev_err(dev, "Defer probe on ENOTCONN error\n");
+		}
 		goto fail;
 	}
 
