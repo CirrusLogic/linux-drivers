8ec68e5273b1bd2dfc2dcba1aa71c36f380fd244
Fred Treven <fred.treven@cirrus.com>
2023-03-20 14:33:32 -0500

    input: cs40l26: Don't initialize CL DSP debugfs if not supported firmware
    If the EVENT_LOGGER algorithm is not present in the firmware,
    avoid creating the CL DSP debugfs directory.
    
    Change-Id: I17f4040c9ee338f3eca90991b8c64408e4d87a06
    Signed-off-by: Fred Treven <fred.treven@cirrus.com>

--- drivers/input/misc/cs40l26-debugfs.c
+++ drivers/input/misc/cs40l26-debugfs.c
@@ -232,7 +232,8 @@ void cs40l26_debugfs_init(struct cs40l26_private *cs40l26)
 	cs40l26->dbg_fw_algo_id = CS40L26_VIBEGEN_ALGO_ID;
 	cs40l26->debugfs_root = root;
 
-	if (cs40l26->fw_id == CS40L26_FW_ID) {
+	if (cs40l26->fw_id == CS40L26_FW_ID &&
+			cl_dsp_algo_is_present(cs40l26->dsp, CS40L26_EVENT_LOGGER_ALGO_ID)) {
 		cs40l26->cl_dsp_db = cl_dsp_debugfs_create(cs40l26->dsp,
 				cs40l26->debugfs_root,
 				(u32) CS40L26_EVENT_LOGGER_ALGO_ID);
