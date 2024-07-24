MotorolaMobilityLLC/motorola-kernel-modules

misc: cs40l2x: Update DSP initialization for revision A0 silicon
* Enabled the device prior to enabling the DSP to ensure internal clocks
  are present while revision A0 silicon initializes firmware.
* Corrected the waveform filename and replaced with a macro.
* Updated the minimum supported firmware revision to 3.0.0.

Change-Id: If41a6865e661e55f3eed8f401bcfe19176af3de3
Signed-off-by: Jeff LaBundy <jeff.labundy@cirrus.com>
Signed-off-by: Ravi Vembu <raviv@motorola.com>

drivers/misc/cs40l2x.c
@@ -1061,6 +1061,14 @@ static void cs40l2x_dsp_start(struct cs40l2x_private *cs40l2x)
 
 	switch (cs40l2x->revid) {
 	case CS40L2X_REVID_A0:
+		ret = regmap_update_bits(regmap, CS40L2X_PWR_CTRL1,
+				CS40L2X_GLOBAL_EN_MASK,
+				1 << CS40L2X_GLOBAL_EN_SHIFT);
+		if (ret) {
+			dev_err(dev, "Failed to enable device\n");
+			return;
+		}
+
 		ret = regmap_update_bits(regmap, CS40L2X_DSP1_CCM_CORE_CTRL,
 				CS40L2X_DSP1_RESET_MASK |
 				CS40L2X_DSP1_EN_MASK,
@@ -1436,7 +1444,7 @@ static void cs40l2x_firmware_load(const struct firmware *fw, void *context)
 	if (ret)
 		goto err_rls_fw;
 
-	request_firmware_nowait(THIS_MODULE, FW_ACTION_HOTPLUG, "cs40l2x.bin",
+	request_firmware_nowait(THIS_MODULE, FW_ACTION_HOTPLUG, CS40L2X_WT_NAME,
 			dev, GFP_KERNEL, cs40l2x, cs40l2x_waveform_load);
 err_rls_fw:
 	release_firmware(fw);

CirrusLogic/linux-drivers

drivers/misc/cs40l2x.c
@@ -1061,6 +1061,14 @@ static void cs40l2x_dsp_start(struct cs40l2x_private *cs40l2x)
 
 	switch (cs40l2x->revid) {
 	case CS40L2X_REVID_A0:
+		ret = regmap_update_bits(regmap, CS40L2X_PWR_CTRL1,
+				CS40L2X_GLOBAL_EN_MASK,
+				1 << CS40L2X_GLOBAL_EN_SHIFT);
+		if (ret) {
+			dev_err(dev, "Failed to enable device\n");
+			return;
+		}
+
 		ret = regmap_update_bits(regmap, CS40L2X_DSP1_CCM_CORE_CTRL,
 				CS40L2X_DSP1_RESET_MASK |
 				CS40L2X_DSP1_EN_MASK,
@@ -1436,7 +1444,7 @@ static void cs40l2x_firmware_load(const struct firmware *fw, void *context)
 	if (ret)
 		goto err_rls_fw;
 
-	request_firmware_nowait(THIS_MODULE, FW_ACTION_NOUEVENT, "cs40l2x.bin",
+	request_firmware_nowait(THIS_MODULE, FW_ACTION_UEVENT, CS40L2X_WT_NAME,
 			dev, GFP_KERNEL, cs40l2x, cs40l2x_waveform_load);
 err_rls_fw:
 	release_firmware(fw);