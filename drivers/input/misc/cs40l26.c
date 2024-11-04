4a4865008cec3b131dcca0196e7f3c03de4cd029
James Ogletree <jogletre@opensource.cirrus.com>
2024-11-04 22:12:42 +0000

    Input: cs40l26 - Fix use of uninitialized data
    Fix undefined behavior by initializing work_data
    struct to zeroes.
    
    Change-Id: Ic0f19fa9f6d0ade35ed76f9f7b873b70411bc991
    Signed-off-by: James Ogletree <jogletre@opensource.cirrus.com>

--- drivers/input/misc/cs40l26.c
+++ drivers/input/misc/cs40l26.c
@@ -2977,6 +2977,8 @@ static int cs40l26_upload_effect(struct input_dev *dev,
 
 	dev_dbg(cs40l26->dev, "%s: effect ID = %d\n", __func__, effect->id);
 
+	memset((void *) &work_data, 0, sizeof(struct cs40l26_work));
+
 	if (effect->u.periodic.waveform == FF_CUSTOM) {
 		work_data.raw_custom_data_len = len;
 
@@ -3141,6 +3143,8 @@ static int cs40l26_erase_effect(struct input_dev *dev, int effect_id)
 	struct cs40l26_work work_data;
 
 	dev_dbg(cs40l26->dev, "%s: effect ID = %d\n", __func__, effect_id);
+
+	memset((void *) &work_data, 0, sizeof(struct cs40l26_work));
 
 	work_data.effect = &dev->ff->effects[effect_id];
 
