74e4a8192bdec445266f9b3be58f457cdf218f99
Fred Treven <ftreven@opensource.cirrus.com>
2025-11-12 14:56:27 -0600

    ASoC: cs40l26: Only swap to BCLK early if using ASP Dout
    If ASP Dout is enabled for signal recording, REFCLK_INPUT
    needs to be swapped to using BCLK in component_set_sysclk()
    since this callback is invoked prior to DAPM powering up
    for ASP streaming.
    
    Doing this prevents the device from receiving interrupts
    until the stream has been fully powered up. This cannot
    be allowed in cases for which ASP Dout is not configured.
    
    Change-Id: Id72dcf24200f79168cd4a2d5cf634a03036dfd2d
    Signed-off-by: Fred Treven <ftreven@opensource.cirrus.com>

--- sound/soc/codecs/cs40l26.c
+++ sound/soc/codecs/cs40l26.c
@@ -1015,7 +1015,8 @@ static int cs40l26_component_set_sysclk(struct snd_soc_component *component,
 
 	codec->sysclk_rate = (u32) (CS40L26_PLL_CLK_FREQ_MASK & freq);
 
-	cs40l26_swap_ext_clk(codec, CS40L26_PLL_REFCLK_BCLK);
+	if (codec->core->asp_dout)
+		cs40l26_swap_ext_clk(codec, CS40L26_PLL_REFCLK_BCLK);
 
 	return 0;
 }
